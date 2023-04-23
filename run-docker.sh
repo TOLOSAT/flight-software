eb5218224f50bb7020ff3e476ca93b001be4e6cc
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-04-23 17:57:02 +0200

    Docker now working

--- run-docker.sh
+++ run-docker.sh
@@ -8,7 +8,7 @@ if [[ "$(docker images -q flight-software 2> /dev/null)" == "" ]]; then
     if [ $? -eq 0 ]; then
         echo "Docker image 'flight-software' built successfully."
         # Run the Docker container after the image is built
-        docker run -it --rm --hostname tolosat-devtool -p 4444:4444 -v $(pwd):/app --privileged -v /dev/bus/usb:/dev/bus/usb flight-software:latest
+        docker run -it --rm --hostname tolosat-devtool -p 4444:4444 -v $(pwd):/home/obc/app --privileged -v /dev/bus/usb:/dev/bus/usb flight-software:latest
     else
         echo "Failed to build Docker image 'flight-software'."
         exit 1
@@ -16,7 +16,7 @@ if [[ "$(docker images -q flight-software 2> /dev/null)" == "" ]]; then
 else
     echo "Docker image 'flight-software' already exists."
     # Run the Docker container if the image already exists
-    docker run -it --rm --hostname tolosat-devtool -p 4444:4444 -v $(pwd):/app --privileged -v /dev/bus/usb:/dev/bus/usb flight-software:latest
+    docker run -it --rm --hostname tolosat-devtool -p 4444:4444 -v $(pwd):/home/obc/app --privileged -v /dev/bus/usb:/dev/bus/usb flight-software:latest
 fi
 
 
