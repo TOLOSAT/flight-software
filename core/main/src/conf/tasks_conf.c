01dd9463b028ba29556d9cd1f1f82fe76aa4a75d
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-04-29 04:56:01 +0000

    Moved tasks and buffer management to main instead of tasks

--- core/main/src/conf/tasks_conf.c
+++ core/main/src/conf/tasks_conf.c
@@ -16,8 +16,8 @@
 /***************************** Configuration *********************************/
 
 const taskDef_t g_tasks[NB_TASKS] = {
-    {BLINK01_TASK, StartBlink01, NULL, {.priority = osPriorityNormal     , .stack_size = 512}},
-    {BLINK02_TASK, StartBlink02, NULL, {.priority = osPriorityBelowNormal, .stack_size = 512}},
+    {BLINK01_TASK, StartBlink01, NULL, {.priority = osPriorityNormal     , .stack_size = 1024}},
+    {BLINK02_TASK, StartBlink02, NULL, {.priority = osPriorityBelowNormal, .stack_size = 1024}},
 };
 
 taskId_t g_tasks_ids[NB_TASKS];
