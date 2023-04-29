9b17859b1b1aec517eb6fe3f18e1b1b1faa2f75a
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-04-29 09:52:38 +0000

    Init TOLOSAT HAL

---
+++ tools/HALs/HAL-TOLOSAT/inc/tolosat_hal.h
@@ -0,0 +1,25 @@
+/**
+ * @file tolosat_hal.h
+ * @author Merlin Kooshmanian
+ * @brief Header including all TOLOSAT HAL headers
+ * @date 29/04/2023
+ * 
+ * Last Update : 29/04/2023
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+#ifndef TOLOSAT_HAL_H
+#define TOLOSAT_HAL_H
+
+/***************************** Include Files *********************************/
+
+#include "tolosat_hal_types.h"
+#include "tolosat_hal_init.h"
+
+/************************** Constant Definitions *****************************/
+
+/**************************** Type Definitions *******************************/
+
+/************************** Function Prototypes ******************************/
+
+#endif /* TOLOSAT_HAL_H */
