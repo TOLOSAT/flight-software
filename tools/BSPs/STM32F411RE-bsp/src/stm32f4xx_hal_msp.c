2dab59e31b10113f7ca500e9cb22d44a0f30b2af
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-05-02 18:28:10 +0000

    Adding DMA but not tested, move msp to bsp again

---
+++ tools/BSPs/STM32F411RE-bsp/src/stm32f4xx_hal_msp.c
@@ -0,0 +1,240 @@
+/**
+ ******************************************************************************
+ * @file         stm32f4xx_hal_msp.c
+ * @brief        This file provides code for the MSP Initialization
+ *               and de-Initialization codes.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+/***************************** Include Files *********************************/
+
+#include "stm32f4xx_hal.h"
+
+/************************** Constant Definitions *****************************/
+
+/**************************** Type Definitions *******************************/
+
+/************************** Function Prototypes ******************************/
+
+/************************** Variable Definitions *****************************/
+
+extern DMA_HandleTypeDef hdma_usart1_rx;
+extern DMA_HandleTypeDef hdma_usart1_tx;
+
+/************************* Functions Definitions *****************************/
+
+/**
+ * Initializes the Global MSP.
+ */
+void HAL_MspInit(void)
+{
+  __HAL_RCC_SYSCFG_CLK_ENABLE();
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* System interrupt init*/
+  /* PendSV_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(PendSV_IRQn, 15, 0);
+}
+
+/**
+ * @brief I2C MSP Initialization
+ * This function configures the hardware resources used in this example
+ * @param hi2c: I2C handle pointer
+ * @retval None
+ */
+void HAL_I2C_MspInit(I2C_HandleTypeDef *hi2c)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if (hi2c->Instance == I2C1)
+  {
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**I2C1 GPIO Configuration
+    PB8     ------> I2C1_SCL
+    PB9     ------> I2C1_SDA
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* Peripheral clock enable */
+    __HAL_RCC_I2C1_CLK_ENABLE();
+  }
+}
+
+/**
+ * @brief I2C MSP De-Initialization
+ * This function freeze the hardware resources used in this example
+ * @param hi2c: I2C handle pointer
+ * @retval None
+ */
+void HAL_I2C_MspDeInit(I2C_HandleTypeDef *hi2c)
+{
+  if (hi2c->Instance == I2C1)
+  {
+    /* Peripheral clock disable */
+    __HAL_RCC_I2C1_CLK_DISABLE();
+
+    /**I2C1 GPIO Configuration
+    PB8     ------> I2C1_SCL
+    PB9     ------> I2C1_SDA
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_8);
+
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_9);
+  }
+}
+
+/**
+ * @brief UART MSP Initialization
+ * This function configures the hardware resources used in this example
+ * @param huart: UART handle pointer
+ * @retval None
+ */
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if (huart->Instance == USART1)
+  {
+    /* Peripheral clock enable */
+    __HAL_RCC_USART1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**USART1 GPIO Configuration
+    PB6     ------> USART1_TX
+    PB7     ------> USART1_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_6 | GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* USART1 DMA Init */
+    /* USART1_RX Init */
+    hdma_usart1_rx.Instance = DMA2_Stream2;
+    hdma_usart1_rx.Init.Channel = DMA_CHANNEL_4;
+    hdma_usart1_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_usart1_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_usart1_rx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_usart1_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_usart1_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_usart1_rx.Init.Mode = DMA_NORMAL;
+    hdma_usart1_rx.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_usart1_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    if (HAL_DMA_Init(&hdma_usart1_rx) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    __HAL_LINKDMA(huart, hdmarx, hdma_usart1_rx);
+
+    /* USART1_TX Init */
+    hdma_usart1_tx.Instance = DMA2_Stream7;
+    hdma_usart1_tx.Init.Channel = DMA_CHANNEL_4;
+    hdma_usart1_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+    hdma_usart1_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_usart1_tx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_usart1_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_usart1_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_usart1_tx.Init.Mode = DMA_NORMAL;
+    hdma_usart1_tx.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_usart1_tx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    if (HAL_DMA_Init(&hdma_usart1_tx) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    __HAL_LINKDMA(huart, hdmatx, hdma_usart1_tx);
+  }
+  else if (huart->Instance == USART2)
+  {
+    /* Peripheral clock enable */
+    __HAL_RCC_USART2_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  }
+  else if (huart->Instance == USART6)
+  {
+    /* Peripheral clock enable */
+    __HAL_RCC_USART6_CLK_ENABLE();
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    /**USART6 GPIO Configuration
+    PC6     ------> USART6_TX
+    PC7     ------> USART6_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_6 | GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF8_USART6;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  }
+}
+
+/**
+ * @brief UART MSP De-Initialization
+ * This function freeze the hardware resources used in this example
+ * @param huart: UART handle pointer
+ * @retval None
+ */
+void HAL_UART_MspDeInit(UART_HandleTypeDef *huart)
+{
+  if (huart->Instance == USART1)
+  {
+    /* Peripheral clock disable */
+    __HAL_RCC_USART1_CLK_DISABLE();
+
+    /**USART1 GPIO Configuration
+    PB6     ------> USART1_TX
+    PB7     ------> USART1_RX
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_6 | GPIO_PIN_7);
+  }
+  else if (huart->Instance == USART2)
+  {
+    /* Peripheral clock disable */
+    __HAL_RCC_USART2_CLK_DISABLE();
+
+    /**USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2 | GPIO_PIN_3);
+  }
+  else if (huart->Instance == USART6)
+  {
+    /* Peripheral clock disable */
+    __HAL_RCC_USART6_CLK_DISABLE();
+
+    /**USART6 GPIO Configuration
+    PC6     ------> USART6_TX
+    PC7     ------> USART6_RX
+    */
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_6 | GPIO_PIN_7);
+  }
+}
--- tools/HALs/HAL-TOLOSAT/src/tolosat_hal_uart.c
+++ tools/HALs/HAL-TOLOSAT/src/tolosat_hal_uart.c
@@ -28,6 +28,8 @@
  * @param   uart_inst Instance that contains UART parameters and UART Handler
  * @retval  FCT_SUCCESSFUL if creation succeed
  * @retval  FCT_INVALID_PARAM if UART ref is not available for this board, baudrate or one pointer is null
+ * 
+ * NB : Only USART1 feature DMA on this TOLOSAT HAL
  */
 halStatus_t UartOpen(uartInst_t *uart_inst)
 {
@@ -89,6 +91,40 @@ halStatus_t UartOpen(uartInst_t *uart_inst)
                         return_value = FCT_INVALID_PARAM;
                     }
                 }
+                else if (uart_inst->drive_type == UART_DMA_DRIVE)
+                {
+                    if (uart_inst->uart_ref == USART1)
+                    {
+#if defined(STM32F411xE)
+                        /* DMA controller clock enable */
+                        __HAL_RCC_DMA2_CLK_ENABLE();
+
+                        /* DMA interrupt init */
+                        /* DMA2_Stream2_IRQn interrupt configuration */
+                        HAL_NVIC_SetPriority(DMA2_Stream2_IRQn, 8, 0);
+                        HAL_NVIC_EnableIRQ(DMA2_Stream2_IRQn);
+                        /* DMA2_Stream7_IRQn interrupt configuration */
+                        HAL_NVIC_SetPriority(DMA2_Stream7_IRQn, 8, 0);
+                        HAL_NVIC_EnableIRQ(DMA2_Stream7_IRQn);
+#endif
+#if defined(STM32F103xB)
+                        /* DMA controller clock enable */
+                        __HAL_RCC_DMA1_CLK_ENABLE();
+
+                        /* DMA interrupt init */
+                        /* DMA1_Channel4_IRQn interrupt configuration */
+                        HAL_NVIC_SetPriority(DMA1_Channel4_IRQn, 8, 0);
+                        HAL_NVIC_EnableIRQ(DMA1_Channel4_IRQn);
+                        /* DMA1_Channel5_IRQn interrupt configuration */
+                        HAL_NVIC_SetPriority(DMA1_Channel5_IRQn, 8, 0);
+                        HAL_NVIC_EnableIRQ(DMA1_Channel5_IRQn);
+#endif
+                    }
+                    else
+                    {
+                        return_value = FCT_INVALID_PARAM;
+                    }
+                }
             }
             else
             {
@@ -113,8 +149,7 @@ halStatus_t UartOpen(uartInst_t *uart_inst)
  * @retval  FCT_INVALID_PARAM if one pointer is null
  * @retval  FCT_ERROR if transmit went wrong
  *
- * Attention : currently works only in polling and interrupt mode
- * Needs to supports DMA
+ * NB : Only USART1 feature DMA on this TOLOSAT HAL
  */
 halStatus_t UartWrite(uartInst_t *uart_inst, uartMsg_t *msg, uartMsgLength_t length)
 {
@@ -141,7 +176,15 @@ halStatus_t UartWrite(uartInst_t *uart_inst, uartMsg_t *msg, uartMsgLength_t len
                 return_value = FCT_ERROR;
             }
         }
-        else 
+        else if (uart_inst->drive_type == UART_DMA_DRIVE)
+        {
+            test_val = HAL_UART_Transmit_DMA(&uart_inst->handle_struct, msg, length);
+            if (test_val != HAL_OK)
+            {
+                return_value = FCT_ERROR;
+            }
+        }
+        else
         {
             return_value = FCT_INVALID_PARAM;
         }
@@ -164,8 +207,7 @@ halStatus_t UartWrite(uartInst_t *uart_inst, uartMsg_t *msg, uartMsgLength_t len
  * @retval  FCT_INVALID_PARAM if one pointer is null
  * @retval  FCT_ERROR if transmit went wrong
  *
- * Attention : currently works only in polling and interrupt mode
- * Needs to supports DMA
+ * NB : Only USART1 feature DMA on this TOLOSAT HAL
  */
 halStatus_t UartRead(uartInst_t *uart_inst, uartMsg_t *msg, uartMsgLength_t length)
 {
@@ -192,7 +234,15 @@ halStatus_t UartRead(uartInst_t *uart_inst, uartMsg_t *msg, uartMsgLength_t leng
                 return_value = FCT_ERROR;
             }
         }
-        else 
+        else if (uart_inst->drive_type == UART_DMA_DRIVE)
+        {
+            test_val = HAL_UART_Receive_DMA(&uart_inst->handle_struct, msg, length);
+            if (test_val != HAL_OK)
+            {
+                return_value = FCT_ERROR;
+            }
+        }
+        else
         {
             return_value = FCT_INVALID_PARAM;
         }
@@ -292,174 +342,3 @@ halStatus_t UartClose(uartInst_t *uart_inst)
 
     return (return_value);
 }
-
-/**************************************/
-/******* MSP Function (ST only) *******/
-/**************************************/
-#if defined(STM32F411xE) || defined(STM32F103xB)
-
-/**
- * @brief UART MSP Initialization
- * This function configures the hardware resources used in this example
- * @param huart: UART handle pointer
- * @retval None
- */
-void HAL_UART_MspInit(UART_HandleTypeDef *huart)
-{
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    if (huart->Instance == USART1)
-    {
-        /* Peripheral clock enable */
-        __HAL_RCC_USART1_CLK_ENABLE();
-
-        __HAL_RCC_GPIOA_CLK_ENABLE();
-        /**USART1 GPIO Configuration
-        PA9     ------> USART1_TX
-        PA10     ------> USART1_RX
-        */
-#if defined(STM32F411xE)
-        GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
-        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-#endif
-#if defined(STM32F103xB)
-        GPIO_InitStruct.Pin = GPIO_PIN_9;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-        GPIO_InitStruct.Pin = GPIO_PIN_10;
-        GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-#endif
-    }
-    else if (huart->Instance == USART2)
-    {
-        /* Peripheral clock enable */
-        __HAL_RCC_USART2_CLK_ENABLE();
-
-        __HAL_RCC_GPIOA_CLK_ENABLE();
-        /**USART2 GPIO Configuration
-        PA2     ------> USART2_TX
-        PA3     ------> USART2_RX
-        */
-#if defined(STM32F411xE)
-        GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
-#endif
-#if defined(STM32F103xB)
-        GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-#endif
-        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-    }
-#if defined(STM32F411xE)
-    else if (huart->Instance == USART6)
-    {
-        /* Peripheral clock enable */
-        __HAL_RCC_USART6_CLK_ENABLE();
-
-        __HAL_RCC_GPIOC_CLK_ENABLE();
-        /**USART6 GPIO Configuration
-        PC6     ------> USART6_TX
-        PC7     ------> USART6_RX
-        */
-        GPIO_InitStruct.Pin = GPIO_PIN_6 | GPIO_PIN_7;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        GPIO_InitStruct.Alternate = GPIO_AF8_USART6;
-        HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-    }
-#endif
-#if defined(STM32F103xB)
-    else if (huart->Instance == USART3)
-    {
-        /* Peripheral clock enable */
-        __HAL_RCC_USART3_CLK_ENABLE();
-
-        __HAL_RCC_GPIOB_CLK_ENABLE();
-        /**USART3 GPIO Configuration
-        PB10     ------> USART3_TX
-        PB11     ------> USART3_RX
-        */
-        GPIO_InitStruct.Pin = GPIO_PIN_10;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-        GPIO_InitStruct.Pin = GPIO_PIN_11;
-        GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-    }
-#endif
-}
-
-/**
- * @brief UART MSP De-Initialization
- * This function freeze the hardware resources used in this example
- * @param huart: UART handle pointer
- * @retval None
- */
-void HAL_UART_MspDeInit(UART_HandleTypeDef *huart)
-{
-    if (huart->Instance == USART1)
-    {
-        /* Peripheral clock disable */
-        __HAL_RCC_USART1_CLK_DISABLE();
-
-        /**USART1 GPIO Configuration
-        PA9     ------> USART1_TX
-        PA10     ------> USART1_RX
-        */
-        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9 | GPIO_PIN_10);
-    }
-    else if (huart->Instance == USART2)
-    {
-        /* Peripheral clock disable */
-        __HAL_RCC_USART2_CLK_DISABLE();
-
-        /**USART2 GPIO Configuration
-        PA2     ------> USART2_TX
-        PA3     ------> USART2_RX
-        */
-        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2 | GPIO_PIN_3);
-    }
-#if defined(STM32F411xE)
-    else if (huart->Instance == USART6)
-    {
-        /* Peripheral clock disable */
-        __HAL_RCC_USART6_CLK_DISABLE();
-
-        /**USART6 GPIO Configuration
-        PC6     ------> USART6_TX
-        PC7     ------> USART6_RX
-        */
-        HAL_GPIO_DeInit(GPIOC, GPIO_PIN_6 | GPIO_PIN_7);
-    }
-#endif
-#if defined(STM32F103xB)
-    else if (huart->Instance == USART3)
-    {
-        /* Peripheral clock disable */
-        __HAL_RCC_USART3_CLK_DISABLE();
-
-        /**USART3 GPIO Configuration
-        PB10     ------> USART3_TX
-        PB11     ------> USART3_RX
-        */
-        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_10 | GPIO_PIN_11);
-    }
-#endif
-}
-
-#endif
