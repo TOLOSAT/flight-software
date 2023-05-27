7d67ed8114d9c69d083bc47011cee590f742427c
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-05-27 17:40:24 +0200

    Add modifications in order to reduce #if defined()

--- tools/BSPs/STM32H745ZI-bsp/inc/stm32h7xx_nucleo_bsp.h
+++ tools/BSPs/STM32H745ZI-bsp/inc/stm32h7xx_nucleo_bsp.h
@@ -54,12 +54,21 @@
 #define COM1_RX_AF                                      GPIO_AF7_USART3
 #define COM_POLL_TIMEOUT                                1000
 
-#define TOLOSAT_UART_TMTC                               USART2
-#define TOLOSAT_UART_TMTC_DMA_RX                        hdma_usart2_rx
-#define TOLOSAT_UART_TMTC_DMA_TX                        hdma_usart2_tx
-#define TOLOSAT_UART_PRINT                              USART3
-#define TOLOSAT_UART_PL                                 USART6
-#define TOLOSAT_I2C_AVIONIC                             I2C1
+#define TAPAS_UART_TMTC                                 USART2
+#define TAPAS_UART_TMTC_DMA_RX                          hdma_usart2_rx
+#define TAPAS_UART_TMTC_DMA_TX                          hdma_usart2_tx
+#define TAPAS_UART_TMTC_IRQ_HANDLER                     USART2_IRQHandler
+#define TAPAS_UART_TMTC_DMA_RX_IRQ_HANDLER              DMA1_Stream0_IRQHandler
+#define TAPAS_UART_TMTC_DMA_TX_IRQ_HANDLER              DMA1_Stream1_IRQHandler
+
+#define TAPAS_UART_PRINT                                USART3
+#define TAPAS_UART_PRINT_IRQ_HANDLER                    USART3_IRQHandler
+
+#define TAPAS_UART_PL                                   USART6
+#define TAPAS_UART_PL_IRQ_HANDLER                       USART6_IRQHandler
+
+#define TAPAS_I2C_AVIONIC                               I2C1
+#define TAPAS_I2C_AVIONIC_EVT_IRQ_HANDLER               I2C1_EV_IRQHandler
 
 /**************************** Type Definitions *******************************/
 
