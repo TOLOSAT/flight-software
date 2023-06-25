ca39b462fdd61fbbb8f4cb46bfb6839fd0b30def
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-06-25 19:54:59 +0000

    Add nb msg count in buffers

--- core/application/src/conf/tasks_conf.c
+++ core/application/src/conf/tasks_conf.c
@@ -22,9 +22,9 @@
 const taskStaticConf_t g_tasks_static_conf[NB_TASKS] = 
 {
     /* Task Ref   , Name       , Handler       , Priority              , Stack Size , Default Period , Default Deadline */
-    {MAIN_TASK    , "MainTask" , DummyMainTask , osPriorityAboveNormal , 1024U      , 800u           , 800u              },
-    {BLINK01_TASK , "Blink01"  , StartBlink01  , osPriorityNormal      , 1024U      , 500u           , 500u              },
-    {BLINK02_TASK , "Blink02"  , StartBlink02  , osPriorityBelowNormal , 1024U      , 1100u          , 1100u             },
+    {MAIN_TASK    , "MainTask" , DummyMainTask , osPriorityAboveNormal , 1024U      , 800u           , 800u             },
+    {BLINK01_TASK , "Blink01"  , StartBlink01  , osPriorityNormal      , 1024U      , 500u           , 500u             },
+    {BLINK02_TASK , "Blink02"  , StartBlink02  , osPriorityBelowNormal , 1024U      , 1100u          , 1100u            },
 };
 
 /**
@@ -34,7 +34,7 @@ const taskStaticConf_t g_tasks_static_conf[NB_TASKS] =
 taskDynamicConf_t g_tasks_dynamic_conf[NB_TASKS] = 
 {
     /* Task ID , Task Mode     , Period , Deadline , Last Wake-Up Tick */
-    {0u        , TASK_NOMINAL  , 0u     , 0u       , 0u                 },
-    {0u        , TASK_NOMINAL  , 0u     , 0u       , 0u                 },
-    {0u        , TASK_NOMINAL  , 0u     , 0u       , 0u                 },
+    {0u        , TASK_NOMINAL  , 0u     , 0u       , 0u                },
+    {0u        , TASK_NOMINAL  , 0u     , 0u       , 0u                },
+    {0u        , TASK_NOMINAL  , 0u     , 0u       , 0u                },
 };
