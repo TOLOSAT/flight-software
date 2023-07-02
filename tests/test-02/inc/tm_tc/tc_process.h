5f226b87b256c81a0a08be12ba09647af53c94a2
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-07-02 08:19:02 +0000

    Init feature-tmtcflow

---
+++ tests/test-02/inc/tm_tc/tc_process.h
@@ -0,0 +1,26 @@
+/**
+ * @file tc_process.h
+ * @author Merlin Kooshmanian
+ * @brief Header file for TC_PROCESS Task
+ * @date 28/06/2023
+ * 
+ * Last Update : 28/06/2023
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+#ifndef TC_PROCESS_H
+#define TC_PROCESS_H
+
+/***************************** Include Files *********************************/
+
+#include <stdint.h>
+
+/************************** Constant Definitions *****************************/
+
+/**************************** Type Definitions *******************************/
+
+/************************** Function Prototypes ******************************/
+
+void TcProcessMain(void *current_status);
+
+#endif /* TC_PROCESS_H */
---
+++ tests/test-02/src/tm_tc/tc_scheduler.c
@@ -0,0 +1,58 @@
+/**
+ * @file tc_scheduler.c
+ * @author Merlin Kooshmanian
+ * @brief Source file for TC_SCHEDULER Task
+ * @date 28/06/2023
+ * 
+ * Last Update : 28/06/2023
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+/***************************** Include Files *********************************/
+
+#include <stdio.h>
+#include <cmsis_os2.h>
+
+#include "tm_tc/tc_scheduler.h"
+#include "tasks.h"
+#include "conf/tasks_conf.h"
+#include "buffers.h"
+#include "conf/buffers_conf.h"
+#include "tolosat_hal.h"
+
+/************************** Constant Definitions *****************************/
+
+#define TASK_NAME       "TC_SCHEDULER"      /**< Current Task Name */
+#define TASK_ID         (TC_SCHEDULER_TASK) /**< Current Task ID */
+
+/**************************** Type Definitions *******************************/
+
+/************************** Function Prototypes ******************************/
+
+/************************** Variable Definitions *****************************/
+
+/************************* Functions Definitions *****************************/
+
+/**
+ * @fn      TcSchedulerMain(void *current_status)
+ * @brief   Main of the TC_SCHEDULER Task
+ * @param   current_status Status of the current task
+ */
+void TcSchedulerMain(void *current_status)
+{
+    // Variable Initialisation
+
+    // Initialisation
+    printf("["TASK_NAME"] Init\n");
+    initPeriodicWait(current_status);
+
+    // Function Core
+    while (1)
+    {
+        printf("["TASK_NAME"] Hello\n");
+        waitUntilNextPeriod(current_status);
+    }
+
+    // In case we accidentally exit from task loop
+    osThreadTerminate(NULL);
+}
