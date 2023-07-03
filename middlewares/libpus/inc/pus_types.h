a86af76ee30186760bad10e5dc62a8807b37d43e
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-07-03 16:58:05 +0000

    Force remove padding in pus_types.h and add test variable in makefiles

--- middlewares/libpus/inc/pus_types.h
+++ middlewares/libpus/inc/pus_types.h
@@ -21,10 +21,12 @@
 #define TM_MAX_SIZE         256u    /**< Maximum Size of a TM */
 #define SPP_HEADER_SIZE     6u      /**< Space Packet Header Size */
 #define CRC_TRAILER_SIZE    2u      /**< Size for CRC Trailer */
-#define TC_HEADER_SIZE      4u      /**< Size of a TC Header */
+#define TC_HEADER_SIZE      5u      /**< Size of a TC Header */
 #define TM_HEADER_SIZE      15u     /**< Size of a TM Header */
 #define TC_MAX_DATA_SIZE    (TC_MAX_SIZE - SPP_HEADER_SIZE - TC_HEADER_SIZE - CRC_TRAILER_SIZE) /**< Maximum Size for TC data */
 #define TM_MAX_DATA_SIZE    (TM_MAX_SIZE - SPP_HEADER_SIZE - TM_HEADER_SIZE - CRC_TRAILER_SIZE) /**< Maximum Size for TM data */
+
+#define __BYTE_ALIGNED__  __attribute__((packed, aligned(1)))
 
 /**************************** Type Definitions *******************************/
 
@@ -65,7 +67,7 @@ typedef uint8_t pusSubService_t;
 typedef uint16_t pusMsgCount_t;
 
 /** @brief Source ID Field type */
-typedef uint8_t pusSourceID_t;
+typedef uint16_t pusSourceID_t;
 
 /** @brief Destination ID Field type */
 typedef uint16_t pusDestinationID_t;
@@ -89,7 +91,7 @@ typedef struct
     sppPacketId_t packet_id;                            /**< @brief Packet ID */
     sppPacketSequenceCtrl_t packet_sequence_control;    /**< @brief TMTC counter for this ID */
     sppDataLength_t packet_data_length;                 /**< @brief Packet Data Field Length */
-} sppHeader_t;
+}__BYTE_ALIGNED__ sppHeader_t;
 
 /** 
  * @typedef pusTCHeader_t
@@ -102,7 +104,7 @@ typedef struct
     pusService_t service;           /**< @brief PUS Service */
     pusSubService_t subservice;     /**< @brief PUS SubService */
     pusSourceID_t source_id;        /**< @brief ID of source application */
-} pusTCHeader_t;
+}__BYTE_ALIGNED__ pusTCHeader_t;
 
 /** 
  * @typedef pusTMHeader_t
@@ -117,7 +119,7 @@ typedef struct
     pusMsgCount_t message_counter;      /**< @brief Message counter */
     pusDestinationID_t destination_id;  /**< @brief ID of destination application */
     cucTime_t time;                     /**< @brief OBT when TC has been emitted */
-} pusTMHeader_t;
+}__BYTE_ALIGNED__ pusTMHeader_t;
 
 /** 
  * @typedef pusTC_t
@@ -130,7 +132,7 @@ typedef struct
     pusTCHeader_t tc_header;            /**< @brief PUS TC Header */
     pusData_t data[TC_MAX_DATA_SIZE];   /**< @brief TC Raw Data */
     pusCRC_t crc;                       /**< @brief TC CRC */
-} pusTC_t;
+}__BYTE_ALIGNED__ pusTC_t;
 
 /** 
  * @typedef pusTM_t
@@ -143,7 +145,7 @@ typedef struct
     pusTMHeader_t tm_header;            /**< @brief PUS TM Header */
     pusData_t data[TM_MAX_DATA_SIZE];   /**< @brief TM Raw Data */
     pusCRC_t crc;                       /**< @brief TM CRC */
-} pusTM_t;
+}__BYTE_ALIGNED__ pusTM_t;
 
 /************************** Function Prototypes ******************************/
 
