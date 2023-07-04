c6f924072306d32b23bab7d04e1f1d7656d91951
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-07-04 21:08:59 +0000

    TC receiver now checks TC validity

--- middlewares/libpus/src/pus_tools/tc_management.c
+++ middlewares/libpus/src/pus_tools/tc_management.c
@@ -14,12 +14,27 @@
 
 #include "pus_tools/tc_management.h"
 #include "pus_tools/crc_computation.h"
+#include "pus_tools/endianness_mgmt.h"
 
 /************************** Constant Definitions *****************************/
+
+#define PACKET_VERSION_NUMBER_MASK 0xe000
+#define PACKET_VERSION_NUMBER_OFFSET 13u
+#define VALID_PACKET_VERSION_NUMBER 0b000
+
+#define PACKET_TYPE_MASK 0x1000
+#define PACKET_TYPE_OFFSET 12u
+#define TC_TYPE 0b1
+
+#define HEADER_PRESENCE_MASK 0x0800
+#define HEADER_PRESENCE_OFFSET 11u
+#define HEADER_PRESENT 0b1
 
 /**************************** Type Definitions *******************************/
 
 /************************** Function Prototypes ******************************/
+
+pusStatus_t CheckCRC(pusTC_t *tc);
 
 /************************** Variable Definitions *****************************/
 
@@ -30,9 +45,9 @@
  * @brief   Function that format TC the right way
  * @param   tc Pointer to the TC we want to format
  * @retval  PUS_SUCCESSFUL always
- * 
- * As we've done a silly memcpy with the uart driver, the 
- * TC fields don't have the right endianness, or aren't in 
+ *
+ * As we've done a silly memcpy with the uart driver, the
+ * TC fields don't have the right endianness, or aren't in
  * the right place.
  */
 pusStatus_t FormatTC(pusTC_t *tc)
@@ -41,19 +56,19 @@ pusStatus_t FormatTC(pusTC_t *tc)
     pusStatus_t return_value = PUS_SUCCESSFUL;
 
     // Function Core
-    
+
     // Endianness Correction
-    tc->spp_header.packet_id = ((0xff00 & tc->spp_header.packet_id) >> 8u) | ((0x00ff & tc->spp_header.packet_id) << 8u);
-    tc->spp_header.packet_sequence_control = ((0xff00 & tc->spp_header.packet_sequence_control) >> 8u) | ((0x00ff & tc->spp_header.packet_sequence_control) << 8u);
-    tc->spp_header.packet_data_length = ((0xff00 & tc->spp_header.packet_data_length) >> 8u) | ((0x00ff & tc->spp_header.packet_data_length) << 8u);
-    tc->tc_header.source_id = ((0xff00 & tc->tc_header.source_id) >> 8u) | ((0x00ff & tc->tc_header.source_id) << 8u);
-    
+    tc->spp_header.packet_id = HALF_WORD_BYTE_SWAP(tc->spp_header.packet_id);
+    tc->spp_header.packet_sequence_control = HALF_WORD_BYTE_SWAP(tc->spp_header.packet_sequence_control);
+    tc->spp_header.packet_data_length = HALF_WORD_BYTE_SWAP(tc->spp_header.packet_data_length);
+    tc->tc_header.source_id = HALF_WORD_BYTE_SWAP(tc->tc_header.source_id);
+
     // Put CRC at the right place
-    tc->crc = tc->data[tc->spp_header.packet_data_length - TC_HEADER_SIZE - CRC_TRAILER_SIZE + 1u] << 8u | tc->data[tc->spp_header.packet_data_length - TC_HEADER_SIZE - CRC_TRAILER_SIZE + 2u];
+    tc->crc = ARRAY_TO_UINT16_BIG_ENDIAN((tc->data + tc->spp_header.packet_data_length - TC_HEADER_SIZE - CRC_TRAILER_SIZE + 1u));
     tc->data[tc->spp_header.packet_data_length - TC_HEADER_SIZE - CRC_TRAILER_SIZE + 1u] = 0u;
     tc->data[tc->spp_header.packet_data_length - TC_HEADER_SIZE - CRC_TRAILER_SIZE + 2u] = 0u;
 
-    return(return_value);
+    return (return_value);
 }
 
 /**
@@ -67,37 +82,81 @@ pusStatus_t CheckCRC(pusTC_t *tc)
 {
     // Variable Initialisation
     pusStatus_t return_value = PUS_SUCCESSFUL;
+    uint16_t data_size = HALF_WORD_BYTE_SWAP(tc->spp_header.packet_data_length) + 1u;
+    pusCRC_t reiceved_crc = ARRAY_TO_UINT16_BIG_ENDIAN((tc->data + data_size - TC_HEADER_SIZE - CRC_TRAILER_SIZE));
     pusCRC_t computed_crc = 0u;
-    pusCRC_t reiceved_crc = 0u;
-    uint32_t data_size = 0u;
 
     // Function Core
-    data_size = (((0xff00 & tc->spp_header.packet_data_length) >> 8u) | ((0x00ff & tc->spp_header.packet_data_length) << 8u)) + 1u;
-    computed_crc = computeCRC((uint8_t *) tc, data_size + SPP_HEADER_SIZE - CRC_TRAILER_SIZE);
-    reiceved_crc = tc->data[data_size - TC_HEADER_SIZE - CRC_TRAILER_SIZE] << 8u | tc->data[data_size - TC_HEADER_SIZE - CRC_TRAILER_SIZE + 1u];
-
-    if(computed_crc != reiceved_crc){
+    computed_crc = computeCRC((uint8_t *)tc, data_size + SPP_HEADER_SIZE - CRC_TRAILER_SIZE);
+    if (computed_crc != reiceved_crc)
+    {
         return_value = PUS_ERROR;
     }
 
-    return(return_value);
+    return (return_value);
 }
 
 /**
- * @fn      CheckCRC(pusTC_t *tc)
- * @brief   Function that verifies if APID, Service and Subservice of the TC are valid
+ * @fn      CheckTCValidity(pusTC_t *tc, pusAcceptanceError_t *error)
+ * @brief   Function that verifies if TC is valid (right version, type, size)
  * @param   tc Pointer to the TC variable where we want to verify it validity.
- * @retval  PUS_ERROR if 
+ * @param   error Pointer to pass error type to TM(1,2)
+ * @retval  PUS_ERROR if
  * @retval  PUS_SUCCESSFUL else
  */
-pusStatus_t CheckTCValidity(pusTC_t *tc)
+pusStatus_t CheckTCValidity(pusTC_t *tc, pusAcceptanceError_t *error)
 {
     // Variable Initialisation
     pusStatus_t return_value = PUS_SUCCESSFUL;
+    uint16_t packet_id = HALF_WORD_BYTE_SWAP(tc->spp_header.packet_id);
+    uint16_t data_size = HALF_WORD_BYTE_SWAP(tc->spp_header.packet_data_length) + 1u;
 
     // Function Core
+    // Check Packet Version Number
+    if (((packet_id & PACKET_VERSION_NUMBER_MASK) >> PACKET_VERSION_NUMBER_OFFSET) == VALID_PACKET_VERSION_NUMBER)
+    {
+        // Check Packet Type
+        if (((packet_id & PACKET_TYPE_MASK) >> PACKET_TYPE_OFFSET) == TC_TYPE)
+        {
+            // Check Secondary Header Presence
+            if (((packet_id & HEADER_PRESENCE_MASK) >> HEADER_PRESENCE_OFFSET) == HEADER_PRESENT)
+            {
+                // Check Size
+                if (data_size >= TC_HEADER_SIZE + CRC_TRAILER_SIZE)
+                {
+                    // Check CRC
+                    if(CheckCRC(tc) != PUS_SUCCESSFUL)
+                    {
+                        return_value = PUS_ERROR;
+                        *error = PUS_ACCEPTANCE_INVALID_CRC;
+                    }
 
-    return(return_value);
+                }
+                else
+                {
+                    return_value = PUS_ERROR;
+                    *error = PUS_ACCEPTANCE_INVALID_FORMAT;
+                }
+            }
+            else
+            {
+                return_value = PUS_ERROR;
+                *error = PUS_ACCEPTANCE_INVALID_FORMAT;
+            }
+        }
+        else
+        {
+            return_value = PUS_ERROR;
+            *error = PUS_ACCEPTANCE_INVALID_FORMAT;
+        }
+    }
+    else
+    {
+        return_value = PUS_ERROR;
+        *error = PUS_ACCEPTANCE_INVALID_FORMAT;
+    }
+
+    return (return_value);
 }
 
 /**
@@ -112,7 +171,7 @@ pusStatus_t EraseTC(pusTC_t *tc)
     pusStatus_t return_value = PUS_SUCCESSFUL;
 
     // Function Core
-    memset((uint8_t*) tc, 0u, TC_MAX_SIZE);
+    memset((uint8_t *)tc, 0u, TC_MAX_SIZE);
 
-    return(return_value);
+    return (return_value);
 }
