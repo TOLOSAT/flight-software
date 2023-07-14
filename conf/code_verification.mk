2ce01daf2da75eb80f8e972a69b8fab6c8d3b0c5
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-07-14 19:32:48 +0200

    Code correction

--- conf/code_verification.mk
+++ conf/code_verification.mk
@@ -4,12 +4,17 @@
 ################ CHECKER FILES ###############
 ##############################################
 
-CHECKER_SRCS  = $(MAIN_SRCS) $(APPLICATION_SRCS) $(LIBPUS_SRCS) $(BSP_SRCS) $(HAL_TOLOSAT_SRCS)
-CHECKER_INCS += -I$(MAIN_INCDIR)
-CHECKER_INCS += -I$(APPLICATION_INCDIR)
-CHECKER_INCS += -I$(HAL_INCDIR) -I$(HAL_INCDIR)/Legacy -I$(CONF_HALS_DIR)
-CHECKER_INCS += -I$(HAL_TOLOSAT_INCDIR)
-CHECKER_INCS += -I$(BSP_INCDIR)
+CHECKER_SRCS =	$(MAIN_SRCS) \
+				$(APPLICATION_SRCS) \
+				$(LIBPUS_SRCS) \
+				$(HAL_TOLOSAT_SRCS) \
+				$(BSP_SRCS)
+
+CHECKER_INCS =	-I$(MAIN_INCDIR) \
+				-I$(APPLICATION_INCDIR) \
+				-I$(LIBPUS_INCDIR) \
+				-I$(HAL_TOLOSAT_INCDIR) \
+				-I$(BSP_INCDIR)
 
 ##############################################
 ############### CHECKER CONFIGS ##############
@@ -17,6 +22,7 @@ CHECKER_INCS += -I$(BSP_INCDIR)
 
 CHECKER_CMDS  = --enable=all # Enables all warnings
 CHECKER_CMDS += --suppress=missingInclude # Disables missing include warnings
+CHECKER_CMDS += --suppress=variableScope # Disables variable scope warnings
 CHECKER_CMDS += --suppress=*:$(BSP_SRCDIR)/system*.c # Disables all warning for system file (ST wrote this file)
 CHECKER_CMDS += --suppress=*:$(BSP_SRCDIR)/*hal_msp.c # Disables all warning for system file (ST wrote this file)
 CHECKER_CMDS += --suppress=unusedFunction # Disables unused function warnings
