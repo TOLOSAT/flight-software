aca747f72f7b477b8f6d6409213ab72b2cd21fa8
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-07-23 12:54:01 +0000

    Make rtc_inst local in the HAL

---
+++ middlewares/libpus/src/pus_tools/time_management.c
@@ -0,0 +1,24 @@
+/**
+ * @file time_management.c
+ * @author Merlin Kooshmanian
+ * @brief Source file for Time Management
+ * @date 23/07/2023
+ *
+ * Last Update : 23/07/2023
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+/******************************* Include Files *******************************/
+
+#include "pus_tools/time_management.h"
+
+/***************************** Macros Definitions ****************************/
+
+/***************************** Types Definitions *****************************/
+
+/**************************** Functions Prototypes ***************************/
+
+/*************************** Variables Definitions ***************************/
+
+/*************************** Functions Definitions ***************************/
+
