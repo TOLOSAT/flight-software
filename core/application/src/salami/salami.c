cc1be3ba881aa95d096301d3c065ddad795dc571
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-07-30 21:15:39 +0000

    Add error on purpose to check if the action works

--- core/application/src/salami/salami.c
+++ core/application/src/salami/salami.c
@@ -46,6 +46,8 @@ void SalamiMain(void *task_dyn_conf)
     // Function Core
     while (1)
     {
+        uint8_t i;
+        i++;
         (void) GpioToggle(&led_inst);
         
         task_status = waitUntilNextPeriod(task_dyn_conf);
