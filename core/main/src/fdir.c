562746c09dba9536b89422985304e1cdf4943118
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-07-30 15:06:52 +0000

    Improved a bit FDIR

--- core/main/src/fdir.c
+++ core/main/src/fdir.c
@@ -22,7 +22,7 @@
 /*************************** Functions Declarations **************************/
 
 extern void Reset_Handler(void); /**< Reset Handler defined in startup.c file */
-void vApplicationStackOverflowHook(TaskHandle_t xTask, char * pcTaskName);
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName);
 
 /*************************** Variables Definitions ***************************/
 
@@ -36,29 +36,55 @@ void vApplicationStackOverflowHook(TaskHandle_t xTask, char * pcTaskName);
  */
 void CheckErrors(uint32_t status, errorsSanction_t sanction)
 {
-    if(status != 0u)
+    if (status != 0u)
     {
-        if(sanction == ERROR_HANDLER)
+        // Check which sanction we have to take
+        if (sanction == FDIR_SYSTEM_RESET)
         {
+            // Reset processor
+            NVIC_SystemReset();
+            Reset_Handler();
+        }
+        else if (sanction == FDIR_ERROR_HANDLER)
+        {
+            // Go to error handler
             Error_Handler();
         }
-        else if (sanction == RESET_HANDLER)
+        else if (sanction == FDIR_HALT_TASK)
         {
-            Reset_Handler();
+            osThreadId_t current_task_id = NULL;
+            current_task_id = osThreadGetId();
+            // Check if we are in a task
+            if (current_task_id != NULL)
+            {
+                // If we are in the task we suspend it
+                osThreadSuspend(current_task_id);
+            }
+            else
+            {
+                // If we are not in a task then we go to error handler by default
+                Error_Handler();
+            }
         }
         else
         {
-            /* No Sanction needed */
+            // No Sanction needed
         }
     }
     else
     {
-        /* No errors */
+        // No errors
     }
 }
 
 /**
- * @brief  This function is executed in case of error occurrence.
+ * @fn      Error_Handler(void)
+ * @brief   This function is executed in case of error occurrence.
+ * @warning Real FDIR has to be done.
+ * 
+ * Normally this function has to save some context (what happened before error). 
+ * But currently no real fdir has been done. For debugging purposes, Error handler 
+ * is just a while loop that hangs processor indefinitly.
  */
 void Error_Handler(void)
 {
@@ -72,14 +98,14 @@ void Error_Handler(void)
 /**
  * @brief  This function is executed if a task runs out of stack
  */
-void vApplicationStackOverflowHook(TaskHandle_t xTask, char * pcTaskName)
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
 {
     // Unused Parameters
     (void)xTask;
     (void)pcTaskName;
 
     // Function Core
-    while(1)
+    while (1)
     {
         /* Do Nothing */
     }
