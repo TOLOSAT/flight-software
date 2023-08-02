51132fd75ad553c360439f23a35c1cead20e956f
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-08-02 21:13:41 +0200

    Modify action to check documentation too

--- update-doc.sh
+++ update-doc.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+# Check if the "build" directory exists, if not, create it
+if [ ! -d ./build ]; then
+    mkdir ./build
+fi
+
 # Check for the presence of the doxygen.log file and the html folder
-if [ -e ./doc/doxygen/doxygen.log ] || [ -d ./doc/doxygen/html ]; then
+if [ -e ./build/doxygen/doxygen.log ] || [ -d ./build/doxygen/html ]; then
     # Deletes the file and folder if they exist
-    rm -rf ./doc/doxygen/doxygen.log ./doc/doxygen/html
+    rm -rf ./build/doxygen/doxygen.log ./build/doxygen/html
 fi
 
 # Run the doxygen command using the Doxyfile configuration file
 doxygen ./doc/doxygen/Doxyfile
+doxygen_return=$?
+
+# Check if the documentation generation was successful
+if [ $doxygen_return -eq 0 ]; then
+    # Create an alias for index.html in the "build" directory
+    ln ./build/doxygen/html/index.html ./build/doxygen/index.html
+fi
+
+# Return the code of the doxygen command as the script's exit code
+exit $doxygen_return
