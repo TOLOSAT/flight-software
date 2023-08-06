eed6d9eac0a801b857d06e52b8b94127a610af77
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-08-06 10:08:43 +0000

    Add function to check tables

--- middlewares/libpus/inc/pus_tools/tables_management.h
+++ middlewares/libpus/inc/pus_tools/tables_management.h
@@ -73,6 +73,8 @@ typedef struct {
 
 pusStatus_t RouteSearch(pusRoutingTable_t *g_routing_table, pusTableSize_t table_size, uint32_t key, uint32_t *route);
 pusStatus_t ExecutionSearch(pusExecutionTable_t *g_execution_table, pusTableSize_t table_size, uint32_t key, pusTMRequested_t *tm_requested, pusExecutionFunctionPtr_t *execution_function_ptr);
+pusStatus_t CheckRoutingTable(pusRoutingTable_t *g_routing_table, pusTableSize_t table_size);
+pusStatus_t CheckExecutionTable(pusExecutionTable_t *g_execution_table, pusTableSize_t table_size);
 
 #endif /* TABLES_MANAGEMENT_H */
 
--- middlewares/libpus/src/pus_tools/tables_managements.c
+++ middlewares/libpus/src/pus_tools/tables_managements.c
@@ -26,8 +26,8 @@
  * @param[in]   table_size Size of the routing table
  * @param[in]   key Key that help us to find the route.
  * @param[out]  route Route we are looking for
- * @retval      PUS_ERROR if key does not exist in routing table
- * @retval      PUS_SUCCESSFUL else
+ * @retval      #PUS_ERROR if key does not exist in routing table
+ * @retval      #PUS_SUCCESSFUL else
  */
 pusStatus_t RouteSearch(pusRoutingTable_t *g_routing_table, pusTableSize_t table_size, uint32_t key, uint32_t *route)
 {
@@ -62,14 +62,14 @@ pusStatus_t RouteSearch(pusRoutingTable_t *g_routing_table, pusTableSize_t table
 
 /**
  * @fn          ExecutionSearch(pusExecutionTable_t *g_execution_table, pusTableSize_t table_size, uint32_t key, pusTMRequested_t *tm_requested, pusExecutionFunctionPtr_t *execution_function_ptr)
- * @brief       This function search for exeution function in execution table with a key
+ * @brief       This function search for execution function in execution table with a key
  * @param[in]   g_execution_table Execution table where we search the function to execute
- * @param[in]   table_size Size of the routing table
+ * @param[in]   table_size Size of the execution table
  * @param[in]   key Key that help us to find the route.
  * @param[out]  execution_function_ptr Pointer to the function we want to execute
  * @param[out]  tm_requested Indicates if a specific TM has to be send for this TC
- * @retval      PUS_ERROR if key does not exist in routing table
- * @retval      PUS_SUCCESSFUL else
+ * @retval      #PUS_ERROR if key does not exist in routing table
+ * @retval      #PUS_SUCCESSFUL else
  */
 pusStatus_t ExecutionSearch(pusExecutionTable_t *g_execution_table, pusTableSize_t table_size, uint32_t key, pusTMRequested_t *tm_requested, pusExecutionFunctionPtr_t *execution_function_ptr)
 {
@@ -98,6 +98,84 @@ pusStatus_t ExecutionSearch(pusExecutionTable_t *g_execution_table, pusTableSize
             right = cursor - 1u;
             cursor = left + (right - left) / 2u;
         }
+    }
+
+    return return_value;
+}
+
+/**
+ * @fn          CheckRoutingTable(pusRoutingTable_t *g_routing_table, pusTableSize_t table_size)
+ * @brief       Check if the table is ordered from smallest to largest key
+ * @param[in]   g_routing_table routing table we want to check
+ * @param[in]   table_size Size of the routing table
+ * @retval      #PUS_ERROR if the table is not ordered from smallest to largest key
+ * @retval      #PUS_INVALID_PARAM if table size is 0 or if table is null pointer
+ * @retval      #PUS_SUCCESSFUL else
+ */
+pusStatus_t CheckRoutingTable(pusRoutingTable_t *g_routing_table, pusTableSize_t table_size)
+{
+    // Variable Initialisation
+    pusStatus_t return_value = PUS_SUCCESSFUL;
+
+    // Function Core
+    if ((g_routing_table != NULL) && (table_size > 0u))
+    {
+        uint32_t i = 0u;
+        uint32_t last_key = 0u;
+        while((i < table_size) && (g_routing_table[i].key > last_key))
+        {
+            last_key = g_routing_table[i].key;
+            i++;
+        }
+        // Checks whether the entire table has been browsed. 
+        // If this is not the case, the table is not ordered.
+        if(i != table_size)
+        {
+            return_value = PUS_ERROR;
+        }
+    }
+    else
+    {
+        return_value = PUS_INVALID_PARAM;
+    }
+
+    return return_value;
+}
+
+/**
+ * @fn          CheckExecutionTable(pusExecutionTable_t *g_execution_table, pusTableSize_t table_size)
+ * @brief       Check if the table is ordered from smallest to largest key
+ * @param[in]   g_execution_table Execution table we want to check
+ * @param[in]   table_size Size of the execution table
+ * @retval      #PUS_ERROR if the table is not ordered from smallest to largest key
+ * @retval      #PUS_INVALID_PARAM if table size is 0 or if table is null pointer
+ * @retval      #PUS_SUCCESSFUL else
+ */
+pusStatus_t CheckExecutionTable(pusExecutionTable_t *g_execution_table, pusTableSize_t table_size)
+{
+    // Variable Initialisation
+    pusStatus_t return_value = PUS_SUCCESSFUL;
+
+    // Function Core
+    if ((g_execution_table != NULL) && (table_size > 0u))
+    {
+        uint32_t i = 0u;
+        uint32_t last_key = 0u;
+        while((i < table_size) && (g_execution_table[i].key > last_key))
+        {
+            last_key = g_execution_table[i].key;
+            i++;
+        }
+        // Checks whether the entire table has been browsed. 
+        // If this is not the case, the table is not ordered.
+        if(i != table_size)
+        {
+            return_value = PUS_ERROR;
+        }
+    }
+    else
+    {
+        return_value = PUS_INVALID_PARAM;
     }
 
     return return_value;
