9f650e40a9278372fac43aa3a9dc6ff894fef42b
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-08-08 20:26:13 +0000

    Add msp for SPI

--- conf/conf_HALs/stm32f1xx_hal_conf.mk
+++ conf/conf_HALs/stm32f1xx_hal_conf.mk
@@ -4,6 +4,7 @@ HAL_SRCS  = $(HAL_SRCDIR)/stm32f1xx_hal.c \
 			$(HAL_SRCDIR)/stm32f1xx_hal_gpio.c \
 			$(HAL_SRCDIR)/stm32f1xx_hal_uart.c \
 			$(HAL_SRCDIR)/stm32f1xx_hal_i2c.c \
+			$(HAL_SRCDIR)/stm32f1xx_hal_spi.c \
 			$(HAL_SRCDIR)/stm32f1xx_hal_dma.c \
 			$(HAL_SRCDIR)/stm32f1xx_hal_rcc.c \
 			$(HAL_SRCDIR)/stm32f1xx_hal_rcc_ex.c \
