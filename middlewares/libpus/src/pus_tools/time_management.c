3cc15a97c12d58a63da1476be9206a57a3cf7b03
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-08-12 12:53:15 +0200

    Now time management functions used proprietary functions (MISRA compliant)

--- middlewares/libpus/src/pus_tools/time_management.c
+++ middlewares/libpus/src/pus_tools/time_management.c
@@ -9,8 +9,6 @@
 
 /******************************* Include Files *******************************/
 
-#include <time.h> // cppcheck-suppress misra-c2012-21.10
-
 #include "pus_tools/time_management.h"
 #include "tolosat_hal.h"
 
@@ -25,7 +23,7 @@
 
 /*************************** Functions Declarations **************************/
 
-static void ConvertRTCtoReadableTime(const rtcTime_t *rtc_time, struct tm *readable_time);
+static pusStatus_t ConvertRTCTimeToUnixTimestamp(rtcTime_t rtc_time, uint32_t *unix_timestamp);
 static pusStatus_t ConvertUnixTimestampToRTCTime(uint32_t unix_timestamp, rtcTime_t *rtc_time);
 
 /*************************** Variables Definitions ***************************/
@@ -45,7 +43,6 @@ pusStatus_t GetCUCTime(cucTime_t *cuc_time)
     // Variable Initialisation
     pusStatus_t return_value = PUS_SUCCESSFUL;
     rtcTime_t rtc_time = {0};
-    struct tm readable_time = {0};
 
     // Function Core
     if (cuc_time != NULL)
@@ -54,18 +51,22 @@ pusStatus_t GetCUCTime(cucTime_t *cuc_time)
         halStatus_t test_val = RtcGetTime(&rtc_time);
         if (test_val == FCT_SUCCESSFUL)
         {
-            // Convert RTC to UNIX Time
-            ConvertRTCtoReadableTime(&rtc_time, &readable_time);
-            time_t unix_time = mktime(&readable_time);
+            // Convert RTC to RAW CUC Time (TAI)
+            uint32_t raw_cuc_time;
+            return_value = ConvertRTCTimeToUnixTimestamp(rtc_time, &raw_cuc_time);
+            if (return_value == PUS_SUCCESSFUL)
+            {
+                // Add TAI offset
+                raw_cuc_time += TAI_UNIX_OFFSET;
 
-            // Convert UNIX Time to CUC Time
-            uint32_t raw_cuc_time = (uint32_t)(unix_time) + TAI_UNIX_OFFSET;
-            cuc_time->time_header = TIME_HEAD_CONSTANT;
-            cuc_time->coarse_time[0] = (uint8_t)((0xff000000u & raw_cuc_time) >> 24u);
-            cuc_time->coarse_time[1] = (uint8_t)((0x00ff0000u & raw_cuc_time) >> 16u);
-            cuc_time->coarse_time[2] = (uint8_t)((0x0000ff00u & raw_cuc_time) >> 8u);
-            cuc_time->coarse_time[3] = (uint8_t)(0x000000ffu & raw_cuc_time);
-            cuc_time->fine_time[0] = rtc_time.millisecond * 256u / 1000u;
+                // Convert RAW CUC Time to CUC Time
+                cuc_time->time_header = TIME_HEAD_CONSTANT;
+                cuc_time->coarse_time[0] = (uint8_t)((0xff000000u & raw_cuc_time) >> 24u);
+                cuc_time->coarse_time[1] = (uint8_t)((0x00ff0000u & raw_cuc_time) >> 16u);
+                cuc_time->coarse_time[2] = (uint8_t)((0x0000ff00u & raw_cuc_time) >> 8u);
+                cuc_time->coarse_time[3] = (uint8_t)(0x000000ffu & raw_cuc_time);
+                cuc_time->fine_time[0] = rtc_time.millisecond * 256u / 1000u;
+            }
         }
         else
         {
@@ -98,10 +99,10 @@ pusStatus_t SetCUCTime(cucTime_t *cuc_time)
     if (cuc_time != NULL)
     {
         // Get UNIX Time from CUC Time
-        time_t unix_time = ((time_t)(cuc_time->coarse_time[3]) << 24u) +
-                           ((time_t)(cuc_time->coarse_time[2]) << 16u) +
-                           ((time_t)(cuc_time->coarse_time[1]) << 8u) +
-                           ((time_t)(cuc_time->coarse_time[0])) - TAI_UNIX_OFFSET;
+        uint32_t unix_time = ((uint32_t)(cuc_time->coarse_time[3]) << 24u) +
+                             ((uint32_t)(cuc_time->coarse_time[2]) << 16u) +
+                             ((uint32_t)(cuc_time->coarse_time[1]) << 8u) +
+                             ((uint32_t)(cuc_time->coarse_time[0])) - TAI_UNIX_OFFSET;
         // Convert UNIX Time to RTC Time
         return_value = ConvertUnixTimestampToRTCTime(unix_time, &rtc_time);
         if (return_value == PUS_SUCCESSFUL)
@@ -123,19 +124,56 @@ pusStatus_t SetCUCTime(cucTime_t *cuc_time)
 }
 
 /**
- * @fn          ConvertRTCtoReadableTime(rtcTime_t *rtc_time, struct tm *readable_time)
- * @brief       Function that convert rtc time into readable time (from libc)
- * @param[in]   rtc_time
- * @param[out]  readable_time
+ * @fn          ConvertRTCTimeToUnixTimestamp(rtcTime_t rtc_time, uint32_t *unix_timestamp)
+ * @brief       Function that convert RTC time into Unix timestamp
+ * @param[in]   rtc_time RTC time (as it has been defined in TOLOSAT HAL)
+ * @param[out]  unix_timestamp Timestamp Unix (number of seconds since january 1, 1970)
+ * @retval      #PUS_INVALID_PARAM if a timestamp is NULL
+ * @retval      #PUS_SUCCESSFUL else
  */
-static void ConvertRTCtoReadableTime(const rtcTime_t *rtc_time, struct tm *readable_time)
+static pusStatus_t ConvertRTCTimeToUnixTimestamp(rtcTime_t rtc_time, uint32_t *unix_timestamp)
 {
-    readable_time->tm_year = (int)(rtc_time->year) + 2000 - 1900;
-    readable_time->tm_mon = (int)(rtc_time->month) - 1;
-    readable_time->tm_mday = (int)(rtc_time->day);
-    readable_time->tm_hour = (int)(rtc_time->hour);
-    readable_time->tm_min = (int)(rtc_time->minute);
-    readable_time->tm_sec = (int)(rtc_time->second);
+    // Variable Initialisation
+    pusStatus_t return_value = PUS_SUCCESSFUL;
+
+    // Function Core
+    if (unix_timestamp != NULL)
+    {
+        // Numbers of day each month
+        const uint8_t DAYS_IN_MONTH[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        // Compute total of days elapsed since january 1rst 1970
+        uint32_t total_days = (rtc_time.year + 30u) * DAYS_IN_YEAR;
+        for (uint16_t i = 1970u; i < ((uint16_t)rtc_time.year + 2000u); i++)
+        {
+            if ((((i % 4u) == 0u) && ((i % 100u) != 0u)) || ((i % 400u) == 0u))
+            {
+                total_days++; // Add 1 day for leap years
+            }
+        }
+
+        for (uint8_t i = 0u; i < (rtc_time.month - 1u); i++)
+        {
+            total_days += DAYS_IN_MONTH[i];
+            if ((i == 1u) && ((((rtc_time.year % 4u) == 0u) && ((rtc_time.year % 100u) != 0u)) || ((rtc_time.year % 400u) == 0u)))
+            {
+                total_days++; // Add 1 day for february month during leap year
+            }
+        }
+        total_days += rtc_time.day - 1u;
+
+        // Total seconds elapsed computation
+        uint32_t total_seconds = (total_days * SECONDS_IN_DAY) + (rtc_time.hour * SECONDS_IN_HOUR) + (rtc_time.minute * SECONDS_IN_MINUTE) + rtc_time.second;
+
+        // Update unix_timestamp with total_second
+        *unix_timestamp = total_seconds;
+    }
+    else
+    {
+        return_value = PUS_INVALID_PARAM;
+    }
+
+    return return_value;
 }
 
 /**
@@ -179,16 +217,16 @@ static pusStatus_t ConvertUnixTimestampToRTCTime(uint32_t unix_timestamp, rtcTim
             rtc_time->year = (uint8_t)(year - 2000u);
 
             // Month and Month Day Calculation
-            const uint8_t daysInMonth[12] = {31u, 28u, 31u, 30u, 31u, 30u, 31u, 31u, 30u, 31u, 30u, 31u};
+            const uint8_t days_in_month[12] = {31u, 28u, 31u, 30u, 31u, 30u, 31u, 31u, 30u, 31u, 30u, 31u};
             rtc_time->month = 1u;
-            while ((rtc_time->month <= 12u) && (timestamp >= (SECONDS_IN_DAY * daysInMonth[rtc_time->month - 1u])))
+            while ((rtc_time->month <= 12u) && (timestamp >= (SECONDS_IN_DAY * days_in_month[rtc_time->month - 1u])))
             {
-                uint8_t daysInCurrentMonth = daysInMonth[rtc_time->month - 1u];
+                uint8_t days_in_current_month = days_in_month[rtc_time->month - 1u];
                 if ((rtc_time->month == 2u) && ((((year % 4u) == 0u) && ((year % 100u) != 0u)) || ((year % 400u) == 0u)))
                 {
-                    daysInCurrentMonth = 29u;
+                    days_in_current_month = 29u;
                 }
-                timestamp -= (SECONDS_IN_DAY * daysInCurrentMonth);
+                timestamp -= (SECONDS_IN_DAY * days_in_current_month);
                 rtc_time->month++;
             }
             rtc_time->day = (timestamp / SECONDS_IN_DAY) + 1u;
