11cfbe782f3a424d4947652ee0871fe9f87ce331
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-08-18 04:40:20 +0000

    Init FS files

---
+++ tools/HALs/HAL-TOLOSAT/inc/tolosat_hal_spi.h
@@ -0,0 +1,39 @@
+/**
+ * @file    tolosat_hal_spi.h
+ * @author  Merlin Kooshmanian
+ * @brief   Header file for TOLOSAT HAL SPI functions
+ * @date    18/08/2023
+ *
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+/**
+ * @defgroup hal_tolosat TOLOSAT HAL
+ * @{
+ * @defgroup drv_spi Driver SPI
+ * Functions that allows to drive SPI
+ * @{
+ */
+
+#ifndef TOLOSAT_HAL_SPI_H
+#define TOLOSAT_HAL_SPI_H
+
+/******************************* Include Files *******************************/
+
+#include "tolosat_hal_types.h"
+
+/***************************** Macros Definitions ****************************/
+
+/***************************** Types Definitions *****************************/
+
+/*************************** Variables Declarations **************************/
+
+/*************************** Functions Declarations **************************/
+
+
+#endif /* TOLOSAT_HAL_SPI_H */
+
+/** 
+ * @} 
+ * @} 
+ */
