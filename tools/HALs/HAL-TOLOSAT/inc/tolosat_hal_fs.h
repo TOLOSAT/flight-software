8d7096b014a02890543e7859aa04dc0f4617c870
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-08-18 04:40:20 +0000

    Init FS files

---
+++ tools/HALs/HAL-TOLOSAT/inc/tolosat_hal_fs.h
@@ -0,0 +1,39 @@
+/**
+ * @file    tolosat_hal_fs.h
+ * @author  Merlin Kooshmanian
+ * @brief   Header file for TOLOSAT HAL File System functions
+ * @date    18/08/2023
+ *
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+/**
+ * @defgroup hal_tolosat TOLOSAT HAL
+ * @{
+ * @defgroup drv_fs Driver FS
+ * Functions that allows to drive File System
+ * @{
+ */
+
+#ifndef TOLOSAT_HAL_FS_H
+#define TOLOSAT_HAL_FS_H
+
+/******************************* Include Files *******************************/
+
+#include "tolosat_hal_types.h"
+
+/***************************** Macros Definitions ****************************/
+
+/***************************** Types Definitions *****************************/
+
+/*************************** Variables Declarations **************************/
+
+/*************************** Functions Declarations **************************/
+
+
+#endif /* TOLOSAT_HAL_FS_H */
+
+/** 
+ * @} 
+ * @} 
+ */
