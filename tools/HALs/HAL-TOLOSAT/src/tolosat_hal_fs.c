91c8cfc614654abd0e9f5a2368eb780574a63f6d
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-08-18 04:40:20 +0000

    Init FS files

---
+++ tools/HALs/HAL-TOLOSAT/src/tolosat_hal_fs.c
@@ -0,0 +1,20 @@
+/**
+ * @file    tolosat_hal_fs.c
+ * @author  Merlin Kooshmanian
+ * @brief   Source file for TOLOSAT HAL File System functions
+ * @date    18/08/2023
+ *
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+/******************************* Include Files *******************************/
+
+#include "tolosat_hal.h"
+
+/***************************** Macros Definitions ****************************/
+
+/*************************** Functions Declarations **************************/
+
+/*************************** Variables Definitions ***************************/
+
+/*************************** Functions Definitions ***************************/
---
+++ tools/HALs/HAL-TOLOSAT/src/tolosat_hal_spi.c
@@ -0,0 +1,20 @@
+/**
+ * @file    tolosat_hal_spi.c
+ * @author  Merlin Kooshmanian
+ * @brief   Source file for TOLOSAT HAL SPI functions
+ * @date    18/08/2023
+ *
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+/******************************* Include Files *******************************/
+
+#include "tolosat_hal.h"
+
+/***************************** Macros Definitions ****************************/
+
+/*************************** Functions Declarations **************************/
+
+/*************************** Variables Definitions ***************************/
+
+/*************************** Functions Definitions ***************************/
