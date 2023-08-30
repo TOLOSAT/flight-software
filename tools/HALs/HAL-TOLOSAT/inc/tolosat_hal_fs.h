90f7a14ecc1e5d0273c0c893dcc86ad7af022e8d
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-08-30 21:24:32 +0000

    Functions TxDataBlock and RxDataBlock has been cleaned

--- tools/HALs/HAL-TOLOSAT/inc/tolosat_hal_fs.h
+++ tools/HALs/HAL-TOLOSAT/inc/tolosat_hal_fs.h
@@ -24,41 +24,53 @@
 
 /***************************** Macros Definitions ****************************/
 
-#define DISK0_REF               0x00u   /**< Disk0 reference */
+#define DISK0_REF                   0x00u       /**< Disk0 reference */
 
 /* Definitions for MMC/SDC command */
-#define CMD_MSG_SIZE            6u      /**< Command Size*/
-#define CMD0                    0x40u   /**< Command GO_IDLE_STATE */
-#define CMD1                    0x41u   /**< Command SEND_OP_COND */
-#define CMD8                    0x48u   /**< Command SEND_IF_COND */
-#define CMD9                    0x49u   /**< Command SEND_CSD */
-#define CMD10                   0x4au   /**< Command SEND_CID */
-#define CMD12                   0x4cu   /**< Command STOP_TRANSMISSION */
-#define CMD16                   0x50u   /**< Command SET_BLOCKLEN */
-#define CMD17                   0x51u   /**< Command READ_SINGLE_BLOCK */
-#define CMD18                   0x52u   /**< Command READ_MULTIPLE_BLOCK */
-#define CMD23                   0x57u   /**< Command SET_BLOCK_COUNT */
-#define CMD24                   0x58u   /**< Command WRITE_BLOCK */
-#define CMD25                   0x59u   /**< Command WRITE_MULTIPLE_BLOCK */
-#define CMD41                   0x69u   /**< Command SEND_OP_COND (ACMD) */
-#define CMD55                   0x77u   /**< Command APP_CMD */
-#define CMD58                   0x7au   /**< Command READ_OCR */
+#define CMD_MSG_SIZE                6u          /**< Command Size*/
+#define CMD0                        0x40u       /**< Command GO_IDLE_STATE */
+#define CMD1                        0x41u       /**< Command SEND_OP_COND */
+#define CMD8                        0x48u       /**< Command SEND_IF_COND */
+#define CMD9                        0x49u       /**< Command SEND_CSD */
+#define CMD10                       0x4au       /**< Command SEND_CID */
+#define CMD12                       0x4cu       /**< Command STOP_TRANSMISSION */
+#define CMD16                       0x50u       /**< Command SET_BLOCKLEN */
+#define CMD17                       0x51u       /**< Command READ_SINGLE_BLOCK */
+#define CMD18                       0x52u       /**< Command READ_MULTIPLE_BLOCK */
+#define CMD23                       0x57u       /**< Command SET_BLOCK_COUNT */
+#define CMD24                       0x58u       /**< Command WRITE_BLOCK */
+#define CMD25                       0x59u       /**< Command WRITE_MULTIPLE_BLOCK */
+#define CMD41                       0x69u       /**< Command SEND_OP_COND (ACMD) */
+#define CMD55                       0x77u       /**< Command APP_CMD */
+#define CMD58                       0x7au       /**< Command READ_OCR */
+
+/* Definition for MMC/SDC tokens */
+#define SD_START_BLOCK_TOKEN        0xfeu        /**< Token notifying start of a 512 bits block */
+#define SD_START_MULT_BLOCK_TOKEN   0xfcu        /**< Token notifying start of multiple 512 bits blocks */
+#define SD_STOP_TOKEN               0xfdu        /**< Token stoping 512 bits block transaction */
+
+/* Definition for MMC/SDC transmit data response */
+#define SD_DATA_RESPONSE_MASK       0x1fu        /**< SPI data transmit data response mask */
+#define SD_DATA_ACCEPTED            0x05u        /**< SPI data transmit data accepted */
+#define SD_DATA_CRC_ERROR           0x0bu        /**< SPI data transmit data crc error */
+#define SD_DATA_WRITE_ERROR         0x0du        /**< SPI data transmit data write */
 
 /* SD Card constants */
-#define SD_CNT_TIMEOUT          10000u      /**< SD Counter maximum value */
-#define SD_WAKEUP_MSG_SIZE      10u         /**< Wakeup message size*/
-#define SD_HCS_BITMASK          0x40u       /**< Bitmask to access to High Capacity Support bit (if 1 it means SD card has more than 2 GB capacity) */
-#define SD_CS_PORT              GPIOA       /**< GPIO Port of SD card CS Pin */
-#define SD_CS_PIN               GPIO_PIN_4  /**< GPIO Pin of SD card CS Pin */
+#define SD_CNT_TIMEOUT              10000u      /**< SD Counter maximum value */
+#define SD_WAKEUP_MSG_SIZE          10u         /**< Wakeup message size*/
+#define SD_BLOCK_SIZE               512u        /**< Card Block Size */
+#define SD_HCS_BITMASK              0x40u       /**< Bitmask to access to High Capacity Support bit (if 1 it means SD card has more than 2 GB capacity) */
+#define SD_CS_PORT                  GPIOA       /**< GPIO Port of SD card CS Pin */
+#define SD_CS_PIN                   GPIO_PIN_4  /**< GPIO Pin of SD card CS Pin */
 
 /* SD card Status Flag */
-#define SD_IDLE_FLAG            0x01u       /**< SD card IDLE flag position */
-#define SD_ERASE_RST_FLAG       0x02u       /**< SD card ERASE RESET flag position */
-#define SD_ILLEGAL_CMD_FLAG     0x04u       /**< SD card ILLEGAL COMMAND flag position */
-#define SD_CRC_ERROR_FLAG       0x08u       /**< SD card CRC ERROR flag position */
-#define SD_ERASE_ERROR_FLAG     0x10u       /**< SD card ERASE ERROR flag position */
-#define SD_ADDR_ERROR_FLAG      0x20u       /**< SD card ADDR ERROR flag position */
-#define SD_PARAM_ERROR_FLAG     0x40u       /**< SD card PARAM ERROR flag position */
+#define SD_IDLE_FLAG                0x01u       /**< SD card IDLE flag position */
+#define SD_ERASE_RST_FLAG           0x02u       /**< SD card ERASE RESET flag position */
+#define SD_ILLEGAL_CMD_FLAG         0x04u       /**< SD card ILLEGAL COMMAND flag position */
+#define SD_CRC_ERROR_FLAG           0x08u       /**< SD card CRC ERROR flag position */
+#define SD_ERASE_ERROR_FLAG         0x10u       /**< SD card ERASE ERROR flag position */
+#define SD_ADDR_ERROR_FLAG          0x20u       /**< SD card ADDR ERROR flag position */
+#define SD_PARAM_ERROR_FLAG         0x40u       /**< SD card PARAM ERROR flag position */
 
 /***************************** Types Definitions *****************************/
 
