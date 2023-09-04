f86a1477c1afaeee3b093f3c8bbea8f83356b0b5
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-09-04 16:21:19 +0200

    Add documentation and readelf generation

--- middlewares/file-system/src/sd_spi_driver.c
+++ middlewares/file-system/src/sd_spi_driver.c
@@ -21,7 +21,14 @@
 
 /*************************** Variables Definitions ***************************/
 
-static uint8_t dummy_tx[512u] = 
+SDCardStatus_t g_sd_card_status = SD_CARD_OFF; /**< Indicates if SD card is ON/OFF */
+SDCardType_t g_sd_card_type = NOT_SDCARD;      /**< SD card type */
+
+/**
+ * @var     g_dummy_tx
+ * @brief   Dummy TX message (size of an SD block) filled with SPI_FILL_CHAR (0xff)
+ */
+static uint8_t g_dummy_tx[SD_BLOCK_SIZE] = 
 {
     SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR,
     SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR,
@@ -88,9 +95,6 @@ static uint8_t dummy_tx[512u] =
     SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR,
     SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR, SPI_FILL_CHAR
 };
-
-SDCardStatus_t g_sd_card_status = SD_CARD_OFF; /**< Indicates if SD card is ON/OFF */
-SDCardType_t g_sd_card_type = NOT_SDCARD;      /**< SD card type */
 
 /*************************** Functions Definitions ***************************/
 
@@ -177,7 +181,7 @@ fsStatus_t SD_WaitUntilReady(void)
     // Read SD card until it returns SPI_FILL_CHAR or timeouted
     while ((test_hal == THAL_SUCCESSFUL) && (answer != SPI_FILL_CHAR) && (counter < SD_CNT_TIMEOUT))
     {
-        test_hal = SpiRead(&spi_sdcard_inst, &answer, dummy_tx, 1u);
+        test_hal = SpiRead(&spi_sdcard_inst, &answer, g_dummy_tx, 1u);
         counter++;
     }
 
@@ -233,7 +237,7 @@ fsStatus_t SD_SwitchOn(void)
             uint32_t counter = 0u;
             while ((test_hal == THAL_SUCCESSFUL) && (answer != SD_IDLE_FLAG) && (counter < SD_CNT_TIMEOUT))
             {
-                test_hal = SpiRead(&spi_sdcard_inst, &answer, dummy_tx, 1u);
+                test_hal = SpiRead(&spi_sdcard_inst, &answer, g_dummy_tx, 1u);
                 counter++;
             }
 
@@ -310,7 +314,7 @@ fsStatus_t SD_RxDataBlock(uint8_t *buff, uint32_t len)
         uint32_t counter = 0u;
         while ((test_hal == THAL_SUCCESSFUL) && (token == SPI_FILL_CHAR) && (counter < SD_CNT_TIMEOUT))
         {
-            test_hal = SpiRead(&spi_sdcard_inst, &token, dummy_tx, 1u);
+            test_hal = SpiRead(&spi_sdcard_inst, &token, g_dummy_tx, 1u);
             counter++;
         }
 
@@ -318,14 +322,14 @@ fsStatus_t SD_RxDataBlock(uint8_t *buff, uint32_t len)
         if ((token == SD_START_BLOCK_TOKEN) && (test_hal == THAL_SUCCESSFUL) && (counter < SD_CNT_TIMEOUT))
         {
             // Receive block
-            test_hal = SpiRead(&spi_sdcard_inst, buff, dummy_tx, len);
+            test_hal = SpiRead(&spi_sdcard_inst, buff, g_dummy_tx, len);
 
             // Check if block has corretly been read
             if (test_hal == THAL_SUCCESSFUL)
             {
                 // Receive (and discard CRC)
                 uint8_t crc[2] = {0};
-                test_hal = SpiRead(&spi_sdcard_inst, (spiMsg_t *)&crc, dummy_tx, 2u);
+                test_hal = SpiRead(&spi_sdcard_inst, (spiMsg_t *)&crc, g_dummy_tx, 2u);
                 // Check if crc has corretly been read
                 if (test_hal != THAL_SUCCESSFUL)
                 {
@@ -390,14 +394,14 @@ fsStatus_t SD_TxDataBlock(const uint8_t *buff, uint32_t len, uint8_t token)
                     {
                         // Read and discard CRC
                         uint8_t crc[2] = {0};
-                        test_hal = SpiRead(&spi_sdcard_inst, (spiMsg_t *)&crc, dummy_tx, 2u);
+                        test_hal = SpiRead(&spi_sdcard_inst, (spiMsg_t *)&crc, g_dummy_tx, 2u);
                         if (test_hal == THAL_SUCCESSFUL)
                         {
                             uint8_t answer = SPI_FILL_CHAR;
                             uint32_t counter = 0u;
                             while ((test_hal == THAL_SUCCESSFUL) && (answer == SPI_FILL_CHAR) && (counter < SD_CNT_TIMEOUT))
                             {
-                                test_hal = SpiRead(&spi_sdcard_inst, &answer, dummy_tx, 1u);
+                                test_hal = SpiRead(&spi_sdcard_inst, &answer, g_dummy_tx, 1u);
                                 counter++;
                             }
                             // Check if we get the answer
@@ -500,7 +504,7 @@ fsStatus_t SD_SendCmd(uint8_t cmd, uint32_t arg, uint8_t *answer, uint32_t answe
                     uint8_t command_status = SPI_FILL_CHAR;
                     while ((command_status == SPI_FILL_CHAR) && (test_hal == THAL_SUCCESSFUL) && (counter < SD_CNT_TIMEOUT))
                     {
-                        test_hal = SpiRead(&spi_sdcard_inst, &command_status, dummy_tx, 1u);
+                        test_hal = SpiRead(&spi_sdcard_inst, &command_status, g_dummy_tx, 1u);
                     }
 
                     // Check Result
@@ -520,7 +524,7 @@ fsStatus_t SD_SendCmd(uint8_t cmd, uint32_t arg, uint8_t *answer, uint32_t answe
                             if (answer_size != 0u)
                             {
                                 // Receive answer
-                                test_hal = SpiRead(&spi_sdcard_inst, answer, dummy_tx, answer_size);
+                                test_hal = SpiRead(&spi_sdcard_inst, answer, g_dummy_tx, answer_size);
 
                                 // Check if everything wents well
                                 if (test_hal != THAL_SUCCESSFUL)
