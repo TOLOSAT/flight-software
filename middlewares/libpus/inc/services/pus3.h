d717c12a3304e24e1cb61182cd306e1b18b3ac0b
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-09-06 13:38:05 +0200

    Add PUS5 and PUS3 build TM functions

---
+++ middlewares/libpus/inc/services/pus3.h
@@ -0,0 +1,40 @@
+/**
+ * @file    pus3.h
+ * @author  Merlin Kooshmanian
+ * @brief   Header file for PUS 3 functions
+ * @date    06/09/2023
+ *
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+/**
+ * @defgroup libpus PUS Library
+ * @{
+ * @defgroup pus3 PUS Service 3
+ * Generic functions used by PUS3
+ * @{
+ */
+
+#ifndef PUS3_H
+#define PUS3_H
+
+/******************************* Include Files *******************************/
+
+#include "pus_types.h"
+
+/***************************** Macros Definitions ****************************/
+
+/***************************** Types Definitions *****************************/
+
+/*************************** Variables Declarations **************************/
+
+/*************************** Functions Declarations **************************/
+
+pusStatus_t BuildS3SS25(pusTM_t *tm, houseKeepingReport_t *report);
+
+#endif /* PUS3_H */
+
+/** 
+ * @} 
+ * @} 
+ */
