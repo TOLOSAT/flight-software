9813f838681d8b899098c06aee179b46e51e9558
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-09-06 13:38:05 +0200

    Add PUS5 and PUS3 build TM functions

---
+++ middlewares/libpus/inc/services/pus5.h
@@ -0,0 +1,40 @@
+/**
+ * @file    pus5.h
+ * @author  Merlin Kooshmanian
+ * @brief   Header file for PUS 5 functions
+ * @date    06/09/2023
+ *
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+/**
+ * @defgroup libpus PUS Library
+ * @{
+ * @defgroup pus5 PUS Service 5
+ * Generic functions used by PUS3
+ * @{
+ */
+
+#ifndef PUS5_H
+#define PUS5_H
+
+/******************************* Include Files *******************************/
+
+#include "pus_types.h"
+
+/***************************** Macros Definitions ****************************/
+
+/***************************** Types Definitions *****************************/
+
+/*************************** Variables Declarations **************************/
+
+/*************************** Functions Declarations **************************/
+
+pusStatus_t BuildS5SS1234(pusTM_t *tm, pusEventSeverity_t severity, eventReport_t *report);
+
+#endif /* PUS5_H */
+
+/** 
+ * @} 
+ * @} 
+ */
