b8166a52d8a5b29c2792d1df7547ca7264368627
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-09-09 12:07:22 +0200

    Now PUS6 has configurations files (+cleaning)

--- tests/test-03/src/conf/tasks_conf.c
+++ tests/test-03/src/conf/tasks_conf.c
@@ -20,8 +20,8 @@
  */
 const taskStaticConf_t g_tasks_static_conf[NB_TASKS] = 
 {
-    /* Task Ref   , Name       , Handler       , Priority              , Stack Size , Default Period , Default Deadline */
-    {MAIN_TASK    , "MainTask" , DummyMainTask , osPriorityAboveNormal , 2048U      , 800u           , 800u             },
+    /* Task Ref    , Name       , Handler       , Priority              , Stack Size , Default Period , Default Deadline */
+    { MAIN_TASK    , "MainTask" , DummyMainTask , osPriorityAboveNormal , 2048U      , 800u           , 800u             },
 };
 
 /**
@@ -30,6 +30,6 @@ const taskStaticConf_t g_tasks_static_conf[NB_TASKS] =
  */
 taskDynamicConf_t g_tasks_dynamic_conf[NB_TASKS] = 
 {
-    /* Task ID , Task Mode     , Period , Deadline , Last Wake-Up Tick */
-    {0u        , TASK_NOMINAL  , 0u     , 0u       , 0u                },
+    /* Task ID  , Task Mode     , Period , Deadline , Last Wake-Up Tick */
+    { 0u        , TASK_NOMINAL  , 0u     , 0u       , 0u                },
 };
