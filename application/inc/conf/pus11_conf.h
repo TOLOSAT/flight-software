a0acc1599fc6c48af3730e7bf2d0362c37a2283a
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-09-14 14:12:10 +0200

    Naming

---
+++ application/inc/conf/pus11_conf.h
@@ -0,0 +1,23 @@
+/**
+ * @file    pus11_conf.h
+ * @author  Merlin Kooshmanian
+ * @brief   Header file stocking configuration for pus11 environment
+ * @date    14/09/2023
+ * 
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+#ifndef PUS11_CONF_H
+#define PUS11_CONF_H
+
+/******************************* Include Files *******************************/
+
+#include "services/pus11.h"
+
+/***************************** Macros Definitions ****************************/
+
+/***************************** Types Definitions *****************************/
+
+/*************************** Variables Declarations **************************/
+
+#endif /* PUS11_CONF_H */
