f3bdc26457900ee1433cfb0894b5cac7914e249b
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-09-14 14:12:10 +0200

    Naming

---
+++ application/src/conf/pus11_conf.c
@@ -0,0 +1,15 @@
+/**
+ * @file    pus11_conf.c
+ * @author  Merlin Kooshmanian
+ * @brief   Source file stocking configuration for pus11 environment
+ * @date    14/09/2023
+ *
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+/******************************* Include Files *******************************/
+
+#include "conf/pus11_conf.h"
+
+/*************************** Variables Definitions ***************************/
+
