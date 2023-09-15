89043186d40140cfe936a308cf7aff06d360eb40
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-09-15 16:32:07 +0200

    WIP add data table for pus11 but need to update S11SS4

--- application/src/conf/pus11_conf.c
+++ application/src/conf/pus11_conf.c
@@ -18,3 +18,9 @@
  * @brief   Schedule for pus11, containing scheduled TC informations
  */
 pusSchedule_t g_pus11_schedule = {0};
+
+/**
+ * @var     g_pus11_data_table
+ * @brief   Data for pus11, containing scheduled TC
+ */
+pus11DataTable_t g_pus11_data_table = {0};
