a0e04b816b3c00bffd50e5db6d3f910b822f2665
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-09-18 18:43:36 +0200

    Invert UART TMTC and UART PRINT for stm32f411re

--- tools/HALs/HAL-TOLOSAT/src/tolosat_hal_uart.c
+++ tools/HALs/HAL-TOLOSAT/src/tolosat_hal_uart.c
@@ -322,18 +322,18 @@ static halStatus_t UartSetUpDMA(uartInst_t *uart_inst)
     if (uart_inst->drive_type == UART_DMA_DRIVE)
     {
 #if defined(STM32F411xE)
-        if (uart_inst->uart_ref == USART1)
+        if (uart_inst->uart_ref == USART2)
         {
             /* DMA controller clock enable */
-            __HAL_RCC_DMA2_CLK_ENABLE();
+            __HAL_RCC_DMA1_CLK_ENABLE();
 
             /* DMA interrupt init */
-            /* DMA2_Stream2_IRQn interrupt configuration */
-            HAL_NVIC_SetPriority(DMA2_Stream2_IRQn, 8, 0);
-            HAL_NVIC_EnableIRQ(DMA2_Stream2_IRQn);
-            /* DMA2_Stream7_IRQn interrupt configuration */
-            HAL_NVIC_SetPriority(DMA2_Stream7_IRQn, 8, 0);
-            HAL_NVIC_EnableIRQ(DMA2_Stream7_IRQn);
+            /* DMA1_Stream5_IRQn interrupt configuration */
+            HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 8, 0);
+            HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
+            /* DMA1_Stream6_IRQn interrupt configuration */
+            HAL_NVIC_SetPriority(DMA1_Stream6_IRQn, 8, 0);
+            HAL_NVIC_EnableIRQ(DMA1_Stream6_IRQn);
         }
 #elif defined(STM32F103xB)
         if (uart_inst->uart_ref == USART1)
