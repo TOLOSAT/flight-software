49aa90f691093044ec91fd081986fcb0af331155
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-09-27 14:57:22 +0200

    Change docker conf

--- run-docker.sh
+++ run-docker.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Check if the Docker image exists
-if [[ "$(docker images -q flight-software 2> /dev/null)" == "" ]]; then
-    echo "Docker image 'flight-software' does not exist, building..."
+if [[ "$(docker images -q tolosat-devtool 2> /dev/null)" == "" ]]; then
+    echo "Docker image 'tolosat-devtool' does not exist, building..."
     # Build the Docker image from the Dockerfile in the current directory
-    docker build -t flight-software .
+    docker build -t tolosat-devtool .
     if [ $? -eq 0 ]; then
         echo "Docker image 'flight-software' built successfully."
         # Run the Docker container after the image is built
         sleep 1
         echo "Launching the docker ..."
-        docker run -it -d --rm --hostname tolosat-devtool -e DISPLAY=$DISPLAY -v $(pwd):/home/obc/flight-software --privileged -v /dev/bus/usb:/dev/bus/usb flight-software:latest
+        docker run -it -d --rm --hostname tolosat-devtool -e DISPLAY=$DISPLAY -v $(pwd):/home/tapas/flight-software --privileged -v /dev/bus/usb:/dev/bus/usb tolosat-devtool:latest
     else
-        echo "Failed to build Docker image 'flight-software'."
+        echo "Failed to build Docker image 'tolosat-devtool'."
         exit 1
     fi
 else
-    echo "Docker image 'flight-software' already exists."
+    echo "Docker image 'tolosat-devtool' already exists."
     # Run the Docker container if the image already exists
     sleep 1
     echo "Launching the docker ..."
-    docker run -it -d --rm --hostname tolosat-devtool -e DISPLAY=$DISPLAY -v $(pwd):/home/obc/flight-software --privileged -v /dev/bus/usb:/dev/bus/usb flight-software:latest
+    docker run -it -d --rm --hostname tolosat-devtool -e DISPLAY=$DISPLAY -v $(pwd):/home/tapas/flight-software --privileged -v /dev/bus/usb:/dev/bus/usb tolosat-devtool:latest
 fi
