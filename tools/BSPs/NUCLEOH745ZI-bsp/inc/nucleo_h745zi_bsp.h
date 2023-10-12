480b664dad371fea3b32031ab6e7df7f65570b3f
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-10-12 11:11:12 +0000

    Now HAL is independant from BSP

--- tools/BSPs/NUCLEOH745ZI-bsp/inc/nucleo_h745zi_bsp.h
+++ tools/BSPs/NUCLEOH745ZI-bsp/inc/nucleo_h745zi_bsp.h
@@ -101,6 +101,34 @@
 
 /* SPECIFIC PROCEDURES */
 #define BSP_PROCEDURE(procedure)                        procedure
+#define TAPAS_CLOCK_SPECIFIC_INIT_PWR()                 BSP_PROCEDURE(HAL_PWREx_ConfigSupply(PWR_DIRECT_SMPS_SUPPLY); \
+                                                                      __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0); \
+                                                                      while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)){})
+#define TAPAS_CLOCK_SPECIFIC_INIT_OSC()                 BSP_PROCEDURE(RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE; \
+                                                                      RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;  \
+                                                                      RCC_OscInitStruct.LSEState = RCC_LSE_ON; \
+                                                                      RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON; \
+                                                                      RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE; \
+                                                                      RCC_OscInitStruct.PLL.PLLM = 1; \
+                                                                      RCC_OscInitStruct.PLL.PLLN = 25; \
+                                                                      RCC_OscInitStruct.PLL.PLLP = 2; \
+                                                                      RCC_OscInitStruct.PLL.PLLQ = 2; \
+                                                                      RCC_OscInitStruct.PLL.PLLR = 2; \
+                                                                      RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3; \
+                                                                      RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE; \
+                                                                      RCC_OscInitStruct.PLL.PLLFRACN = 0;)
+#define TAPAS_CLOCK_SPECIFIC_INIT_BUS()                 BSP_PROCEDURE(RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1; \
+                                                                      RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK; \
+                                                                      RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1; \
+                                                                      RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV1; \
+                                                                      RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2; \
+                                                                      RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2; \
+                                                                      RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2; \
+                                                                      RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2; \
+                                                                      if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) \
+                                                                      { \
+                                                                          return_value = THAL_ERROR; \
+                                                                      })
 #define TAPAS_IIC_SPECIFIC_INIT()                       BSP_PROCEDURE(iic_inst->handle_struct.Init.Timing = 0x307075B1;)
 #define TAPAS_SPI_SPECIFIC_INIT()                       BSP_PROCEDURE(spi_inst->handle_struct.Init.NSSPMode = SPI_NSS_PULSE_ENABLE; \
                                                                       spi_inst->handle_struct.Init.NSSPolarity = SPI_NSS_POLARITY_LOW; \
--- tools/HALs/HAL-TOLOSAT/src/tolosat_hal_init.c
+++ tools/HALs/HAL-TOLOSAT/src/tolosat_hal_init.c
@@ -61,118 +61,16 @@ static halStatus_t SystemClock_Config(void)
     RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
     // Function Core
-#if defined(NUCLEO_F411RE)
-    /** Configure the main internal regulator output voltage
-     */
-    __HAL_RCC_PWR_CLK_ENABLE();
-    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-#elif defined(NUCLEO_F103RB)
-#elif defined(NUCLEO_H745ZI)
-    /** Supply configuration update enable
-     */
-    HAL_PWREx_ConfigSupply(PWR_DIRECT_SMPS_SUPPLY);
+    /** Initializes the RCC Oscillator Powers */
+    TAPAS_CLOCK_SPECIFIC_INIT_PWR();
 
-    /** Configure the main internal regulator output voltage
-     */
-    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+    /** Initializes the RCC Oscillators */
+    TAPAS_CLOCK_SPECIFIC_INIT_OSC();
 
-    while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY))
-    {
-    }
-#else
-#error "Board is not supported"
-#endif
-
-    /** Initializes the RCC Oscillators according to the specified parameters
-     * in the RCC_OscInitTypeDef structure.
-     */
-#if defined(NUCLEO_F411RE)
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM = 8;
-    RCC_OscInitStruct.PLL.PLLN = 400;
-    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
-    RCC_OscInitStruct.PLL.PLLQ = 4;
-#elif defined(NUCLEO_F103RB)
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-#elif defined(NUCLEO_H745ZI)
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM = 1;
-    RCC_OscInitStruct.PLL.PLLN = 25;
-    RCC_OscInitStruct.PLL.PLLP = 2;
-    RCC_OscInitStruct.PLL.PLLQ = 2;
-    RCC_OscInitStruct.PLL.PLLR = 2;
-    RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
-    RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
-    RCC_OscInitStruct.PLL.PLLFRACN = 0;
-#else
-#error "Board is not supported"
-#endif
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) == HAL_OK)
     {
-        /** Initializes the CPU, AHB and APB buses clocks
-         */
-#if defined(NUCLEO_F411RE)
-        RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-        RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-        RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-        RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-        RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-        if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-        {
-            return_value = THAL_ERROR;
-        }
-
-#elif defined(NUCLEO_F103RB)
-        RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-        RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-        RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-        RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-        RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-        if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) == HAL_OK)
-        {
-            RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
-            PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC;
-            PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
-            if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-            {
-                return_value = THAL_ERROR;
-            }
-        }
-        else
-        {
-            return_value = THAL_ERROR;
-        }
-#elif defined(NUCLEO_H745ZI)
-        RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
-        RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-        RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
-        RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV1;
-        RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
-        RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
-        RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
-        RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
-        if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-        {
-            return_value = THAL_ERROR;
-        }
-#else
-#error "Board is not supported"
-#endif
+        /** Initializes the CPU, AHB and APB buses clocks */
+        TAPAS_CLOCK_SPECIFIC_INIT_BUS();
     }
     else
     {
