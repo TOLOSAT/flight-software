4951d03b9b4c484089c27c1c0fe190458847d51f
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-10-12 11:11:12 +0000

    Now HAL is independant from BSP

--- tools/BSPs/NUCLEOF411RE-bsp/inc/nucleo_f411re_bsp.h
+++ tools/BSPs/NUCLEOF411RE-bsp/inc/nucleo_f411re_bsp.h
@@ -93,6 +93,26 @@
 
 /* SPECIFIC PROCEDURES */
 #define BSP_PROCEDURE(procedure)                        procedure
+#define TAPAS_CLOCK_SPECIFIC_INIT_PWR()                 BSP_PROCEDURE(__HAL_RCC_PWR_CLK_ENABLE(); \
+                                                                      __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);)
+#define TAPAS_CLOCK_SPECIFIC_INIT_OSC()                 BSP_PROCEDURE(RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE; \
+                                                                      RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS; \
+                                                                      RCC_OscInitStruct.LSEState = RCC_LSE_ON; \
+                                                                      RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON; \
+                                                                      RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE; \
+                                                                      RCC_OscInitStruct.PLL.PLLM = 8; \
+                                                                      RCC_OscInitStruct.PLL.PLLN = 400; \
+                                                                      RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4; \
+                                                                      RCC_OscInitStruct.PLL.PLLQ = 4;)
+#define TAPAS_CLOCK_SPECIFIC_INIT_BUS()                 BSP_PROCEDURE(RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2; \
+                                                                      RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK; \
+                                                                      RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1; \
+                                                                      RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2; \
+                                                                      RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1; \
+                                                                      if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) \
+                                                                      { \
+                                                                          return_value = THAL_ERROR; \
+                                                                      })
 #define TAPAS_IIC_SPECIFIC_INIT()                       BSP_PROCEDURE(iic_inst->handle_struct.Init.ClockSpeed = 100000; \
                                                                       iic_inst->handle_struct.Init.DutyCycle = I2C_DUTYCYCLE_2;)
 #define TAPAS_SPI_SPECIFIC_INIT()                       BSP_PROCEDURE(spi_inst->handle_struct.Init.Mode = SPI_MODE_MASTER;)
