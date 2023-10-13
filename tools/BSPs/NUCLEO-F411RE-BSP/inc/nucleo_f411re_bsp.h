63e358919af2032eef8ec3b655f2e8ece1f5d3fe
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-10-13 17:56:06 +0000

    Correct bug caused by bsp procedures

--- tools/BSPs/NUCLEO-F411RE-BSP/inc/nucleo_f411re_bsp.h
+++ tools/BSPs/NUCLEO-F411RE-BSP/inc/nucleo_f411re_bsp.h
@@ -95,33 +95,33 @@
 #define BSP_PROCEDURE(procedure)                        procedure
 #define TAPAS_CLOCK_SPECIFIC_INIT_PWR()                 BSP_PROCEDURE(__HAL_RCC_PWR_CLK_ENABLE(); \
                                                                       __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);)
-#define TAPAS_CLOCK_SPECIFIC_INIT_OSC()                 BSP_PROCEDURE(RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE; \
-                                                                      RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS; \
-                                                                      RCC_OscInitStruct.LSEState = RCC_LSE_ON; \
-                                                                      RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON; \
-                                                                      RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE; \
-                                                                      RCC_OscInitStruct.PLL.PLLM = 8; \
-                                                                      RCC_OscInitStruct.PLL.PLLN = 400; \
-                                                                      RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4; \
-                                                                      RCC_OscInitStruct.PLL.PLLQ = 4;)
-#define TAPAS_CLOCK_SPECIFIC_INIT_BUS()                 BSP_PROCEDURE(RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2; \
-                                                                      RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK; \
-                                                                      RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1; \
-                                                                      RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2; \
-                                                                      RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1; \
-                                                                      if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) \
+#define TAPAS_CLOCK_SPECIFIC_INIT_OSC(osc_init_inst)    BSP_PROCEDURE(osc_init_inst.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE; \
+                                                                      osc_init_inst.HSEState = RCC_HSE_BYPASS; \
+                                                                      osc_init_inst.LSEState = RCC_LSE_ON; \
+                                                                      osc_init_inst.PLL.PLLState = RCC_PLL_ON; \
+                                                                      osc_init_inst.PLL.PLLSource = RCC_PLLSOURCE_HSE; \
+                                                                      osc_init_inst.PLL.PLLM = 8; \
+                                                                      osc_init_inst.PLL.PLLN = 400; \
+                                                                      osc_init_inst.PLL.PLLP = RCC_PLLP_DIV4; \
+                                                                      osc_init_inst.PLL.PLLQ = 4;)
+#define TAPAS_CLOCK_SPECIFIC_INIT_BUS(clk_init_inst)    BSP_PROCEDURE(clk_init_inst.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2; \
+                                                                      clk_init_inst.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK; \
+                                                                      clk_init_inst.AHBCLKDivider = RCC_SYSCLK_DIV1; \
+                                                                      clk_init_inst.APB1CLKDivider = RCC_HCLK_DIV2; \
+                                                                      clk_init_inst.APB2CLKDivider = RCC_HCLK_DIV1; \
+                                                                      if (HAL_RCC_ClockConfig(&clk_init_inst, FLASH_LATENCY_2) != HAL_OK) \
                                                                       { \
                                                                           return_value = THAL_ERROR; \
                                                                       })
-#define TAPAS_IIC_SPECIFIC_INIT()                       BSP_PROCEDURE(iic_inst->handle_struct.Init.ClockSpeed = 100000; \
+#define TAPAS_IIC_SPECIFIC_INIT(iic_inst)               BSP_PROCEDURE(iic_inst->handle_struct.Init.ClockSpeed = 100000; \
                                                                       iic_inst->handle_struct.Init.DutyCycle = I2C_DUTYCYCLE_2;)
-#define TAPAS_SPI_SPECIFIC_INIT()                       BSP_PROCEDURE(spi_inst->handle_struct.Init.Mode = SPI_MODE_MASTER;)
-#define TAPAS_RTC_SPECIFIC_INIT()                       BSP_PROCEDURE(rtc_inst.Init.HourFormat = RTC_HOURFORMAT_24; \
+#define TAPAS_SPI_SPECIFIC_INIT(spi_inst)               BSP_PROCEDURE(spi_inst->handle_struct.Init.Mode = SPI_MODE_MASTER;)
+#define TAPAS_RTC_SPECIFIC_INIT(rtc_inst)               BSP_PROCEDURE(rtc_inst.Init.HourFormat = RTC_HOURFORMAT_24; \
                                                                       rtc_inst.Init.AsynchPrediv = 127u; \
                                                                       rtc_inst.Init.SynchPrediv = 255u; \
                                                                       rtc_inst.Init.OutPut = RTC_OUTPUT_DISABLE; \
                                                                       rtc_inst.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH; \
                                                                       rtc_inst.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;)
-#define TAPAS_RTC_SET_MILLISEC()                        BSP_PROCEDURE(rtc_time->millisecond = (MILLISECOND_SCALER*(time.SecondFraction-time.SubSeconds))/(time.SecondFraction+1);)
+#define TAPAS_RTC_SET_MILLISEC(rtc_time)                BSP_PROCEDURE(rtc_time->millisecond = (MILLISECOND_SCALER*(time.SecondFraction-time.SubSeconds))/(time.SecondFraction+1);)
 
 #endif /* NUCLEOF411RE_BSP_H */
--- tools/BSPs/NUCLEO-H745ZI-BSP/inc/nucleo_h745zi_bsp.h
+++ tools/BSPs/NUCLEO-H745ZI-BSP/inc/nucleo_h745zi_bsp.h
@@ -104,33 +104,33 @@
 #define TAPAS_CLOCK_SPECIFIC_INIT_PWR()                 BSP_PROCEDURE(HAL_PWREx_ConfigSupply(PWR_DIRECT_SMPS_SUPPLY); \
                                                                       __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0); \
                                                                       while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)){})
-#define TAPAS_CLOCK_SPECIFIC_INIT_OSC()                 BSP_PROCEDURE(RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE; \
-                                                                      RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;  \
-                                                                      RCC_OscInitStruct.LSEState = RCC_LSE_ON; \
-                                                                      RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON; \
-                                                                      RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE; \
-                                                                      RCC_OscInitStruct.PLL.PLLM = 1; \
-                                                                      RCC_OscInitStruct.PLL.PLLN = 25; \
-                                                                      RCC_OscInitStruct.PLL.PLLP = 2; \
-                                                                      RCC_OscInitStruct.PLL.PLLQ = 2; \
-                                                                      RCC_OscInitStruct.PLL.PLLR = 2; \
-                                                                      RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3; \
-                                                                      RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE; \
-                                                                      RCC_OscInitStruct.PLL.PLLFRACN = 0;)
-#define TAPAS_CLOCK_SPECIFIC_INIT_BUS()                 BSP_PROCEDURE(RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1; \
-                                                                      RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK; \
-                                                                      RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1; \
-                                                                      RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV1; \
-                                                                      RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2; \
-                                                                      RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2; \
-                                                                      RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2; \
-                                                                      RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2; \
-                                                                      if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) \
+#define TAPAS_CLOCK_SPECIFIC_INIT_OSC(osc_init_inst)    BSP_PROCEDURE(osc_init_inst.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE; \
+                                                                      osc_init_inst.HSEState = RCC_HSE_BYPASS;  \
+                                                                      osc_init_inst.LSEState = RCC_LSE_ON; \
+                                                                      osc_init_inst.PLL.PLLState = RCC_PLL_ON; \
+                                                                      osc_init_inst.PLL.PLLSource = RCC_PLLSOURCE_HSE; \
+                                                                      osc_init_inst.PLL.PLLM = 1; \
+                                                                      osc_init_inst.PLL.PLLN = 25; \
+                                                                      osc_init_inst.PLL.PLLP = 2; \
+                                                                      osc_init_inst.PLL.PLLQ = 2; \
+                                                                      osc_init_inst.PLL.PLLR = 2; \
+                                                                      osc_init_inst.PLL.PLLRGE = RCC_PLL1VCIRANGE_3; \
+                                                                      osc_init_inst.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE; \
+                                                                      osc_init_inst.PLL.PLLFRACN = 0;)
+#define TAPAS_CLOCK_SPECIFIC_INIT_BUS(clk_init_inst)    BSP_PROCEDURE(clk_init_inst.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1; \
+                                                                      clk_init_inst.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK; \
+                                                                      clk_init_inst.SYSCLKDivider = RCC_SYSCLK_DIV1; \
+                                                                      clk_init_inst.AHBCLKDivider = RCC_HCLK_DIV1; \
+                                                                      clk_init_inst.APB3CLKDivider = RCC_APB3_DIV2; \
+                                                                      clk_init_inst.APB1CLKDivider = RCC_APB1_DIV2; \
+                                                                      clk_init_inst.APB2CLKDivider = RCC_APB2_DIV2; \
+                                                                      clk_init_inst.APB4CLKDivider = RCC_APB4_DIV2; \
+                                                                      if (HAL_RCC_ClockConfig(&clk_init_inst, FLASH_LATENCY_4) != HAL_OK) \
                                                                       { \
                                                                           return_value = THAL_ERROR; \
                                                                       })
-#define TAPAS_IIC_SPECIFIC_INIT()                       BSP_PROCEDURE(iic_inst->handle_struct.Init.Timing = 0x307075B1;)
-#define TAPAS_SPI_SPECIFIC_INIT()                       BSP_PROCEDURE(spi_inst->handle_struct.Init.NSSPMode = SPI_NSS_PULSE_ENABLE; \
+#define TAPAS_IIC_SPECIFIC_INIT(iic_inst)               BSP_PROCEDURE(iic_inst->handle_struct.Init.Timing = 0x307075B1;)
+#define TAPAS_SPI_SPECIFIC_INIT(spi_inst)               BSP_PROCEDURE(spi_inst->handle_struct.Init.NSSPMode = SPI_NSS_PULSE_ENABLE; \
                                                                       spi_inst->handle_struct.Init.NSSPolarity = SPI_NSS_POLARITY_LOW; \
                                                                       spi_inst->handle_struct.Init.FifoThreshold = SPI_FIFO_THRESHOLD_01DATA; \
                                                                       spi_inst->handle_struct.Init.TxCRCInitializationPattern = SPI_CRC_INITIALIZATION_ALL_ZERO_PATTERN; \
@@ -140,13 +140,13 @@
                                                                       spi_inst->handle_struct.Init.MasterReceiverAutoSusp = SPI_MASTER_RX_AUTOSUSP_DISABLE; \
                                                                       spi_inst->handle_struct.Init.MasterKeepIOState = SPI_MASTER_KEEP_IO_STATE_DISABLE; \
                                                                       spi_inst->handle_struct.Init.IOSwap = SPI_IO_SWAP_DISABLE;)
-#define TAPAS_RTC_SPECIFIC_INIT()                       BSP_PROCEDURE(rtc_inst.Init.HourFormat = RTC_HOURFORMAT_24; \
+#define TAPAS_RTC_SPECIFIC_INIT(rtc_inst)               BSP_PROCEDURE(rtc_inst.Init.HourFormat = RTC_HOURFORMAT_24; \
                                                                       rtc_inst.Init.AsynchPrediv = 127u; \
                                                                       rtc_inst.Init.SynchPrediv = 255u; \
                                                                       rtc_inst.Init.OutPut = RTC_OUTPUT_DISABLE; \
                                                                       rtc_inst.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH; \
                                                                       rtc_inst.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN; \
                                                                       rtc_inst.Init.OutPutRemap = RTC_OUTPUT_REMAP_NONE;)
-#define TAPAS_RTC_SET_MILLISEC()                        BSP_PROCEDURE(rtc_time->millisecond = (MILLISECOND_SCALER*(time.SecondFraction-time.SubSeconds))/(time.SecondFraction+1);)
+#define TAPAS_RTC_SET_MILLISEC(rtc_time)                BSP_PROCEDURE(rtc_time->millisecond = (MILLISECOND_SCALER*(time.SecondFraction-time.SubSeconds))/(time.SecondFraction+1);)
 
 #endif /* NUCLEOH745ZI_BSP_H */
