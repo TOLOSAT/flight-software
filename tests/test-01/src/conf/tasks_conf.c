19f8569d28b79c41f6b6ec57a649fa3abcbe3fcc
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-10-28 22:23:10 +0200

    Corrects bug noted by cppcheck

--- tests/test-01/src/conf/tasks_conf.c
+++ tests/test-01/src/conf/tasks_conf.c
@@ -21,7 +21,7 @@
 const taskStaticConf_t g_tasks_static_conf[NB_TASKS] = 
 {
     /* Task Ref    , Name       , Function      , Priority              , Stack Size , Default Period , Default Deadline */
-    { MAIN_TASK    , "MainTask" , DummyMainTask , osPriorityAboveNormal , 1024U      , 100u           , 100u             },
+    { MAIN_TASK    , "MainTask" , DummyMainTask , PRIORITY_ABOVE_NORMAL , 1024U      , 100u           , 100u             },
 };
 
 /**
