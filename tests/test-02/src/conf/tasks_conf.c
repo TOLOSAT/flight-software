312cd40b0ac6f091926aa8eabdaad53926118dd9
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-11-16 14:52:13 +0000

    Update tests, TAPAS works on NUCLEO-F4 and NUCLEO-H7 boards for sure

--- tests/test-02/src/conf/tasks_conf.c
+++ tests/test-02/src/conf/tasks_conf.c
@@ -25,9 +25,9 @@ const taskStaticConf_t g_tasks_static_conf[NB_TASKS] =
 {
     /* Task Ref       , Name              , Function        , Priority              , Stack Size , Default Period , Default Deadline */
     { TC_RECEIVER     , "TcReceiverMain"  , TcReceiverMain  , PRIORITY_HIGH         , 2048u      , 500u           , 500u             },
-    { TM_SENDER       , "TmSenderMain"    , TmSenderMain    , PRIORITY_LOW          , 1024u      , 100u           , 500u             },
-    { TC_SCHEDULER    , "TcSchedulerMain" , TcSchedulerMain , PRIORITY_NORMAL       , 1024u      , 500u           , 500u             },
-    { TC_PROCESS      , "TcProcessMain"   , TcProcessMain   , PRIORITY_NORMAL       , 2024u      , 500u           , 500u             },
+    { TM_SENDER       , "TmSenderMain"    , TmSenderMain    , PRIORITY_BELOW_NORMAL , 1024u      , 500u           , 500u             },
+    { TC_SCHEDULER    , "TcSchedulerMain" , TcSchedulerMain , PRIORITY_LOW          , 3072u      , 500u           , NO_DEADLINE      },
+    { TC_PROCESS      , "TcProcessMain"   , TcProcessMain   , PRIORITY_NORMAL       , 2048u      , 500u           , 500u             },
 };
 
 /**
