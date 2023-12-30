a5c429398c12d0785b2026fb201c5ed984e3fc37
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-12-30 15:40:53 +0100

    Init feature-mpu branch

---
+++ tests/test-mpu/inc/conf/fs_conf.h
@@ -0,0 +1,49 @@
+/**
+ * @file    fs_conf.h
+ * @author  Merlin Kooshmanian
+ * @brief   Header file stocking configuration for file system content
+ * @date    09/09/2023
+ * 
+ * @copyright Copyright (c) TOLOSAT 2023
+ */
+
+#ifndef FS_CONF_H
+#define FS_CONF_H
+
+/******************************* Include Files *******************************/
+
+#include "tolosat_fs_types.h"
+
+/***************************** Macros Definitions ****************************/
+
+#define MAX_NB_FILES_PER_DEVICES    (NB_SD0_FILES)  /**< Maximum Number of files */
+
+/***************************** Types Definitions *****************************/
+
+/**
+ * @enum    MEMORY_DEVICES_ENUM
+ * @brief   Enum defining memory devices reference numbers
+ */
+enum MEMORY_DEVICES_ENUM 
+{
+    SD0 = 0u,
+    NB_MEMORY_DEVICES,
+};
+
+/**
+ * @enum    FILE_SD0_ENUM
+ * @brief   Enum defining file in SD0 reference numbers
+ */
+enum FILE_SD0_ENUM 
+{
+    TEST_FILE = 0u,
+    PUS11_SCHED_FILE,
+    PUS11_DATA_FILE,
+    NB_SD0_FILES,
+};
+
+/*************************** Variables Declarations **************************/
+
+extern const fsFileConf_t g_files_conf[NB_MEMORY_DEVICES][MAX_NB_FILES_PER_DEVICES];
+
+#endif /* FS_CONF_H */
