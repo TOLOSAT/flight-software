32877cbb298c0b021b807f8d506143adb3d84bb3
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-01-06 17:39:49 +0100

    Add privilege field for configuring tasks

--- application/src/conf/tasks_conf.c
+++ application/src/conf/tasks_conf.c
@@ -32,20 +32,20 @@
  */
 const taskStaticConf_t g_tasks_static_conf[NB_TASKS] = 
 {
-    /* Task Ref       , Name              , Function        , Priority              , Stack Size , Default Period , Default Deadline */
-    { SALAMI_TASK     , "SalamiMain"      , SalamiMain      , PRIORITY_HIGH         , 1024u      , 500u           , 500u             },
-    { MISO_TASK       , "MisoMain"        , MisoMain        , PRIORITY_LOW          , 1024u      , 500u           , NO_DEADLINE      },
-    { CARNE_TASK      , "CarneMain"       , CarneMain       , PRIORITY_HIGH         , 1024u      , 500u           , 500u             },
-    { TC_RECEIVER     , "TcReceiverMain"  , TcReceiverMain  , PRIORITY_HIGH         , 2048u      , 500u           , 500u             },
-    { TM_SENDER       , "TmSenderMain"    , TmSenderMain    , PRIORITY_BELOW_NORMAL , 1024u      , 500u           , 500u             },
-    { TC_SCHEDULER    , "TcSchedulerMain" , TcSchedulerMain , PRIORITY_LOW          , 3072u      , 500u           , NO_DEADLINE      },
-    { TC_PROCESS      , "TcProcessMain"   , TcProcessMain   , PRIORITY_NORMAL       , 2048u      , 500u           , 500u             },
-    { HK_TASK         , "HkMain"          , HkMain          , PRIORITY_LOW          , 2048u      , 500u           , NO_DEADLINE      },
-    { AOCS_TASK       , "AocsMain"        , AocsMain        , PRIORITY_ABOVE_NORMAL , 1024u      , 500u           , 500u             },
-    { POWER_TASK      , "PowerMain"       , PowerMain       , PRIORITY_ABOVE_NORMAL , 1024u      , 500u           , 500u             },
-    { THERMAL_TASK    , "ThermalMain"     , ThermalMain     , PRIORITY_ABOVE_NORMAL , 1024u      , 500u           , 500u             },
-    { GRAVIMETRY_TASK , "GravimetryMain"  , GravimetryMain  , PRIORITY_NORMAL       , 1024u      , 500u           , 500u             },
-    { IRIDIUM_TASK    , "IridiumMain"     , IridiumMain     , PRIORITY_NORMAL       , 1024u      , 500u           , 500u             },
+    /* Task Ref       , Name              , Function        , Priority              , Stack Size , Default Period , Default Deadline , Privilege       */
+    { SALAMI_TASK     , "SalamiMain"      , SalamiMain      , PRIORITY_HIGH         , 1024u      , 500u           , 500u             , TASK_PRIVILEGED },
+    { MISO_TASK       , "MisoMain"        , MisoMain        , PRIORITY_LOW          , 1024u      , 500u           , NO_DEADLINE      , TASK_PRIVILEGED },
+    { CARNE_TASK      , "CarneMain"       , CarneMain       , PRIORITY_HIGH         , 1024u      , 500u           , 500u             , TASK_PRIVILEGED },
+    { TC_RECEIVER     , "TcReceiverMain"  , TcReceiverMain  , PRIORITY_HIGH         , 2048u      , 500u           , 500u             , TASK_PRIVILEGED },
+    { TM_SENDER       , "TmSenderMain"    , TmSenderMain    , PRIORITY_BELOW_NORMAL , 1024u      , 500u           , 500u             , TASK_PRIVILEGED },
+    { TC_SCHEDULER    , "TcSchedulerMain" , TcSchedulerMain , PRIORITY_LOW          , 3072u      , 500u           , NO_DEADLINE      , TASK_PRIVILEGED },
+    { TC_PROCESS      , "TcProcessMain"   , TcProcessMain   , PRIORITY_NORMAL       , 2048u      , 500u           , 500u             , TASK_PRIVILEGED },
+    { HK_TASK         , "HkMain"          , HkMain          , PRIORITY_LOW          , 2048u      , 500u           , NO_DEADLINE      , TASK_PRIVILEGED },
+    { AOCS_TASK       , "AocsMain"        , AocsMain        , PRIORITY_ABOVE_NORMAL , 1024u      , 500u           , 500u             , TASK_PRIVILEGED },
+    { POWER_TASK      , "PowerMain"       , PowerMain       , PRIORITY_ABOVE_NORMAL , 1024u      , 500u           , 500u             , TASK_PRIVILEGED },
+    { THERMAL_TASK    , "ThermalMain"     , ThermalMain     , PRIORITY_ABOVE_NORMAL , 1024u      , 500u           , 500u             , TASK_PRIVILEGED },
+    { GRAVIMETRY_TASK , "GravimetryMain"  , GravimetryMain  , PRIORITY_NORMAL       , 1024u      , 500u           , 500u             , TASK_PRIVILEGED },
+    { IRIDIUM_TASK    , "IridiumMain"     , IridiumMain     , PRIORITY_NORMAL       , 1024u      , 500u           , 500u             , TASK_PRIVILEGED },
 };
 
 /**
