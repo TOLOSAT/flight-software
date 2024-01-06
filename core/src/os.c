3122f376d626c201fed2a10964b7ec0cffb1e334
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-01-06 10:59:46 +0100

    Old template for FreeRTOSConfig and add malloc failed hook

--- core/src/os.c
+++ core/src/os.c
@@ -15,7 +15,7 @@
 #include "stm32f4xx.h"
 #elif defined(NUCLEO_F103RB)
 #include "stm32f1xx.h"
-#else 
+#else
 #error "Board is not supported"
 #endif
 
@@ -69,14 +69,14 @@ void SysTick_Handler(void)
  * @fn      vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize)
  * @brief   This function is used to allocate memory to Idle Task when scheduler is started
  * @return  Nothing
- * 
+ *
  * Os specific function that need to be provided if static allocation is used
  */
 void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize)
 {
     /* Idle task control block and stack */
     static StaticTask_t Idle_TCB;
-    static StackType_t Idle_Stack[configMINIMAL_STACK_SIZE];  // cppcheck-suppress misra-c2012-18.8
+    static StackType_t Idle_Stack[configMINIMAL_STACK_SIZE]; // cppcheck-suppress misra-c2012-18.8
 
     *ppxIdleTaskTCBBuffer = &Idle_TCB;
     *ppxIdleTaskStackBuffer = &Idle_Stack[0];
@@ -87,7 +87,7 @@ void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackTyp
  * @fn      vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize)
  * @brief   This function is used to allocate memory to timer tasks when they are created
  * @return  Nothing
- * 
+ *
  * Os specific function that need to be provided if static allocation is used
  */
 void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize)
@@ -101,11 +101,12 @@ void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackT
     *pulTimerTaskStackSize = (uint32_t)configTIMER_TASK_STACK_DEPTH;
 }
 
+#if (configCHECK_FOR_STACK_OVERFLOW > 1)
 /**
  * @fn      vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
  * @brief   This function is executed if a task runs out of stack
  * @return  Nothing
- * 
+ *
  * Os specific function that need to be provided if stack overflow hook is used
  */
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
@@ -120,3 +121,22 @@ void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
         /* Do Nothing */
     }
 }
+#endif
+
+#if (configUSE_MALLOC_FAILED_HOOK == 1)
+/**
+ * @fn      vApplicationMallocFailedHook(void)
+ * @brief   This function is executed when a malloc failed to attribute memory
+ * @return  Nothing
+ *
+ * Os specific function that need to be provided if malloc failed hook is used
+ */
+void vApplicationMallocFailedHook(void)
+{
+    // Function Core
+    while (1)
+    {
+        /* Do Nothing */
+    }
+}
+#endif
