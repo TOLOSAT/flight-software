341f70037071b5b7cf7beae62445b48d15d86d03
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-02-06 17:21:49 +0100

    Buffers are now automatically generated from csv

--- gen/pre-build.mk
+++ gen/pre-build.mk
@@ -5,4 +5,8 @@
 ##############################################
 
 pre-build :
-	${PYTHON} $(TOOLS_DIR)/task-parser.py -i $(APPLICATION_CONF_DIR)/tasks_conf.csv -o $(PRE_BUILD_DIR)/conf
+	${PYTHON} $(TOOLS_DIR)/tasks-parser.py -i $(APPLICATION_CONF_DIR)/tasks_conf.csv -o $(PRE_BUILD_DIR)/conf
+	${PYTHON} $(TOOLS_DIR)/buffers-parser.py -i $(APPLICATION_CONF_DIR)/buffers_conf.csv -o $(PRE_BUILD_DIR)/conf
+	@echo "****************************"
+	@echo "****   Pre Build Done   ****"
+	@echo "****************************"
