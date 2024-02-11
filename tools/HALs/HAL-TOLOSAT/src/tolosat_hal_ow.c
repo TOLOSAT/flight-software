671f3d744b5829bd1ad3fc89c9eaba245c3d161c
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-02-11 18:49:49 +0000

    Init feature One Wire branch

---
+++ tools/HALs/HAL-TOLOSAT/src/tolosat_hal_ow.c
@@ -0,0 +1,20 @@
+/**
+ * @file    tolosat_hal_ow.c
+ * @author  Merlin Kooshmanian
+ * @brief   Source file for TOLOSAT HAL OW functions
+ * @date    11/02/2024
+ *
+ * @copyright Copyright (c) TOLOSAT 2024
+ */
+
+/******************************* Include Files *******************************/
+
+#include "tolosat_hal.h"
+
+/***************************** Macros Definitions ****************************/
+
+/*************************** Functions Declarations **************************/
+
+/*************************** Variables Definitions ***************************/
+
+/*************************** Functions Definitions ***************************/
