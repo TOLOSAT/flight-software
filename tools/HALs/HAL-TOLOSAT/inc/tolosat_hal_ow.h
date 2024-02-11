19dc0e95022aa3517416bde17b2b7d9ab66ea369
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-02-11 19:25:05 +0000

    Creates OW functions and modify gpio functions

--- tools/HALs/HAL-TOLOSAT/inc/tolosat_hal_ow.h
+++ tools/HALs/HAL-TOLOSAT/inc/tolosat_hal_ow.h
@@ -22,14 +22,37 @@
 /******************************* Include Files *******************************/
 
 #include "tolosat_hal_types.h"
+#include "tolosat_hal_gpio.h"
 
 /***************************** Macros Definitions ****************************/
 
 /***************************** Types Definitions *****************************/
 
+/** @brief One Wire message type definition */
+typedef uint8_t owMsg_t;
+
+/** @brief One Wire message length type definition */
+typedef uint16_t owMsgLength_t;
+
+/** 
+ * @struct  owInst_t
+ * @brief   Struct type definition of a One Wire instance
+ */
+typedef struct
+{
+    gpioInst_t gpio_handle_struct;  /**< @brief GPIO handle struct for One Wire */
+} owInst_t;
+
+
 /*************************** Variables Declarations **************************/
 
 /*************************** Functions Declarations **************************/
+
+halStatus_t OwOpen(owInst_t *ow_isnt);
+halStatus_t OwWrite(owInst_t *ow_isnt, owMsg_t *msg, owMsgLength_t length);
+halStatus_t OwRead(owInst_t *ow_isnt, owMsg_t *msg, owMsgLength_t length);
+halStatus_t OwIoctl(owInst_t *ow_isnt, halIoCtlCmd_t io_cmd);
+halStatus_t OwClose(owInst_t *ow_isnt);
 
 #endif /* TOLOSAT_HAL_OW_H */
 
