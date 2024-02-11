9b3b40502144db7bf043e2c40fbdaabcb2636195
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-02-11 19:25:05 +0000

    Creates OW functions and modify gpio functions

--- core/src/io_instances.c
+++ core/src/io_instances.c
@@ -79,6 +79,8 @@ spiInst_t spi_sdcard_inst = {
  * @brief   GPIO led instance declaration
  */
 gpioInst_t led_inst = {
+    .port = USER_LED_GPIO_PORT,
+    .pin = USER_LED_PIN,
     .mode = GPIO_MODE_OUTPUT_PP,
     .pull = GPIO_NOPULL,
     .speed = GPIO_SPEED_FREQ_LOW,
@@ -89,6 +91,8 @@ gpioInst_t led_inst = {
  * @brief   GPIO button instance declaration
  */
 gpioInst_t user_button_inst = {
+    .port = USER_BUTTON_GPIO_PORT,
+    .pin = USER_BUTTON_PIN,
     .mode = GPIO_MODE_IT_FALLING,
     .pull = GPIO_NOPULL,
     .speed = GPIO_SPEED_FREQ_LOW,
@@ -99,6 +103,8 @@ gpioInst_t user_button_inst = {
  * @brief   GPIO sd card chip select instance declaration
  */
 gpioInst_t sd_card_cs = {
+    .port = TAPAS_SPI_SDCARD_CS_GPIO_PORT,
+    .pin = TAPAS_SPI_SDCARD_CS_PIN,
     .mode = GPIO_MODE_OUTPUT_PP,
     .pull = GPIO_NOPULL,
     .speed = GPIO_SPEED_FREQ_LOW,
