b775e833718f9ff0b7d59f1f6e4f496f54fe1352
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-02-11 18:49:49 +0000

    Init feature One Wire branch

---
+++ tools/HALs/HAL-TOLOSAT/inc/tolosat_hal_ow.h
@@ -0,0 +1,39 @@
+/**
+ * @file    tolosat_hal_ow.h
+ * @author  Merlin Kooshmanian
+ * @brief   Header file for TOLOSAT HAL OW functions
+ * @date    11/02/2024
+ *
+ * @copyright Copyright (c) TOLOSAT 2024
+ */
+
+/**
+ * @defgroup hal_tolosat TOLOSAT HAL
+ * @{
+ * @defgroup drv_ow Driver One Wire
+ * Functions that allows to drive One Wire independantly of which 
+ * hardware or HAL is used
+ * @{
+ */
+
+#ifndef TOLOSAT_HAL_OW_H
+#define TOLOSAT_HAL_OW_H
+
+/******************************* Include Files *******************************/
+
+#include "tolosat_hal_types.h"
+
+/***************************** Macros Definitions ****************************/
+
+/***************************** Types Definitions *****************************/
+
+/*************************** Variables Declarations **************************/
+
+/*************************** Functions Declarations **************************/
+
+#endif /* TOLOSAT_HAL_OW_H */
+
+/** 
+ * @} 
+ * @} 
+ */
