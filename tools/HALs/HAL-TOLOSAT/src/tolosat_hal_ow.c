ff501b9537d2814ce9091848341014f0c9df5b60
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-02-12 09:34:42 +0100

    Add low level functions for OW

--- tools/HALs/HAL-TOLOSAT/src/tolosat_hal_ow.c
+++ tools/HALs/HAL-TOLOSAT/src/tolosat_hal_ow.c
@@ -13,6 +13,14 @@
 
 /***************************** Macros Definitions ****************************/
 
+#define OW_RESET_PULSE_DURATION 480u     /**< Amount of time the line need to be pulled down to initialise One Wire connection */
+#define OW_PRESENCE_WAIT_DURATION 70u    /**< Amount of time we need to wait until the slave will pull the line down */
+#define OW_PRESENCE_PULSE_DURATION 410u  /**< Amount of time we need to wait until the slave will release the line */
+#define OW_WRITE_1_PULL_DOWN_TIME_US 10u /**< Amount of time the line need to be pulled down to write 1 for One Wire */
+#define OW_WRITE_1_PULL_UP_TIME_US 55u   /**< Amount of time the line need to be pulled up to write 1 for One Wire */
+#define OW_WRITE_0_PULL_DOWN_TIME_US 65u /**< Amount of time the line need to be pulled down to write 0 for One Wire */
+#define OW_WRITE_0_PULL_UP_TIME_US 5u    /**< Amount of time the line need to be pulled up to write 0 for One Wire */
+
 /*************************** Functions Declarations **************************/
 
 static halStatus_t OwWriteByte(owInst_t *ow_inst, uint8_t byte);
@@ -20,8 +28,16 @@ static halStatus_t OwReadByte(owInst_t *ow_inst, uint8_t *byte);
 static halStatus_t OwInitConnection(owInst_t *ow_inst);
 static halStatus_t OwWriteBit(owInst_t *ow_inst, uint8_t bit);
 static halStatus_t OwReadBit(owInst_t *ow_inst, uint8_t *bit);
+static void OwTimerInit(void);
+static void OwDelayUs(uint32_t delay_us);
 
 /*************************** Variables Definitions ***************************/
+
+/**
+ * @var     ow_timer
+ * @brief   One Wire Timer instance declaration
+ */
+static TIM_HandleTypeDef ow_timer;
 
 /*************************** Functions Definitions ***************************/
 
@@ -41,12 +57,14 @@ halStatus_t OwOpen(owInst_t *ow_inst)
     // Function Core
     if (ow_inst != NULL)
     {
-        return_value = GpioOpen(&ow_inst->gpio_handle_struct);
+        return_value = GpioOpen(&ow_inst->gpio_inst);
     }
     else
     {
         return_value = THAL_INVALID_PARAM;
     }
+
+    OwTimerInit();
 
     return return_value;
 }
@@ -165,7 +183,7 @@ halStatus_t OwClose(owInst_t *ow_inst)
     // Function Core
     if (ow_inst != NULL)
     {
-        return_value = GpioClose(&ow_inst->gpio_handle_struct);
+        return_value = GpioClose(&ow_inst->gpio_inst);
     }
     else
     {
@@ -243,10 +261,11 @@ static halStatus_t OwReadByte(owInst_t *ow_inst, uint8_t *byte)
 
 /**
  * @fn              OwInitConnection(owInst_t *ow_inst)
- * @brief           Function that initialize a One Wire connection 
+ * @brief           Function that initialize a One Wire connection
  * @param[in,out]   ow_inst Instance that contains One Wire parameters handlers
  * @retval          #THAL_INVALID_PARAM if ow_inst is a null pointer
- * @retval          #THAL_ERROR if an error occured when using GPIO
+ * @retval          #THAL_BUSY line is busy, somebody is pulling the line low
+ * @retval          #THAL_BUSY if nobody has answered the master after a reset pulse
  * @retval          #THAL_SUCCESSFUL else
  */
 static halStatus_t OwInitConnection(owInst_t *ow_inst)
@@ -257,7 +276,33 @@ static halStatus_t OwInitConnection(owInst_t *ow_inst)
     // Function Core
     if (ow_inst != NULL)
     {
-        (void)(ow_inst);
+        // First check the line is idle (pulled up)
+        gpioValue_t line_state = GPIO_PIN_RESET;
+        (void)GpioRead(&ow_inst->gpio_inst, &line_state);
+        if (line_state == GPIO_PIN_SET)
+        {
+            // First pulling the line down as a "reset pulse"
+            (void)GpioWrite(&ow_inst->gpio_inst, GPIO_PIN_RESET);
+            OwDelayUs(OW_RESET_PULSE_DURATION);
+
+            // Then release the line and wait for the slave to answer
+            (void)GpioWrite(&ow_inst->gpio_inst, GPIO_PIN_SET);
+            OwDelayUs(OW_PRESENCE_WAIT_DURATION);
+
+            // Then read the line
+            (void)GpioRead(&ow_inst->gpio_inst, &line_state);
+            OwDelayUs(OW_PRESENCE_WAIT_DURATION);
+
+            // Check if slave has answered
+            if (line_state != GPIO_PIN_RESET)
+            {
+                return_value = THAL_TIMEOUT;
+            }
+        }
+        else
+        {
+            return_value = THAL_BUSY;
+        }
     }
     else
     {
@@ -273,7 +318,6 @@ static halStatus_t OwInitConnection(owInst_t *ow_inst)
  * @param[in]   ow_inst Instance that contains One Wire parameters handlers
  * @param[in]   bit Bit to write
  * @retval      #THAL_INVALID_PARAM if there is a null pointer
- * @retval      #THAL_ERROR if an error occured when using GPIO
  * @retval      #THAL_SUCCESSFUL else
  */
 static halStatus_t OwWriteBit(owInst_t *ow_inst, uint8_t bit)
@@ -284,8 +328,22 @@ static halStatus_t OwWriteBit(owInst_t *ow_inst, uint8_t bit)
     // Function Core
     if (ow_inst != NULL)
     {
-        (void)(ow_inst);
-        (void)(bit);
+        if ((bit & 0x01u) == 0x01u)
+        {
+            // Write '1'
+            (void)GpioWrite(&ow_inst->gpio_inst, GPIO_PIN_RESET);
+            OwDelayUs(OW_WRITE_1_PULL_DOWN_TIME_US); // Delay for '1'
+            (void)GpioWrite(&ow_inst->gpio_inst, GPIO_PIN_SET);
+            OwDelayUs(OW_WRITE_1_PULL_UP_TIME_US); // Delay to complete the time slot
+        }
+        else
+        {
+            // Write '0'
+            (void)GpioWrite(&ow_inst->gpio_inst, GPIO_PIN_RESET);
+            OwDelayUs(OW_WRITE_0_PULL_DOWN_TIME_US); // Delay for '0'
+            (void)GpioWrite(&ow_inst->gpio_inst, GPIO_PIN_SET);
+            OwDelayUs(OW_WRITE_0_PULL_UP_TIME_US); // Delay to complete the time slot
+        }
     }
     else
     {
@@ -301,7 +359,6 @@ static halStatus_t OwWriteBit(owInst_t *ow_inst, uint8_t bit)
  * @param[in]   ow_inst Instance that contains One Wire parameters handlers
  * @param[in]   bit Bit to read
  * @retval      #THAL_INVALID_PARAM if there is a null pointer
- * @retval      #THAL_ERROR if an error occured when using GPIO
  * @retval      #THAL_SUCCESSFUL else
  */
 static halStatus_t OwReadBit(owInst_t *ow_inst, uint8_t *bit)
@@ -312,8 +369,15 @@ static halStatus_t OwReadBit(owInst_t *ow_inst, uint8_t *bit)
     // Function Core
     if (ow_inst != NULL)
     {
-        (void)(ow_inst);
-        (void)(bit);
+        gpioValue_t line_state = GPIO_PIN_RESET;
+        (void)GpioWrite(&ow_inst->gpio_inst, GPIO_PIN_RESET);
+        OwDelayUs(3); // Short delay
+        (void)GpioWrite(&ow_inst->gpio_inst, GPIO_PIN_SET);
+        OwDelayUs(10); // Wait for the device to respond
+        (void)GpioRead(&ow_inst->gpio_inst, &line_state);
+        OwDelayUs(45); // Wait to complete 60us period
+
+        *bit = (uint8_t)line_state;
     }
     else
     {
@@ -321,4 +385,23 @@ static halStatus_t OwReadBit(owInst_t *ow_inst, uint8_t *bit)
     }
 
     return return_value;
+}
+
+static void OwTimerInit(void) 
+{
+    __HAL_RCC_TIM5_CLK_ENABLE(); // Activez l'horloge du timer 5
+    ow_timer.Instance = TIM5;
+    ow_timer.Init.Prescaler = (uint32_t)((SystemCoreClock / 2) / 1000000) - 1; // 1 MHz Counter Clock
+    ow_timer.Init.CounterMode = TIM_COUNTERMODE_UP;
+    ow_timer.Init.Period = 0xFFFF; // Max period
+    ow_timer.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    ow_timer.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+    HAL_TIM_Base_Init(&ow_timer);
+    HAL_TIM_Base_Start(&ow_timer); // Start the timer
+}
+
+static void OwDelayUs(uint32_t delay_us)
+{
+    __HAL_TIM_SET_COUNTER(&ow_timer, 0); // Set the counter value to 0
+    while (__HAL_TIM_GET_COUNTER(&ow_timer) < delay_us); // Wait for the counter to reach the us input in the parameter
 }
