1df39a17a3ef0ad7310cc4d9e16392adcd3effd3
Clément <59148218+c-l-e-m-e-n-t@users.noreply.github.com>
2024-02-13 15:32:24 +0100

    update stack usage control

--- application/src/miso/miso.c
+++ application/src/miso/miso.c
@@ -24,6 +24,8 @@
 #include "string.h"
 #include <stdio.h>
 #include "../../core/inc/io_instances.h"
+#include "services/pus161.h"
+#include "tc_execution.h"
 
 /***************************** Macros Definitions ****************************/
 
@@ -31,10 +33,9 @@
 
 /* ================== get the idle time of the system ================== 
 * @brief           get the idle time of the system
-* @param[in,out]   void
 * @return          uint32_t : idle time
 ========================================================================*/
-static uint32_t getIdleTime(){
+uint32_t getIdleTime(){
     uint32_t IdleTime  = ulTaskGetIdleRunTimePercent();
     //TODO : supprimer le printf
     printf("Idle Time : %ld\n", IdleTime);
@@ -42,18 +43,58 @@ static uint32_t getIdleTime(){
 }
 
 /* ================== get the stack usage of the system ==================
-* @brief           get the stack usage of the system
-* @param[in,out]   void
-* @return          void
-*/
-void getStackUsage(){
-    UBaseType_t uxHighWaterMark;
-    uxHighWaterMark = uxTaskGetStackHighWaterMark( NULL );
-    printf("Stack Usage : %ld\n", uxHighWaterMark);
+* @brief           get the maximum stack usage of the system
+* @return          uint32 : stack usage
+========================================================================*/
+uint32_t getStackUsage()
+{
+    TaskStatus_t *pxTaskStatusArray;
+    volatile UBaseType_t uxArraySize, x;
+    unsigned long ulTotalRunTime;
+    uint32_t maxStackUsage = 100000000;
+
+    /* Take a snapshot of the number of tasks in case it changes while this
+    function is executing. */
+    uxArraySize = uxTaskGetNumberOfTasks();
+
+    /* Allocate a TaskStatus_t structure for each task.  An array could be
+    allocated statically at compile time. */
+    pxTaskStatusArray = pvPortMalloc( uxArraySize * sizeof( TaskStatus_t ) );
+
+    if( pxTaskStatusArray != NULL )
+    {
+        /* Generate raw status information about each task. */
+        uxArraySize = uxTaskGetSystemState( pxTaskStatusArray, uxArraySize, &ulTotalRunTime );
+
+        /* Check the stack for each task */
+        for( x = 0; x < uxArraySize; x++ )
+        {
+            if (pxTaskStatusArray[x].usStackHighWaterMark < maxStackUsage)
+            {
+                maxStackUsage = pxTaskStatusArray[x].usStackHighWaterMark;
+            }
+        }
+
+        /* The array is no longer needed, free the memory it consumes. */
+        vPortFree( pxTaskStatusArray );   
+    }
+
+    return maxStackUsage;
 }
 
 
 /*************************** Variables Definitions ***************************/
+
+/**
+ * @var     g_pus161_execution_table
+ * @brief   Execution table for incomming pus 161 TC 
+ * @warning Keys must be ordered from smallest to largest
+ */
+/*
+pusExecutionTable_t g_pus161_execution_table[NB_PUS161_EXECUTION] = 
+{
+    { BUILD_ROUTING_KEY(OBC_APID, 161u, 1u) , ExecuteS161SS1 , TM_NOT_REQUESTED },
+};*/
 
 /*************************** Functions Definitions ***************************/
 
@@ -65,11 +106,14 @@ void getStackUsage(){
 void IN_MISO_TEXT_SECTION MisoMain(void *task_dyn_conf)
 {
     // Variable Initialisation
-    uint32_t task_status;
+    uint32_t task_status, stackUsage, idleTime;
 
     // Initialisation
     task_status = InitPeriodicWait(task_dyn_conf);
     CheckErrors(task_status, FDIR_ERROR_HANDLER);
+    /*
+    task_status = CheckExecutionTable((pusExecutionTable_t *) &g_pus161_execution_table, NB_PUS161_EXECUTION);
+    CheckErrors(task_status, FDIR_ERROR_HANDLER);   */
 
 
     // Function Core
@@ -77,8 +121,42 @@ void IN_MISO_TEXT_SECTION MisoMain(void *task_dyn_conf)
     {
         task_status = WaitUntilNextPeriod(task_dyn_conf);
         CheckErrors(task_status, FDIR_ERROR_HANDLER);
-        uint32_t idleTime = getIdleTime();
-        printf("Idle Time : %ld\n", idleTime);
-        getStackUsage();
+
+
+        //Check TC presence
+        // Execute incoming TC
+        /*
+        const tcExecutionBasicBuffers_t basic_buffers = {TC_PUS161, NO_BUFFER_REF, TM_PUS161};
+        task_status = ExecuteTC((pusExecutionTable_t *)&g_pus161_execution_table, NB_PUS161_EXECUTION, basic_buffers);
+        CheckErrors(task_status, FDIR_NO_SANCTION);
+        */
+
+
+        //Check stack usage
+        //TODO : généraliser à toutes les taches
+        stackUsage = getStackUsage();
+        printf("Stack Usage : %ld\n", stackUsage); //TODO : supprimer le printf
+        if (stackUsage == 1)
+        {
+            //overflow
+        }
+        else if (stackUsage < 20)
+        {
+            //overflow proche
+        }
+
+        //Check runtimes
+        idleTime = getIdleTime();
+        printf("Idle Time : %ld\n", idleTime); //TODO : supprimer le printf
+
+        //Generate report
+        if ((idleTime > 80) || (stackUsage < 20))
+        {
+            //TODO : Generate report
+        }
+
+
+        //TODO : Life signal
+        
     }
 }
