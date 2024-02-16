405093c5946ef15dc3e4c2fe8956c8195f38d694
Clément <59148218+c-l-e-m-e-n-t@users.noreply.github.com>
2024-02-16 13:35:09 +0100

    debug + max des stacks fonctionnel (a mettre en forme)

--- middlewares/libpus/src/services/pus161.c
+++ middlewares/libpus/src/services/pus161.c
@@ -14,6 +14,7 @@
 #include "services/pus161.h"
 #include "pus_tools/tm_management.h"
 #include "pus_tools/endianness_management.h"
+#include "miso/miso.h"
 
 /***************************** Macros Definitions ****************************/
 
@@ -25,13 +26,10 @@
 
 /**
  * @fn          ExecuteS17SS1(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code)
- * @brief       Function that send S17SS2 TM (connexion report)
+ * @brief       Function that send S17SS2 TM (idle time report)
  * @param[in]   tc S17SS1 TC (this parameter is unused for these service and subservice)
  * @param[out]  tm S17SS2 TM that we will send
- * @param[out]  error_code Indicates which error has been encountered for S1SS8 TM
  * @retval      #PUS_INVALID_PARAM if a pointer is NULL
- * @retval      #PUS_ERROR if cannot build TM
- * @retval      #PUS_SUCCESSFUL else
  */
 pusStatus_t ExecuteS161SS1(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code)
 {
@@ -40,6 +38,7 @@ pusStatus_t ExecuteS161SS1(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_
 
     // Variable Initialisation
     pusStatus_t return_value = PUS_SUCCESSFUL;
+    pusData_t data[4] = {0};
 
     // Function Core
     if ((tm != NULL) && (error_code != NULL))
@@ -48,20 +47,55 @@ pusStatus_t ExecuteS161SS1(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_
         *error_code = PUS_EXECUTION_NO_ERROR;
 
         // Recuperation de l'idle processeur depuis miso
-        /*
-        uint32_t idle = 0;
-        uint32_t *idle_ptr = &idle;
-        idle = getIdleTime();
+        uint32_t idle = getIdleTime();
 
+        // Set up data
+        (void)memcpy((void *)&data, (void *)&idle, 4);
+        
 
         // Build TM 
-        pusStatus_t test_build = BuildTM(tm, 161u, 2u, (pusData_t *)&idle, 0);
-        if (test_build != PUS_SUCCESSFUL)
-        {
-            return_value = PUS_ERROR;
-            *error_code = PUS_EXECUTION_TM_BUILDING_FAILED;
-        }
-        */
+        return_value = BuildTM(tm, 161u, 2u, (pusData_t *)&data, 4);
+        
+    }
+    else
+    {
+        return_value = PUS_INVALID_PARAM;
+    }
+
+    return return_value;
+}
+
+/**
+ * @fn          ExecuteS17SS3(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code)
+ * @brief       Function that send S17SS4 TM (Stack usage report)
+ * @param[in]   tc S17SS3 TC (this parameter is unused for these service and subservice)
+ * @param[out]  tm S17SS4 TM that we will send
+ * @retval      #PUS_INVALID_PARAM if a pointer is NULL
+ */
+pusStatus_t ExecuteS161SS3(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code)
+{
+    // Unused Parameters
+    (void)(tc);
+
+    // Variable Initialisation
+    pusStatus_t return_value = PUS_SUCCESSFUL;
+    pusData_t data[4] = {0};
+
+    // Function Core
+    if ((tm != NULL) && (error_code != NULL))
+    {
+        // Error code Initialization
+        *error_code = PUS_EXECUTION_NO_ERROR;
+
+        // Get stack usage
+        uint32_t stack = getStackUsage();
+
+        // Set up data
+        (void)memcpy((void *)&data, (void *)&stack, 4);
+        
+        // Build TM 
+        return_value = BuildTM(tm, 161u, 4u, (pusData_t *)&data, 4);
+        
     }
     else
     {
