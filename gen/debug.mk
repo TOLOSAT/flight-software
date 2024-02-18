fcc747e532464dc328f846c66a41782157ceed9b
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-02-18 17:29:48 +0000

    The error preventing me from debugging with RAM was that the I-cache was being used and that gdb by default uses software breakpoints which don't take the cache into account.

--- gen/debug.mk
+++ gen/debug.mk
@@ -16,6 +16,7 @@ UPLOAD_CMDS += -c 'shutdown'
 else ifeq ($(LOAD_MEMORY), ram)
 UPLOAD_CMDS  = -c 'reset init'
 UPLOAD_CMDS += -c 'load_image $(TARGET)'
+UPLOAD_CMDS += -c 'gdb_breakpoint_override hard'
 UPLOAD_CMDS += -c 'reset'
 UPLOAD_CMDS += -c 'shutdown'
 else
@@ -29,6 +30,7 @@ DBG_CMDS += -c 'program $(TARGET)'
 DBG_CMDS += -c 'reset halt'
 else ifeq ($(LOAD_MEMORY), ram)
 DBG_CMDS  = -c 'reset init'
+DBG_CMDS += -c 'gdb_breakpoint_override hard'
 DBG_CMDS += -c 'load_image $(TARGET)'
 DBG_CMDS += -c 'reset halt'
 else
