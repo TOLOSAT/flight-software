eb2eede2c2a704cd19e6e16fde5d0c6affd99544
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-02-28 15:13:33 +0100

    Script for FS

--- gen/pre-build.mk
+++ gen/pre-build.mk
@@ -7,6 +7,7 @@
 pre-build :
 	${PYTHON} $(TOOLS_DIR)/pre-build/tasks-parser.py -i $(APPLICATION_CONF_DIR)/tasks_conf.csv -o $(PRE_BUILD_DIR)/conf
 	${PYTHON} $(TOOLS_DIR)/pre-build/buffers-parser.py -i $(APPLICATION_CONF_DIR)/buffers_conf.csv -o $(PRE_BUILD_DIR)/conf
+	${PYTHON} $(TOOLS_DIR)/pre-build/fs-parser.py -i $(APPLICATION_CONF_DIR)/fs_conf.csv -o $(PRE_BUILD_DIR)/conf
 	@echo "****************************"
 	@echo "****   Pre Build Done   ****"
 	@echo "****************************"
