d7957cc8354cdba513ad4bbdd86c96491d41d87e
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-02-28 14:26:26 +0100

    Few changes in csv format and script, added file csv

--- gen/pre-build.mk
+++ gen/pre-build.mk
@@ -5,8 +5,8 @@
 ##############################################
 
 pre-build :
-	${PYTHON} $(TOOLS_DIR)/tasks-parser.py -i $(APPLICATION_CONF_DIR)/tasks_conf.csv -o $(PRE_BUILD_DIR)/conf
-	${PYTHON} $(TOOLS_DIR)/buffers-parser.py -i $(APPLICATION_CONF_DIR)/buffers_conf.csv -o $(PRE_BUILD_DIR)/conf
+	${PYTHON} $(TOOLS_DIR)/pre-build/tasks-parser.py -i $(APPLICATION_CONF_DIR)/tasks_conf.csv -o $(PRE_BUILD_DIR)/conf
+	${PYTHON} $(TOOLS_DIR)/pre-build/buffers-parser.py -i $(APPLICATION_CONF_DIR)/buffers_conf.csv -o $(PRE_BUILD_DIR)/conf
 	@echo "****************************"
 	@echo "****   Pre Build Done   ****"
 	@echo "****************************"
