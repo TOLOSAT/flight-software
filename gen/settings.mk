3af7972a1a05830523781199ad2adaa0bee29d33
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-03-01 17:38:09 +0100

    Improved Makefiles

--- gen/settings.mk
+++ gen/settings.mk
@@ -7,16 +7,20 @@ include gen/cc_settings.mk
 ################# ENVIRONMENT ################
 ##############################################
 
+# Export the variable DOCKER_WARNING like that the message appear only once per make call
+export DOCKER_WARNING = no
+
 # Checks if the code is executed inside a docker container
-ifneq ($(MAKECMDGOALS), verif)
-ifneq ($(shell echo $$IS_A_DOCKER), yes)
+ifneq ($(shell echo $$DOCKER_WARNING), no)
 $(warning *************************************************************)
 $(warning ***** Not inside the docker. Environment is deprecated. *****)
 $(warning *****        Program will starts in few seconds.        *****)
 $(warning *************************************************************)
 do := $(shell sleep 3)
 endif
-endif
+
+# Number of processor in order to improve speed of compilation
+NUM_PROCESSORS = $(shell nproc)
 
 ##############################################
 ################### TOOLS ####################
