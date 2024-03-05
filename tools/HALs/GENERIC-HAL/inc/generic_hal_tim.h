47e5ec847a72b27fe665a3c4c3e0284ce71e4e40
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-03-05 15:49:12 +0000

    Init function working

--- tools/HALs/GENERIC-HAL/inc/generic_hal_tim.h
+++ tools/HALs/GENERIC-HAL/inc/generic_hal_tim.h
@@ -16,7 +16,7 @@
 
 /***************************** Macros Definitions ****************************/
 
-#define GENERIC_HAL_MAX_DELAY   1000U   /**< Max delay for function that uses ST HAL timer */
+#define GENERIC_HAL_MAX_DELAY   100000u   /**< Max delay for function that uses ST HAL timer */
 
 /***************************** Types Definitions *****************************/
 
