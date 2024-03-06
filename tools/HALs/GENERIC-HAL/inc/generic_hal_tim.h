de4363a2f9bfc7361fb9cd6a4ae8c46948e0820a
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-03-06 11:04:09 +0000

    Network availability added

--- tools/HALs/GENERIC-HAL/inc/generic_hal_tim.h
+++ tools/HALs/GENERIC-HAL/inc/generic_hal_tim.h
@@ -16,7 +16,7 @@
 
 /***************************** Macros Definitions ****************************/
 
-#define GENERIC_HAL_MAX_DELAY   2000u  /**< Max delay for function that uses ST HAL timer */
+#define GENERIC_HAL_MAX_DELAY   20u     /**< Max delay (in micro seconds) for function that uses ST HAL timer */
 
 /***************************** Types Definitions *****************************/
 
