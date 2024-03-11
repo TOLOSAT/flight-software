3aa2d05fc8feb19054756b875b8803ef5a42de65
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-03-11 18:23:14 +0000

    Few correction and remove STM32F1 support (processor too small to support TAPAS or even a test)

--- tools/HALs/GENERIC-HAL/inc/generic_hal_types.h
+++ tools/HALs/GENERIC-HAL/inc/generic_hal_types.h
@@ -19,8 +19,6 @@
 #include "stm32h7xx_hal.h"
 #elif defined(NUCLEO_F411RE) || defined(DISCOVERY_F407VG)
 #include "stm32f4xx_hal.h"
-#elif defined(NUCLEO_F103RB)
-#include "stm32f1xx_hal.h"
 #else 
 #error "Board is not supported"
 #endif
