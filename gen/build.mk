bd521f57cfbcc35bf80659041eacb05598340133
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-03-25 17:46:25 +0100

    WIP : making bootloader independant from flight software

--- gen/build.mk
+++ gen/build.mk
@@ -7,7 +7,6 @@ include gen/build_os.mk
 include gen/build_middlewares.mk
 include gen/build_hal.mk
 include gen/build_bsp.mk
-include gen/build_boot.mk
 
 ##############################################
 #################### BUILD ###################
@@ -16,17 +15,13 @@ include gen/build_boot.mk
 # Targets definitions
 TARGET_DBG		= $(TARGET_DIR)/$(PROJ_NAME)-$(VERSION).elf
 TARGET_RLS		= $(TARGET_DIR)/$(PROJ_NAME)-$(VERSION).elf
-BOOT_TARGET_DBG	= $(TARGET_DIR)/boot-software-$(VERSION).elf
-BOOT_TARGET_RLS	= $(TARGET_DIR)/boot-software-$(VERSION).elf
 
 # Target definition according to version
 ifeq ($(VERSION), debug)
 TARGET 			= $(TARGET_DBG)
-BOOT_TARGET 	= $(BOOT_TARGET_DBG)
 VERSION_FLAGS 	= $(DEBUG_FLAGS)
 else ifeq ($(VERSION), release)
 TARGET 			= $(TARGET_RLS)
-BOOT_TARGET 	= $(BOOT_TARGET_RLS)
 VERSION_FLAGS 	= $(RELEASE_FLAGS)
 else
 $(error Please select debug or release)
@@ -43,40 +38,18 @@ PRIVATE_LIBS = $(foreach lib,$(PRIVATE_COMPONENTS),-l$(lib)-$(VERSION))
 PUBLIC_LIBS = $(foreach lib,$(PUBLIC_COMPONENTS),-l$(lib)-$(VERSION))
 
 ##############################################
-########## BOOT BUILD CONFIGURATION ##########
-##############################################
-
-BOOT_LDFLAGS = $(subst -DLOAD_RAM,-DLOAD_FLASH,$(PROJECT_LDFLAGS))
-
-BOOT_PRIVATE_COMPONENTS = bootsw generic-hal
-BOOT_PUBLIC_COMPONENTS = hal fatfs
-
-BOOT_PRIVATE_LIBS = $(foreach lib,$(BOOT_PRIVATE_COMPONENTS),-l$(lib)-$(VERSION))
-BOOT_PUBLIC_LIBS = $(foreach lib,$(BOOT_PUBLIC_COMPONENTS),-l$(lib)-$(VERSION))
-
-##############################################
 ################ BUILD RECIPE ################
 ##############################################
 
-.PHONY += build boot
+.PHONY += build
 
 build : $(TARGET)
 
-boot : $(BOOT_TARGET)
-
 # Target Linking Stage
-$(TARGET) : $(PUBLIC_COMPONENTS) $(PRIVATE_COMPONENTS)
+$(TARGET) : $(PRIVATE_COMPONENTS) $(PUBLIC_COMPONENTS)
 	mkdir -p $(@D)
 	$(CC) -L$(BUILD_LIBS_DIR) -Wl,--whole-archive $(PRIVATE_LIBS) -Wl,--no-whole-archive $(PUBLIC_LIBS) $(PROJECT_LDFLAGS) -T $(LINKER_SCRIPT) -o $@ > $(@:.elf=.size)
 	$(READELF) -a $@ > $(@:.elf=.readelf)
 	@echo "*****************************"
 	@echo "***   Target Build Done   ***"
-	@echo "*****************************"
-
-$(BOOT_TARGET) : $(BOOT_PUBLIC_COMPONENTS) $(BOOT_PRIVATE_COMPONENTS)
-	mkdir -p $(@D)
-	$(CC) -L$(BUILD_LIBS_DIR) -Wl,--whole-archive $(BOOT_PRIVATE_LIBS) -Wl,--no-whole-archive $(BOOT_PUBLIC_LIBS) $(PROJECT_LDFLAGS) -T $(LINKER_SCRIPT:$(LOAD_MEMORY).ld=boot.ld) -o $@ > $(@:.elf=.size)
-	$(READELF) -a $@ > $(@:.elf=.readelf)
-	@echo "*****************************"
-	@echo "****   Boot Build Done   ****"
 	@echo "*****************************"
