c19544215d700a549f7dd68e1846c12d2ff08ba4
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-03-29 15:29:51 +0100

    Better organised file system

--- gen/build.mk
+++ gen/build.mk
@@ -50,8 +50,8 @@ $(TARGET) : $(PRIVATE_COMPONENTS) $(PUBLIC_COMPONENTS)
 	mkdir -p $(@D)
 	$(CC) -L$(BUILD_LIBS_DIR) -Wl,--whole-archive $(PRIVATE_LIBS) -Wl,--no-whole-archive $(PUBLIC_LIBS) $(PROJECT_LDFLAGS) -T $(LINKER_SCRIPT) -o $@ > $(@:.elf=.size)
 	$(READELF) -a $@ > $(@:.elf=.readelf)
-	$(STRIP) $@ -o $(@D)/PROGRAM.ELF
-	$(PYTHON) $(TOOLS_DIR)/crc32-gen.py $(@D)/PROGRAM.ELF -o $(@D)/PROGRAM.ELF
+	$(STRIP) $@ -o $(@D)/program.elf
+	$(PYTHON) $(TOOLS_DIR)/crc32-gen.py $(@D)/program.elf -o $(@D)/program.elf
 	@echo "*****************************"
 	@echo "***   Target Build Done   ***"
 	@echo "*****************************"
