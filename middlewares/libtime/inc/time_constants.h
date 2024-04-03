7454f2a6f334d8c00fc91e785d9db90d06f8ac55
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-04-03 22:37:48 +0200

    Start to replace dummy header by CUC time

--- middlewares/libtime/inc/time_constants.h
+++ middlewares/libtime/inc/time_constants.h
@@ -19,5 +19,6 @@
 #define CUC_TIME_SIZE                   (CUC_HEAD_SIZE + COARSE_TIME_SIZE + FINE_TIME_SIZE) /**< CUC time variables size */
 #define TIME_HEAD_CONSTANT              0x1du                                               /**< P-field for CUC time (equivalent of 0b00011111u)*/
 #define TAI_UNIX_OFFSET                 378691200u                                          /**< Number of seconds between TAI Ref (January 1rst 1958) and UNIX Ref (January 1rst 1970) */
+#define CUC_TIME_STR_SIZE               (2*CUC_TIME_SIZE)                                   /**< Number of char needed to represent CUC time as a string (2 char are need to represent 1 uint8_t)*/
 
 #endif /* TIME_CONSTANTS_H */
