f0788adc1ef2c5cf26d0a749bb73cd71d09520d2
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-04-03 22:37:48 +0200

    Start to replace dummy header by CUC time

--- middlewares/libtime/inc/time_management.h
+++ middlewares/libtime/inc/time_management.h
@@ -43,6 +43,7 @@ typedef enum
 
 /*************************** Functions Declarations **************************/
 
+timeStatus_t GetStrCUCTime(char cuc_time_str[CUC_TIME_STR_SIZE]);
 timeStatus_t GetCUCTime(cucTime_t *cuc_time);
 timeStatus_t SetCUCTime(cucTime_t *cuc_time);
 timeStatus_t CompareCUCTimes(cucTime_t *older_cuc_time, cucTime_t *newer_cuc_time);
