0ca34e3c36e85054d484751c076526775557ca0b
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-04-14 11:54:49 +0200

    Improve configuratio menu (test support and load current config)

--- gen/menuconfig.sh
+++ gen/menuconfig.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
-# Variables initialisation
-PROJ_NAME="flight-software"
-VERSION="debug"
-BOARD="ART_PI"
-LOAD_MEMORY="RAM"
-CONSOLE_MODE="FILE"
-FS_MODE="SDMMC"
+# Menu Variables 
+CURR_OPTION="1"
 
+# Configuration Variables
+PROJ_NAME="flight-software"
+VERSION=""
+BOARD=""
+LOAD_MEMORY=""
+CONSOLE_MODE=""
+FS_MODE=""
+TEST=""
+
+# First load the project configuration if a config file exists
+if [ -f ".config" ]; then
+    while IFS='=' read -r key value; do
+        case "$key" in
+            "PROJ_NAME") PROJ_NAME=$value ;;
+            "VERSION") VERSION=$value ;;
+            "BOARD") BOARD=$value ;;
+            "LOAD_MEMORY") LOAD_MEMORY=$value ;;
+            "CONSOLE_MODE") CONSOLE_MODE=$value ;;
+            "FS_MODE") FS_MODE=$value ;;
+            "TEST") TEST=$value ;;
+        esac
+    done < ".config"
+fi
+
+# Configuration Menu
 while true; do
     EXEC=$(dialog --clear --backtitle "TAPAS Configuration" \
-        --title "Menu Principal" \
+        --title "Main Menu" \
         --ok-label "Select" \
         --extra-button --extra-label "Save" \
         --cancel-label "Exit" \
+        --default-item "$CURR_OPTION" \
         --menu "Please configure the flight software using the arrows and \
                 the enter key. Use <Save> button to save your changes at any time." 18 65 5 \
         "1" "Project Name ($PROJ_NAME)" \
@@ -22,6 +43,7 @@ while true; do
         "4" "Memory Load ($LOAD_MEMORY)" \
         "5" "Console Mode ($CONSOLE_MODE)" \
         "6" "File System Mode ($FS_MODE)" \
+        "7" "Target Test ($TEST)" \
         3>&1 1>&2 2>&3 3>&- )
 
     # Check if Save was pressed
@@ -34,14 +56,19 @@ while true; do
         echo "LOAD_MEMORY=$LOAD_MEMORY" >> .config
         echo "CONSOLE_MODE=$CONSOLE_MODE" >> .config
         echo "FS_MODE=$FS_MODE" >> .config
+        echo "TEST=$TEST" >> .config
         dialog --title "Configuration Save" --msgbox "Configurations saved successfully!" 6 60
         continue
     fi
 
+    # Update current option in the menu
+    CURR_OPTION=$EXEC
+
+    # Then goes to the chosen submenu
     case $EXEC in
         1)
-            # Input box for Project Name
-            PROJ_NAME=$(dialog --stdout --backtitle "Project Configuration" \
+            # Menu for project name selection
+            PROJ_NAME=$(dialog --stdout --backtitle "TAPAS Configuration" \
                                --title "Project Name" \
                                --inputbox "Enter the new project name:" 8 50 "$PROJ_NAME")
             if [ $? -ne 0 ]; then
@@ -49,6 +76,7 @@ while true; do
             fi
             ;;
         2)
+            # Menu for version selection
             EXEC=$(dialog --clear --backtitle "TAPAS Configuration" \
                 --title "Version Selection" \
                 --ok-label "Select" \
@@ -67,6 +95,7 @@ while true; do
             esac
             ;;
         3)
+            # Menu for board selection
             EXEC=$(dialog --clear --backtitle "TAPAS Configuration" \
                 --title "Board Selection" \
                 --ok-label "Select" \
@@ -93,6 +122,7 @@ while true; do
             esac
             ;;
         4)
+            # Menu for load memory selection
             EXEC=$(dialog --clear --backtitle "TAPAS Configuration" \
                 --title "Load Memory Selection" \
                 --ok-label "Select" \
@@ -111,6 +141,7 @@ while true; do
             esac
             ;;
         5)
+            # Menu for console mode selection
             EXEC=$(dialog --clear --backtitle "TAPAS Configuration" \
                 --title "Console Selection" \
                 --ok-label "Select" \
@@ -137,6 +168,7 @@ while true; do
             esac
             ;;
         6)
+            # Menu for file system mode selection
             EXEC=$(dialog --clear --backtitle "TAPAS Configuration" \
                 --title "File System Selection" \
                 --ok-label "Select" \
@@ -157,6 +189,15 @@ while true; do
                     FS_MODE="SDMMC"
                     ;;
             esac
+            ;;
+        7)
+            # Menu for test selection
+            TEST=$(dialog --stdout --backtitle "TAPAS Configuration" \
+                               --title "Target Test" \
+                               --inputbox "Enter the test name. Leave empty if you want to run the full flight software" 8 50 "$TEST")
+            if [ $? -ne 0 ]; then
+                TEST=""
+            fi
             ;;
         *)
             break
