14d7d7876765b4897145526fd4944c049c5439b1
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-04-30 20:30:32 +0200

    Corrected some paths and did some platform reduction

--- tests/test-mpu/inc/platform.h
+++ tests/test-mpu/inc/platform.h
@@ -23,14 +23,10 @@
 
 /*************************** Variables Declarations **************************/
 
-extern iicInst_t iic_avionic_inst;
-extern uartInst_t uart_tmtc_inst;
-extern uartInst_t uart_print_inst;
-extern uartInst_t uart_pl_inst;
-extern spiInst_t spi_avionic_inst;
-extern owInst_t one_wire_inst;
 extern gpioInst_t led_inst;
 extern gpioInst_t user_button_inst;
+extern uartInst_t uart_print_inst;
+extern spiInst_t spi_avionic_inst;
 extern gpioInst_t sd_card_gpio;
 extern fsInst_t sd_fs_inst;
 
