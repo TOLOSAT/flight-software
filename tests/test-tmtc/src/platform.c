15d3b4b5c8c4b62af49dd7ef4d59fe29775e2fa5
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-04-30 20:17:02 +0200

    Moved platform to the application(s) folder(s)

---
+++ tests/test-tmtc/src/platform.c
@@ -0,0 +1,237 @@
+/**
+ * @file    platform.c
+ * @author  Merlin Kooshmanian
+ * @brief   Source file stocking platform informations
+ * @date    30/04/2024
+ *
+ * @copyright Copyright (c) TOLOSAT 2024
+ */
+
+/******************************* Include Files *******************************/
+
+#include "platform.h"
+#include "core_basics.h"
+
+/***************************** Macros Definitions ****************************/
+
+/*************************** Functions Declarations **************************/
+
+extern void USER_BUTTON_IRQ_HANDLER(void);
+extern void I2C_AVIONIC_EVT_IRQ_HANDLER(void);
+extern void UART_PL_IRQ_HANDLER(void);
+extern void UART_TMTC_IRQ_HANDLER(void);
+extern void UART_TMTC_DMA_RX_IRQ_HANDLER(void);
+extern void UART_TMTC_DMA_TX_IRQ_HANDLER(void);
+
+/***************************** External Variables ****************************/
+
+extern DMA_HandleTypeDef UART_TMTC_DMA_RX;
+extern DMA_HandleTypeDef UART_TMTC_DMA_TX;
+
+/*************************** Variables Definitions ***************************/
+
+/**
+ * @var     uart_tmtc_inst
+ * @brief   UART tmtc instance declaration
+ */
+uartInst_t uart_tmtc_inst = {
+    .uart_ref = UART_TMTC,
+    .drive_type = UART_DMA_DRIVE,
+    .baudrate = 115200,
+};
+
+/**
+ * @var     uart_print_inst
+ * @brief   UART print instance declaration
+ */
+uartInst_t uart_print_inst = {
+    .uart_ref = UART_PRINT,
+    .drive_type = UART_POLLING_DRIVE,
+    .baudrate = 115200,
+};
+
+/**
+ * @var     uart_pl_inst
+ * @brief   UART payload instance declaration
+ */
+uartInst_t uart_pl_inst = {
+    .uart_ref = UART_PL,
+    .drive_type = UART_INTERRUPT_DRIVE,
+    .baudrate = 115200,
+};
+
+/**
+ * @var     iic_avionic_inst
+ * @brief   I2C avionic instance declaration
+ */
+iicInst_t iic_avionic_inst = {
+    .iic_ref = I2C_AVIONIC,
+    .drive_type = IIC_IT_MASTER_DRIVE,
+};
+
+/**
+ * @var     spi_avionic_inst
+ * @brief   SPI avionic instance declaration
+ */
+spiInst_t spi_avionic_inst = {
+    .spi_ref = SPI_AVIONIC,
+    .drive_type = SPI_POLLING_MASTER_DRIVE,
+    .prescaler = SPI_BAUDRATEPRESCALER_8,
+};
+
+/**
+ * @var     one_wire_inst
+ * @brief   One Wire instance declaration
+ */
+owInst_t one_wire_inst = {
+    .gpio_inst.port = ONEWIRE_GPIO_PORT,
+    .gpio_inst.pin = ONEWIRE_PIN,
+    .gpio_inst.mode = GPIO_MODE_OUTPUT_OD,
+    .gpio_inst.pull = GPIO_NOPULL,
+    .gpio_inst.speed = GPIO_SPEED_FREQ_MEDIUM,
+};
+
+/**
+ * @var     led_inst
+ * @brief   GPIO led instance declaration
+ */
+gpioInst_t led_inst = {
+    .port = USER_LED_GPIO_PORT,
+    .pin = USER_LED_PIN,
+    .mode = GPIO_MODE_OUTPUT_PP,
+    .pull = GPIO_NOPULL,
+    .speed = GPIO_SPEED_FREQ_LOW,
+};
+
+/**
+ * @var     user_button_inst
+ * @brief   GPIO button instance declaration
+ */
+gpioInst_t user_button_inst = {
+    .port = USER_BUTTON_GPIO_PORT,
+    .pin = USER_BUTTON_PIN,
+    .mode = GPIO_MODE_IT_FALLING,
+    .pull = GPIO_NOPULL,
+    .speed = GPIO_SPEED_FREQ_LOW,
+};
+
+/**
+ * @var     sd_card_gpio
+ * @brief   GPIO for sd card (cs or card detect depend of the context) instance declaration
+ */
+gpioInst_t sd_card_gpio = {
+    .port = SD_GPIO_PORT,
+    .pin = SD_GPIO_PIN,
+    .mode = GPIO_MODE_OUTPUT_PP,
+    .pull = GPIO_NOPULL,
+    .speed = GPIO_SPEED_FREQ_LOW,
+};
+
+/**
+ * @var     sd_fs_inst
+ * @brief   File System instance declaration
+ */
+fsInst_t sd_fs_inst = {0};
+
+/*************************** Functions Definitions ***************************/
+
+/**
+ * @fn      PlatformInit(void)
+ * @brief   Function that initialise the platform
+ */
+uint32_t PlatformInit(void)
+{
+    // Variable Initialisation
+    uint32_t status = 0u;
+
+    // GPIOs Initialisation
+    status = GpioOpen(&led_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+    status = GpioOpen(&user_button_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+    status = GpioOpen(&sd_card_gpio);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+
+    // UARTs Initialisation
+    status = UartOpen(&uart_print_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+    status = UartOpen(&uart_tmtc_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+    status = UartOpen(&uart_pl_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+
+    // I2Cs Initialisation
+    status = IicOpen(&iic_avionic_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+
+    // SPIs Initialisation
+    status = SpiOpen(&spi_avionic_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+
+    // OneWire Initialisation
+    status = OwOpen(&one_wire_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+
+    // File System Initialisation
+    status = FsOpen(&sd_fs_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+
+    return status;
+}
+
+/*************************** Interruption Handlers ***************************/
+
+/**
+ * @brief This function is the BUTTON interruption handler.
+ */
+void USER_BUTTON_IRQ_HANDLER(void)
+{
+    // First clear interrupt flag
+    if (__HAL_GPIO_EXTI_GET_IT(USER_BUTTON_PIN) != 0x00U)
+    {
+    __HAL_GPIO_EXTI_CLEAR_IT(USER_BUTTON_PIN);
+    }
+
+    // Then do the interrupt routine
+    /* Do something here */
+}
+
+/**
+ * @brief This function handles I2C_AVIONIC event interrupt.
+ */
+void I2C_AVIONIC_EVT_IRQ_HANDLER(void)
+{
+    HAL_I2C_EV_IRQHandler(&iic_avionic_inst.handle_struct);
+}
+
+/**
+ * @brief This function handles USART_PL global interrupt.
+ */
+void UART_PL_IRQ_HANDLER(void)
+{
+    HAL_UART_IRQHandler(&uart_pl_inst.handle_struct);
+}
+
+/**
+ * @brief This function handles USART_TMTC global interrupt.
+ */
+void UART_TMTC_IRQ_HANDLER(void)
+{
+    HAL_UART_IRQHandler(&uart_tmtc_inst.handle_struct);
+}
+
+/**
+ * @brief This function handles RX DMA for USART_TMTC global interrupt.
+ */
+void UART_TMTC_DMA_RX_IRQ_HANDLER(void)
+{
+    HAL_DMA_IRQHandler(&UART_TMTC_DMA_RX);
+}
+
+/**
+ * @brief This function handles TX DMA for USART_TMTC global interrupt.
+ */
+void UART_TMTC_DMA_TX_IRQ_HANDLER(void)
+{
+    HAL_DMA_IRQHandler(&UART_TMTC_DMA_TX);
+}
