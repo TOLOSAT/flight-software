85d5cc9ee492aad6d5e90b169518e8c1983e4edc
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-05-24 20:16:39 +0200

    Added a makefile per app that contains build component, the goal is to compile only needed components

---
+++ tests/test-iridium/application.mk
@@ -0,0 +1,5 @@
+# Application Specific Makefile
+
+PRIVATE_COMPONENTS = application core time fs iridiumdrv generic-hal bsp
+PUBLIC_COMPONENTS = os hal fatfs
+
---
+++ tests/test-tmtc/application.mk
@@ -0,0 +1,5 @@
+# Application Specific Makefile
+
+PRIVATE_COMPONENTS = application core pus time fs generic-hal bsp
+PUBLIC_COMPONENTS = os hal fatfs
+
