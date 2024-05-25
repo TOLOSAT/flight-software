4ebffdd27624af44a00da9ce8a5af7577ed4ecbf
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-05-25 16:46:16 +0200

    Added a test case for testing the mutex (then will be used for suspension) and added a check before trying to release a mutex

---
+++ tests/test-suspension/src/dummy_tasks.c
@@ -0,0 +1,120 @@
+/**
+ * @file    dummy_tasks.c
+ * @author  Merlin Kooshmanian
+ * @brief   Source file with dummy tasks
+ * @date    26/04/2023
+ *
+ * @copyright Copyright (c) TOLOSAT 2024
+ */
+
+/******************************* Include Files *******************************/
+
+#include "dummy_tasks.h"
+#include "core_basics.h"
+#include "platform.h"
+
+/***************************** Macros Definitions ****************************/
+
+/*************************** Functions Declarations **************************/
+
+/*************************** Variables Definitions ***************************/
+
+extern gpioInst_t led_inst;
+
+/*************************** Functions Definitions ***************************/
+
+/**
+ * @fn      DummyMainTask01(void *task_dyn_conf)
+ * @brief   Function that runs the dummy main task.
+ * @param   task_dyn_conf Status of the current task
+ */
+void DummyMainTask01(void *task_dyn_conf)
+{
+    // Variable Initialisation
+    uint32_t task_status;
+    coreStatus_t test;
+
+    // Initialisation
+    ConsolePrint("[#1] Init\n");
+    task_status = InitPeriodicWait(task_dyn_conf);
+    CheckErrors(task_status, FDIR_ERROR_HANDLER);
+
+    // Function Core
+    while (1)
+    {
+        (void)GpioToggle(&led_inst);
+
+        test = AcquireMutex(DUMMY_MUTEX);
+        if (test == CORE_SUCCESSFUL)
+        {
+            ConsolePrint("[#1] Mutex Acquired\n");
+
+            task_status = WaitUntilNextPeriod(task_dyn_conf);
+            CheckErrors(task_status, FDIR_ERROR_HANDLER);
+
+            test = ReleaseMutex(DUMMY_MUTEX);
+            if (test == CORE_SUCCESSFUL)
+            {
+                ConsolePrint("[#1] Mutex Released\n");
+            }
+            else
+            {
+                ConsolePrint("[#1] Error releasing the mutex\n");
+            }
+        }
+        else
+        {
+            ConsolePrint("[#1] Error acquiring the mutex\n");
+        }
+
+        task_status = WaitUntilNextPeriod(task_dyn_conf);
+        CheckErrors(task_status, FDIR_ERROR_HANDLER);
+    }
+}
+
+/**
+ * @fn      DummyMainTask02(void *task_dyn_conf)
+ * @brief   Function that runs the dummy main task.
+ * @param   task_dyn_conf Status of the current task
+ */
+void DummyMainTask02(void *task_dyn_conf)
+{
+    // Variable Initialisation
+    uint32_t task_status;
+    coreStatus_t test;
+
+    // Initialisation
+    ConsolePrint("[#2] Init\n");
+    task_status = InitPeriodicWait(task_dyn_conf);
+    CheckErrors(task_status, FDIR_ERROR_HANDLER);
+
+    // Function Core
+    while (1)
+    {
+        test = AcquireMutex(DUMMY_MUTEX);
+        if (test == CORE_SUCCESSFUL)
+        {
+            ConsolePrint("[#2] Mutex Acquired\n");
+
+            task_status = WaitUntilNextPeriod(task_dyn_conf);
+            CheckErrors(task_status, FDIR_ERROR_HANDLER);
+
+            test = ReleaseMutex(DUMMY_MUTEX);
+            if (test == CORE_SUCCESSFUL)
+            {
+                ConsolePrint("[#2] Mutex Released\n");
+            }
+            else
+            {
+                ConsolePrint("[#2] Error releasing the mutex\n");
+            }
+        }
+        else
+        {
+            ConsolePrint("[#2] Error acquiring the mutex\n");
+        }
+
+        task_status = WaitUntilNextPeriod(task_dyn_conf);
+        CheckErrors(task_status, FDIR_ERROR_HANDLER);
+    }
+}
---
+++ tests/test-suspension/src/platform.c
@@ -0,0 +1,136 @@
+/**
+ * @file    platform.c
+ * @author  Merlin Kooshmanian
+ * @brief   Source file stocking platform informations
+ * @date    30/04/2024
+ *
+ * @copyright Copyright (c) TOLOSAT 2024
+ */
+
+/******************************* Include Files *******************************/
+
+#include "platform.h"
+#include "core_basics.h"
+
+/***************************** Macros Definitions ****************************/
+
+/*************************** Functions Declarations **************************/
+
+extern void USER_BUTTON_IRQ_HANDLER(void);
+
+/***************************** External Variables ****************************/
+
+/*************************** Variables Definitions ***************************/
+
+/**
+ * @var     uart_print_inst
+ * @brief   UART print instance declaration
+ */
+uartInst_t IN_UART_DATA_SECTION uart_print_inst = {
+    .uart_ref = UART_PRINT,
+    .drive_type = UART_POLLING_DRIVE,
+    .baudrate = 115200,
+};
+
+/**
+ * @var     spi_avionic_inst
+ * @brief   SPI avionic instance declaration
+ */
+spiInst_t IN_SPI_DATA_SECTION spi_avionic_inst = {
+    .spi_ref = SPI_AVIONIC,
+    .drive_type = SPI_POLLING_MASTER_DRIVE,
+    .prescaler = SPI_BAUDRATEPRESCALER_8,
+};
+
+/**
+ * @var     led_inst
+ * @brief   GPIO led instance declaration
+ */
+gpioInst_t IN_GPIO_DATA_SECTION led_inst = {
+    .port = USER_LED_GPIO_PORT,
+    .pin = USER_LED_PIN,
+    .mode = GPIO_MODE_OUTPUT_PP,
+    .pull = GPIO_NOPULL,
+    .speed = GPIO_SPEED_FREQ_LOW,
+};
+
+/**
+ * @var     user_button_inst
+ * @brief   GPIO button instance declaration
+ */
+gpioInst_t IN_GPIO_DATA_SECTION user_button_inst = {
+    .port = USER_BUTTON_GPIO_PORT,
+    .pin = USER_BUTTON_PIN,
+    .mode = GPIO_MODE_IT_FALLING,
+    .pull = GPIO_NOPULL,
+    .speed = GPIO_SPEED_FREQ_LOW,
+};
+
+/**
+ * @var     sd_card_gpio
+ * @brief   GPIO for sd card (cs or card detect depend of the context) instance declaration
+ */
+gpioInst_t IN_GPIO_DATA_SECTION sd_card_gpio = {
+    .port = SD_GPIO_PORT,
+    .pin = SD_GPIO_PIN,
+    .mode = GPIO_MODE_OUTPUT_PP,
+    .pull = GPIO_NOPULL,
+    .speed = GPIO_SPEED_FREQ_LOW,
+};
+
+/**
+ * @var     sd_fs_inst
+ * @brief   File System instance declaration
+ */
+fsInst_t IN_FS_DATA_SECTION sd_fs_inst = {0};
+
+/*************************** Functions Definitions ***************************/
+
+/**
+ * @fn      PlatformInit(void)
+ * @brief   Function that initialise the platform
+ */
+uint32_t IN_INIT_TEXT_SECTION PlatformInit(void)
+{
+    // Variable Initialisation
+    uint32_t status = 0u;
+
+    // GPIOs Initialisation
+    status = GpioOpen(&led_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+    status = GpioOpen(&user_button_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+    status = GpioOpen(&sd_card_gpio);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+
+    // UARTs Initialisation
+    status = UartOpen(&uart_print_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+
+    // SPIs Initialisation
+    status = SpiOpen(&spi_avionic_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+
+    // File System Initialisation
+    status = FsOpen(&sd_fs_inst);
+    CheckErrors(status, FDIR_ERROR_HANDLER);
+
+    return status;
+}
+
+/*************************** Interruption Handlers ***************************/
+
+/**
+ * @brief This function is the BUTTON interruption handler.
+ */
+void IN_GPIO_TEXT_SECTION USER_BUTTON_IRQ_HANDLER(void)
+{
+    // First clear interrupt flag
+    if (__HAL_GPIO_EXTI_GET_IT(USER_BUTTON_PIN) != 0x00U)
+    {
+    __HAL_GPIO_EXTI_CLEAR_IT(USER_BUTTON_PIN);
+    }
+
+    // Then do the interrupt routine
+    /* Do something here */
+}
