b91a5e549f92a65473a9d8df7f51c4674910c60d
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-06-15 15:35:37 +0200

    Added the possibility to upload/debug QEMU

--- middlewares/generic-hal/src/ARM-wrapper/generic_hal_rtc.c
+++ middlewares/generic-hal/src/ARM-wrapper/generic_hal_rtc.c
@@ -13,6 +13,14 @@
 
 /***************************** Macros Definitions ****************************/
 
+#define RTC_DEFAULT_YEAR        0u      /**< Default year alias 2000 */
+#define RTC_DEFAULT_MONTH       1u      /**< Default month alias january */
+#define RTC_DEFAULT_DAY         1u      /**< Default day alias 1rst */
+#define RTC_DEFAULT_HOUR        0u      /**< Default hour alias 0 */
+#define RTC_DEFAULT_MINUTE      0u      /**< Default minute alias 0 */
+#define RTC_DEFAULT_SECOND      0u      /**< Default second alias 0 */
+#define RTC_DEFAULT_MILLISECOND 0u      /**< Default second alias 0 */
+
 /*************************** Functions Declarations **************************/
 
 /*************************** Variables Definitions ***************************/
@@ -55,6 +63,13 @@ halStatus_t IN_RTC_TEXT_SECTION RtcSetTime(const rtcTime_t *rtc_time)
  */
 halStatus_t IN_RTC_TEXT_SECTION RtcGetTime(rtcTime_t *rtc_time)
 {
-    (void)(rtc_time);
+    uint32_t tick_sec = HalGetTick()/1000u;
+    rtc_time->year = RTC_DEFAULT_YEAR;
+    rtc_time->month = RTC_DEFAULT_MONTH;
+    rtc_time->day = RTC_DEFAULT_DAY;
+    rtc_time->hour = (tick_sec/3600u)%24;
+    rtc_time->minute = (tick_sec/60u)%60;
+    rtc_time->second = tick_sec%60u;
+    rtc_time->millisecond = RTC_DEFAULT_MILLISECOND;
     return GEN_HAL_SUCCESSFUL;
 }
