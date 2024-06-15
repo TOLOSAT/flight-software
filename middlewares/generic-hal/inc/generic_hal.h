ee585f200b5dd77aff47becab5edd6a84985d501
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-06-15 12:41:22 +0200

    WIP : add a lot of compatibility for QEMU and simplified the hello example ... still need to add some HAL files

--- middlewares/generic-hal/inc/generic_hal.h
+++ middlewares/generic-hal/inc/generic_hal.h
@@ -12,27 +12,26 @@
 
 /******************************* Include Files *******************************/
 
-#if defined(ART_PI)
-#include "art_pi_bsp.h"
-#elif defined(NUCLEO_H745ZI)
-#include "nucleo_h745zi_bsp.h"
-#elif defined(NUCLEO_F411RE)
-#include "nucleo_f411re_bsp.h"
-#elif defined(DISCOVERY_F407VG)
-#include "discovery_f407vg_bsp.h"
-#else
-#error "Board is not supported"
-#endif
-
+#if defined(QEMU)
 #include "generic_hal_types.h"
 #include "generic_hal_init.h"
 #include "generic_hal_gpio.h"
 #include "generic_hal_uart.h"
+#include "generic_hal_tim.h"
+#include "generic_hal_rtc.h"
+#include "generic_hal_ecc.h"
+#else
+#include "generic_hal_types.h"
+#include "generic_hal_init.h"
+#include "generic_hal_gpio.h"
+#include "generic_hal_uart.h"
+#include "generic_hal_tim.h"
 #include "generic_hal_i2c.h"
 #include "generic_hal_spi.h"
 #include "generic_hal_ow.h"
 #include "generic_hal_rtc.h"
 #include "generic_hal_ecc.h"
+#endif
 
 /***************************** Macros Definitions ****************************/
 
