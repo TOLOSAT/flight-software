d1c785efe2ab372e1e1b320c3278263135b57516
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-07-07 15:52:12 +0200

    First layer of correction, moved some files clean some comments and create PUS161 init function

--- application/src/miso/pus161.c
+++ application/src/miso/pus161.c
@@ -1,21 +1,19 @@
 /**
- * @file    pus17.c
- * @author  Merlin Kooshmanian
- * @brief   Source file for PUS 17 functions (Test)
- * @date    12/07/2023
+ * @file    pus161.c
+ * @author  Clement Cognard & Merlin Kooshmanian
+ * @brief   Source file for PUS 161 functions (MISO)
+ * @date    08/02/2024
  *
- * @copyright Copyright (c) TOLOSAT 2023
+ * @copyright Copyright (c) TOLOSAT 2024
  */
 
 /******************************* Include Files *******************************/
 
 #include <string.h>
 
-#include "services/pus161.h"
+#include "miso/pus161.h"
 #include "pus_tools/tm_management.h"
 #include "pus_tools/endianness_management.h"
-#include "miso/miso.h"
-
 
 /***************************** Macros Definitions ****************************/
 
@@ -23,13 +21,33 @@
 
 /*************************** Variables Definitions ***************************/
 
+static pus161Data_t *pus161_data_pointer;
+
 /*************************** Functions Definitions ***************************/
 
 /**
- * @fn          ExecuteS17SS1(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code)
- * @brief       Function that send S17SS2 TM (idle time report)
- * @param[in]   tc S17SS1 TC (this parameter is unused for these service and subservice)
- * @param[out]  tm S17SS2 TM that we will send
+ * @fn          InitS161(pus161Data_t *pus161_data)
+ * @brief       Function that initialises PUS 161 with shared data struct
+ * @param[in]   pus161_data shared data struct
+ * @retval      #PUS_SUCCESSFUL always
+ */
+pusStatus_t InitS161(pus161Data_t *pus161_data)
+{
+    // Variable Initialisation
+    pusStatus_t return_value = PUS_SUCCESSFUL;
+
+    // Function Core
+    pus161_data_pointer = pus161_data;
+    memset(pus161_data_pointer, 0u, sizeof(pus161Data_t));
+
+    return return_value;
+}
+
+/**
+ * @fn          ExecuteS161SS1(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code)
+ * @brief       Function that send S161SS2 TM (idle time report)
+ * @param[in]   tc S161SS1 TC that requests this TM
+ * @param[out]  tm S161SS2 TM that we will send
  * @retval      #PUS_INVALID_PARAM if a pointer is NULL
  */
 pusStatus_t ExecuteS161SS1(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code)
@@ -48,7 +66,7 @@ pusStatus_t ExecuteS161SS1(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_
         *error_code = PUS_EXECUTION_NO_ERROR;
 
         // Recuperation de l'idle processeur depuis miso
-        uint32_t idle = getIdleTime();
+        uint32_t idle = pus161_data_pointer->idle_time;
 
         // Set up data
         (void)memcpy((void *)&data, (void *)&idle, 4);
@@ -67,10 +85,10 @@ pusStatus_t ExecuteS161SS1(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_
 }
 
 /**
- * @fn          ExecuteS17SS3(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code)
- * @brief       Function that send S17SS4 TM (Stack usage report)
- * @param[in]   tc S17SS3 TC (this parameter is unused for these service and subservice)
- * @param[out]  tm S17SS4 TM that we will send
+ * @fn          ExecuteS161SS4(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code)
+ * @brief       Function that send S161SS4 TM (Stack usage report)
+ * @param[in]   tc S161SS3 TC that requests this TM
+ * @param[out]  tm S161SS4 TM that we will send
  * @retval      #PUS_INVALID_PARAM if a pointer is NULL
  */
 pusStatus_t ExecuteS161SS3(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code)
@@ -89,7 +107,7 @@ pusStatus_t ExecuteS161SS3(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_
         *error_code = PUS_EXECUTION_NO_ERROR;
 
         // Get stack usage
-        uint32_t stack = getStackUsage();
+        uint32_t stack = pus161_data_pointer->max_stack_usage;
 
         // Set up data
         (void)memcpy((void *)&data, (void *)&stack, 4);
@@ -107,10 +125,10 @@ pusStatus_t ExecuteS161SS3(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_
 }
 
 /**
- * @fn          ExecuteS17SS5(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code)
- * @brief       Function that send S17SS6 TM (Stack usage report)
- * @param[in]   tc S17SS5 TC (this parameter is unused for these service and subservice)
- * @param[out]  tm S17SS6 TM that we will send
+ * @fn          ExecuteS161SS5(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code)
+ * @brief       Function that send S161SS6 TM (Stack usage report)
+ * @param[in]   tc S161SS5 TC that requests this TM
+ * @param[out]  tm S161SS6 TM that we will send
  * @retval      #PUS_INVALID_PARAM if a pointer is NULL
  */
 pusStatus_t ExecuteS161SS5(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code)
@@ -129,10 +147,10 @@ pusStatus_t ExecuteS161SS5(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_
         *error_code = PUS_EXECUTION_NO_ERROR;
 
         // Get stack usage
-        char *state = getState();
+        // To do
 
         // Set up data
-        (void)memcpy((void *)&data, (void *)&state, 4);
+        // To do
         
         // Build TM 
         return_value = BuildTM(tm, 161u, 6u, (pusData_t *)&data, 4);
--- tests/test-tmtc/src/tc_receiver.c
+++ tests/test-tmtc/src/tc_receiver.c
@@ -44,7 +44,6 @@ pusRoutingTable_t g_tc_routing_table[NB_ROUTES] =
     {.key = BUILD_ROUTING_KEY(OBC_APID, 11u,   3u) , .route = TC_PUS11  },
     {.key = BUILD_ROUTING_KEY(OBC_APID, 11u,   4u) , .route = TC_PUS11  },
     {.key = BUILD_ROUTING_KEY(OBC_APID, 17u,   1u) , .route = TC_NORMAL },
-    {.key = BUILD_ROUTING_KEY(OBC_APID, 161u,  1u) , .route = TC_PUS161 }
 };
 
 /*************************** Functions Definitions ***************************/
