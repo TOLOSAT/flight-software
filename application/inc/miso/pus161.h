bcf14186a2825ffd7e6bfa23b0088b6197920ab8
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-07-20 20:36:59 +0200

    Added SS5/SS6 (just task number not present and no runtime)

--- application/inc/miso/pus161.h
+++ application/inc/miso/pus161.h
@@ -16,7 +16,20 @@
 
 /***************************** Macros Definitions ****************************/
 
+#define PUS161_MAX_TASK_NB      32u /**< Maximum number of tasks supported by PUS161 for a system report */
+
 /***************************** Types Definitions *****************************/
+
+/** 
+ * @struct  pus161TaskInfo_t
+ * @brief   Struct type definition of PUS161 data
+ */
+typedef struct 
+{
+    uint8_t stack_usage;    /** @brief Current stack usage in percent */
+    uint8_t time_usage;     /** @brief Current time usage in percent */
+    uint8_t task_mode;      /** @brief Current task mode */
+} pus161TaskInfo_t;
 
 /** 
  * @struct  pus161Data_t
@@ -24,9 +37,11 @@
  */
 typedef struct 
 {
-    uint8_t idle_time;              /** @brief Idle Time in percent */
-    uint8_t highest_stack_consumer; /** @brief Highest Stack Consumer */
-    uint8_t max_stack_usage;        /** @brief Max Stack Usage in percent */
+    uint8_t idle_time;                                  /** @brief Idle Time in percent */
+    uint8_t highest_stack_consumer;                     /** @brief Highest Stack Consumer */
+    uint8_t max_stack_usage;                            /** @brief Max Stack Usage in percent */
+    uint8_t number_of_tasks;                            /** @brief Actual number of tasks */
+    pus161TaskInfo_t system_report[PUS161_MAX_TASK_NB]; /** @brief System report for each task */
 } pus161Data_t;
 
 /*************************** Variables Declarations **************************/
@@ -39,5 +54,6 @@ pusStatus_t BuildS161SS2(pusTM_t *tm, uint8_t idle_time);
 pusStatus_t ExecuteS161SS3(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code);
 pusStatus_t BuildS161SS4(pusTM_t *tm, uint8_t highest_stack_consumer, uint8_t max_stack_usage);
 pusStatus_t ExecuteS161SS5(pusTC_t *tc, pusTM_t *tm, pusExecutionError_t *error_code);
+pusStatus_t BuildS161SS6(pusTM_t *tm, pus161Data_t *system_usage);
 
 #endif /* PUS161_H */
