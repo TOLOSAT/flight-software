c5f9890fb9488b970652b69b30ddd3c0172c487a
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-07-26 21:16:33 +0200

    Added settings protection

---
+++ gen/conf_boards/NUCLEO_F411RE.mk
@@ -0,0 +1,29 @@
+#################################
+######### NUCLEO_F411RE #########
+#################################
+
+# Chip Information
+CHIP_VENDOR = ST
+CHIP_FAMILLY = STM32F4xx
+CHIP = STM32F411xE
+MACH = cortex-m4
+CORE_SELECT =
+FPU_TYPE = -mfpu=fpv4-sp-d16 -mfloat-abi=hard
+FPU_AVAILABILITY = FPU_AVAILABLE
+CACHE_AVAILABILITY = CACHE_UNAVAILABLE
+MPU_AVAILABILITY = MPU_UNAVAILABLE
+ECC_AVAILABILITY = ECC_UNAVAILABLE
+FREERTOS_PORTABLE = ARM_CM4F
+
+# Valid Settings
+VALID_LOAD_MEMORY = FLASH
+VALID_CONSOLE_MODES = NONE UART FILE CIRCULAR-BUFFER
+VALID_FS_MODES = NONE SPI
+
+# Debugger Information
+OCD_DBG = interface/stlink.cfg
+OCD_CHIP = target/stm32f4x.cfg
+
+# HAL & BSP Information
+HAL_SRCS_LIST = $(CONF_HALS_DIR)/stm32f4xx_hal_conf.mk
+BSP_LD_SCRIPT = $(BSP_DIR)/stm32f411re_$(shell echo $(LOAD_MEMORY) | tr '[:upper:]' '[:lower:]').ldf
--- gen/path.mk
+++ gen/path.mk
@@ -6,16 +6,16 @@
 
 WORKSPACE 				= $(shell pwd)
 
-CORE_DIR        		= $(WORKSPACE)/core
+CORE_DIR				= $(WORKSPACE)/core
 
 BOOT_DIR				= $(WORKSPACE)/bootloader
 
-MIDDLEWARES_DIR 		= $(WORKSPACE)/middlewares
-LIBPUS_DIR      		= $(MIDDLEWARES_DIR)/libpus
-LIBTIME_DIR      		= $(MIDDLEWARES_DIR)/libtime
-LIBFS_DIR  				= $(MIDDLEWARES_DIR)/libfs
-IRIDIUM_DRV_DIR  	= $(MIDDLEWARES_DIR)/iridium-driver
-GENERIC_HAL_DIR    		= $(MIDDLEWARES_DIR)/generic-hal
+MIDDLEWARES_DIR			= $(WORKSPACE)/middlewares
+LIBPUS_DIR				= $(MIDDLEWARES_DIR)/libpus
+LIBTIME_DIR				= $(MIDDLEWARES_DIR)/libtime
+LIBFS_DIR				= $(MIDDLEWARES_DIR)/libfs
+IRIDIUM_DRV_DIR			= $(MIDDLEWARES_DIR)/iridium-driver
+GENERIC_HAL_DIR			= $(MIDDLEWARES_DIR)/generic-hal
 
 TOOLS_DIR 				= $(WORKSPACE)/tools
 CMSIS_DIR 				= $(TOOLS_DIR)/CMSIS
@@ -23,35 +23,37 @@ OS_DIR    				= $(TOOLS_DIR)/OS/FreeRTOS
 FATFS_DIR 				= $(TOOLS_DIR)/FS/FatFs
 HALs_DIR  				= $(TOOLS_DIR)/HALs
 BSPs_DIR  				= $(TOOLS_DIR)/BSPs
+BSP_DIR 				= $(BSPs_DIR)/$(BOARD)-BSP
 PRE_BUILD_SCRIPTS_DIR	= $(TOOLS_DIR)/pre-build
 
-GEN_DIR           		= $(WORKSPACE)/gen
-CONF_FREERTOS_DIR  		= $(GEN_DIR)/conf_FreeRTOS
-CONF_FATFS_DIR     		= $(GEN_DIR)/conf_FatFs
-CONF_HALS_DIR      		= $(GEN_DIR)/conf_HALs
-CONF_MISRA_DIR     		= $(GEN_DIR)/conf_misra
+GEN_DIR					= $(WORKSPACE)/gen
+CONF_BOARDS_DIR			= $(GEN_DIR)/conf_boards
+CONF_FATFS_DIR			= $(GEN_DIR)/conf_FatFs
+CONF_FREERTOS_DIR		= $(GEN_DIR)/conf_FreeRTOS
+CONF_HALS_DIR			= $(GEN_DIR)/conf_HALs
+CONF_MISRA_DIR			= $(GEN_DIR)/conf_misra
 
 TESTS_DIR 				= $(WORKSPACE)/tests
 
 ifeq ($(TEST),)
 ifeq ($(findstring test-,$(MAKECMDGOALS)),test-)
 TEST_NAME := $(subst test-,test-,$(filter test-%,$(MAKECMDGOALS)))
-APPLICATION_DIR 		= $(TESTS_DIR)/$(TEST_NAME)
+APPLICATION_DIR			= $(TESTS_DIR)/$(TEST_NAME)
 else
-APPLICATION_DIR 		= $(WORKSPACE)/application
+APPLICATION_DIR			= $(WORKSPACE)/application
 endif
 else
-APPLICATION_DIR 		= $(TESTS_DIR)/test-$(TEST)
+APPLICATION_DIR			= $(TESTS_DIR)/test-$(TEST)
 endif
 
-BUILD_DIR             	= $(WORKSPACE)/build
-PRE_BUILD_DIR		  	= $(BUILD_DIR)/pre-build
-BUILD_CORE_DIR        	= $(BUILD_DIR)/core
-BUILD_APPLICATION_DIR 	= $(BUILD_DIR)/application
-BUILD_MIDDLEWARES_DIR 	= $(BUILD_DIR)/middlewares
-BUILD_TOOLS_DIR       	= $(BUILD_DIR)/tools
-BUILD_LIBS_DIR        	= $(BUILD_DIR)/libs
-TARGET_DIR            	= $(BUILD_DIR)/target
+BUILD_DIR				= $(WORKSPACE)/build
+PRE_BUILD_DIR			= $(BUILD_DIR)/pre-build
+BUILD_CORE_DIR			= $(BUILD_DIR)/core
+BUILD_APPLICATION_DIR	= $(BUILD_DIR)/application
+BUILD_MIDDLEWARES_DIR	= $(BUILD_DIR)/middlewares
+BUILD_TOOLS_DIR			= $(BUILD_DIR)/tools
+BUILD_LIBS_DIR			= $(BUILD_DIR)/libs
+TARGET_DIR				= $(BUILD_DIR)/target
 
 LD_SCRIPT				= $(BUILD_DIR)/$(PROJ_NAME).ld
 
@@ -68,21 +70,21 @@ CORE_SRCDIR = $(CORE_DIR)/src
 ##############################################
 
 # Application Directories
-APPLICATION_CONF_DIR = $(APPLICATION_DIR)/conf
-APPLICATION_INCDIR   = $(APPLICATION_DIR)/inc
-APPLICATION_SRCDIR   = $(APPLICATION_DIR)/src
+APPLICATION_CONF_DIR	= $(APPLICATION_DIR)/conf
+APPLICATION_INCDIR		= $(APPLICATION_DIR)/inc
+APPLICATION_SRCDIR		= $(APPLICATION_DIR)/src
 
 ##############################################
 ################ OS DIRECTORIES ##############
 ##############################################²
 
 # FreeRTOS Kernel Directories
-OS_KERNEL_INCDIR      = $(OS_DIR)/include
-OS_KERNEL_SRCDIR      = $(OS_DIR)
-OS_KERNEL_COMMON_DIR  = $(OS_DIR)/portable/Common
-OS_KERNEL_MEMMANG_DIR = $(OS_DIR)/portable/MemMang
-OS_KERNEL_ARM_DIR     = $(OS_DIR)/portable/GCC/$(FREERTOS_PORTABLE)
-OS_KERNEL_OBJDIR      = $(BUILD_DIR)/tools/os
+OS_KERNEL_INCDIR		= $(OS_DIR)/include
+OS_KERNEL_SRCDIR		= $(OS_DIR)
+OS_KERNEL_COMMON_DIR	= $(OS_DIR)/portable/Common
+OS_KERNEL_MEMMANG_DIR	= $(OS_DIR)/portable/MemMang
+OS_KERNEL_ARM_DIR		= $(OS_DIR)/portable/GCC/$(FREERTOS_PORTABLE)
+OS_KERNEL_OBJDIR		= $(BUILD_DIR)/tools/os
 
 ##############################################
 ########### MIDDLEWARES DIRECTORIES ###########
@@ -131,16 +133,16 @@ FATFS_OBJDIR = $(BUILD_DIR)/tools/fatfs
 ##############################################
 
 # CMSIS Directories
+CMSIS_INCDIR 			= $(CMSIS_DIR)/CMSIS-ARM/CMSIS/Core/Include
 ifeq ($(CHIP_FAMILLY), STM32F4xx)
-CMSIS_INCDIR_DEVICE = $(CMSIS_DIR)/cmsis_device_stm32f4/Include
+CMSIS_INCDIR_DEVICE 	= $(CMSIS_DIR)/cmsis_device_stm32f4/Include
 else ifeq ($(CHIP_FAMILLY), STM32H7xx)
-CMSIS_INCDIR_DEVICE = $(CMSIS_DIR)/cmsis_device_stm32h7/Include
+CMSIS_INCDIR_DEVICE 	= $(CMSIS_DIR)/cmsis_device_stm32h7/Include
 else ifeq ($(CHIP_FAMILLY), CMSDK)
-CMSIS_INCDIR_DEVICE = $(CMSIS_DIR)/cmsis_device_cmsdk_cm7/Include
+CMSIS_INCDIR_DEVICE 	= $(CMSIS_DIR)/cmsis_device_cmsdk_cm7/Include
 else
 $(error There is no compatible CMSIS)
 endif
-CMSIS_INCDIR       = $(CMSIS_DIR)/CMSIS-ARM/CMSIS/Core/Include
 
 ##############################################
 ############### HAL DIRECTORIES ##############
