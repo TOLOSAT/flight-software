525fee1f483ca8d9452ecdd7208f71d4332b45d6
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-08-14 21:34:31 +0200

    Added IOCTL for file system core API

--- tools/pre-build/fs-parser.py
+++ tools/pre-build/fs-parser.py
@@ -3,12 +3,12 @@ import argparse
 import os
 from datetime import datetime
 
-# Configuration de l'analyseur d'arguments
+# Argument parser configuration
 parser = argparse.ArgumentParser(description='Generates fs_conf.c and fs_conf.h files from a CSV file.')
 parser.add_argument('-i', '--input', type=str, help='Path to input CSV file.')
 parser.add_argument('-o', '--output', type=str, help='Destination folder for generated files.')
 
-# Analyse des arguments
+# Parse the arguments
 args = parser.parse_args()
 
 if not args.input or not args.output:
@@ -18,18 +18,18 @@ if not args.input or not args.output:
 csv_file_name = args.input
 output_directory = args.output
 
-# Vérifie si le dossier de sortie existe, sinon le crée
+# Check if the output directory exists, if not, create it
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
 c_file_name = os.path.join(output_directory, 'fs_conf.c')
 h_file_name = os.path.join(output_directory, 'fs_conf.h')
 
-# Obtention de la date actuelle pour l'en-tête
+# Get the current date for the header
 current_date = datetime.now().strftime("%d/%m/%Y")
 
 try:
-    file_refs, file_names, file_access_modes = [], [], []
+    file_refs, file_names, file_access_modes, auto_sync_modes = [], [], [], []
 
     with open(csv_file_name, mode='r', newline='') as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -37,11 +37,12 @@ try:
             file_refs.append(row["File Ref"])
             file_names.append(row["File Name"].replace('"', '').strip())
             file_access_modes.append(row["File Access Mode"].strip())
+            auto_sync_modes.append(row["Auto Synchronisation"].strip())
 
     with open(c_file_name, 'w') as c_file:
         c_file.write(f"""/**
  * @file    fs_conf.c
- * @brief   Source file stocking configuration for file system content
+ * @brief   Source file storing configuration for file system content
  * @author  Auto-generated
  * @date    {current_date}
  *
@@ -62,18 +63,16 @@ try:
 
 /**
  * @var     g_file_desc_table
- * @brief   Configuration table where all files descriptors are stored
+ * @brief   Configuration table where all file descriptors are stored
  */
-const fsFileDesc_t IN_DESC_TABLES_SECTION g_file_desc_table[NB_MEMORY_DEVICES][MAX_NB_FILES_PER_DEVICES] = 
+fsFileDesc_t IN_DESC_TABLES_SECTION g_file_desc_table[NB_FILES] = 
 {{
-    /* Fileno , File Name , File Access Mode , Temp File */
-    {{\n""")
-        for ref, name, mode in zip(file_refs, file_names, file_access_modes):
-            temp_file_var = f"{ref.lower()}_temp_file"
-            c_file.write(f"    {{ {ref} , \"{name}\" , {mode} , &{temp_file_var} }},\n")
-        c_file.write("""    }
-};
+    /* Fileno , File Name , File Access Mode , Temp File , Auto Sync */
 """)
+        for ref, name, mode, auto_sync in zip(file_refs, file_names, file_access_modes, auto_sync_modes):
+            temp_file_var = f"{ref.lower()}_temp_file"
+            c_file.write(f"    {{ {ref} , \"{name}\" , {mode} , {auto_sync} , &{temp_file_var} }},\n")
+        c_file.write("};\n")
         for ref in file_refs:
             temp_file_var = f"{ref.lower()}_temp_file"
             c_file.write(f"""
@@ -87,7 +86,7 @@ FIL IN_TMPFS_SECTION {temp_file_var} = {{0}};
     with open(h_file_name, 'w') as h_file:
         h_file.write(f"""/**
  * @file    fs_conf.h
- * @brief   Header file stocking configuration for file system content
+ * @brief   Header file storing configuration for file system content
  * @author  Auto-generated
  * @date    {current_date}
  * 
@@ -103,39 +102,27 @@ FIL IN_TMPFS_SECTION {temp_file_var} = {{0}};
 
 /***************************** Macros Definitions ****************************/
 
-#define MAX_NB_FILES_PER_DEVICES    (NB_SD0_FILES)  /**< Maximum Number of files */
-
 /***************************** Types Definitions *****************************/
 
 /**
- * @enum    MEMORY_DEVICES_ENUM
- * @brief   Enum defining memory devices reference numbers
+ * @enum    FILE_ENUM
+ * @brief   Enum defining file reference numbers
  */
-enum MEMORY_DEVICES_ENUM 
-{{
-    SD0 = 0u,
-    NB_MEMORY_DEVICES,
-}};
-
-/**
- * @enum    FILE_SD0_ENUM
- * @brief   Enum defining file in SD0 reference numbers
- */
-enum FILE_SD0_ENUM 
+enum FILE_ENUM 
 {{\n""")
         for ref in file_refs:
             h_file.write(f"    {ref},\n")
-        h_file.write("""    NB_SD0_FILES,
+        h_file.write("""    NB_FILES,
 };
 
 /*************************** Variables Declarations **************************/
 
-extern const fsFileDesc_t g_file_desc_table[NB_MEMORY_DEVICES][MAX_NB_FILES_PER_DEVICES];""")
+extern fsFileDesc_t g_file_desc_table[NB_FILES];""")
         for ref in file_refs:
             temp_file_var = f"{ref.lower()}_temp_file"
             h_file.write(f"\nextern FIL {temp_file_var};")
         h_file.write("\n\n#endif /* FS_CONF_H */\n")
 
-    print(f"Files '{c_file_name}' and '{h_file_name}' have been generated with success.")
+    print(f"Files '{c_file_name}' and '{h_file_name}' have been generated successfully.")
 except Exception as e:
-    print(f"Error when generating : {e}")
+    print(f"Error when generating: {e}")
