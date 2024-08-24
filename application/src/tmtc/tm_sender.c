f71424645dadc4b05b20be9de3bbb2b53eebfcc2
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-08-24 18:05:30 +0200

    Added mutexes to peripherals

--- application/src/tmtc/tm_sender.c
+++ application/src/tmtc/tm_sender.c
@@ -51,7 +51,7 @@ void IN_TMTC_TEXT_SECTION TmSenderMain(void *task_desc)
     bufferDepth_t buffer_count = 0;
 
     // Initialisation
-    task_status = UartIoctl(&uart_tmtc_inst, UART_IOCTL_START_TX, &send_tm, TM_MAX_SIZE);
+    task_status = UartIoctl(&g_uart_tmtc_inst, UART_IOCTL_START_TX, &send_tm, TM_MAX_SIZE);
     CheckErrors(task_status, FDIR_ERROR_HANDLER);
     task_status = InitPeriodicWait(task_desc);
     CheckErrors(task_status, FDIR_ERROR_HANDLER);
@@ -75,12 +75,12 @@ void IN_TMTC_TEXT_SECTION TmSenderMain(void *task_desc)
                     CheckErrors(task_status, FDIR_ERROR_HANDLER);
 
                     // Yield until DMA ended transaction
-                    halStatus_t test_hal = UartIoctl(&uart_tmtc_inst, UART_IOCTL_CHECK_TX_ENDED, NULL, 0u);
+                    halStatus_t test_hal = UartIoctl(&g_uart_tmtc_inst, UART_IOCTL_CHECK_TX_ENDED, NULL, 0u);
                     while (test_hal == GEN_HAL_BUSY)
                     {
                         task_status = TaskYield(task_desc);
                         CheckErrors(task_status, FDIR_ERROR_HANDLER);
-                        test_hal = UartIoctl(&uart_tmtc_inst, UART_IOCTL_CHECK_TX_ENDED, NULL, 0);
+                        test_hal = UartIoctl(&g_uart_tmtc_inst, UART_IOCTL_CHECK_TX_ENDED, NULL, 0);
                     }
                 }
             }
@@ -111,7 +111,7 @@ static pusStatus_t IN_TMTC_TEXT_SECTION SendTM(pusTM_t *tm)
         uartMsg_t tm_size = tm->spp_header.packet_data_length + SPP_HEADER_SIZE + 1u;
         (void)FormatTM(tm);
 
-        halStatus_t test_hal = UartWrite(&uart_tmtc_inst, (uartMsg_t *)tm, tm_size);
+        halStatus_t test_hal = UartWrite(&g_uart_tmtc_inst, (uartMsg_t *)tm, tm_size);
         if(test_hal != GEN_HAL_SUCCESSFUL)
         {
             return_value = PUS_ERROR;
