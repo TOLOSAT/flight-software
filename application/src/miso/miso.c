61260fb3308a668c246e73fdd3e20facc2b2cf83
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-09-01 18:38:10 +0200

    GetSystemUsage integrated in the core

--- application/src/miso/miso.c
+++ application/src/miso/miso.c
@@ -17,12 +17,9 @@
 /***************************** Macros Definitions ****************************/
 
 #define NB_PUS161_EXECUTION     3u                       /**< Number of pus161 exution functions */
-#define REAL_NB_TASKS           ((uint32_t)NB_TASKS+2u) /**< Real number of tasks (because FreeRTOS adds IdleTask and TimerSVC task) */
 #define MAX_STACK_USAGE         80u                     /**< Maximum stack usage authorized in percent */
 
 /*************************** Functions Declarations **************************/
-
-static appStatus_t GetSystemUsage(pus161Data_t *system_usage);
 
 /*************************** Variables Definitions ***************************/
 
@@ -43,7 +40,7 @@ void IN_MISO_TEXT_SECTION MisoMain(void *task_desc)
         {BUILD_ROUTING_KEY(OBC_APID, 161u, 3u), ExecuteS161SS3, TM_REQUESTED},
         {BUILD_ROUTING_KEY(OBC_APID, 161u, 5u), ExecuteS161SS5, TM_REQUESTED},
     };
-    pus161Data_t *system_usage = NULL;
+    monitoringSystemUsage_t *system_usage = NULL;
 
     // Initialisation
     task_status = InitPeriodicWait(task_desc);
@@ -79,72 +76,3 @@ void IN_MISO_TEXT_SECTION MisoMain(void *task_desc)
         CheckErrors(task_status, FDIR_ERROR_HANDLER);
     }
 }
-
-/**
- * @fn          GetSystemUsage(void)
- * @brief       Retrieves the system usage.
- * @param[in]   system_usage System Usage as defined in PUS161
- * @retval      #APP_SUCCESSFUL always
- *
- * This function will retrieves :
- * - Highest stack consumer
- * - Max stack usage
- * - Idle time
- * And for every task
- * - Stack usage (in percent)
- * - Time usage (in percent)
- * - Task mode (from dynamic task table)
- */
-static appStatus_t IN_MISO_TEXT_SECTION GetSystemUsage(pus161Data_t *system_usage)
-{
-    // Variable Initialisation
-    appStatus_t return_value = APP_SUCCESSFUL;
-    TaskStatus_t task_status_array[REAL_NB_TASKS] = {0};
-    uint8_t highest_stack_consumer_temp = 0u;
-    uint8_t max_stack_usage_temp = 0u;
-    uint32_t total_run_time = 0u;
-
-    // First get idle time
-    system_usage->idle_time = (uint8_t)ulTaskGetIdleRunTimePercent();
-
-    // Take a snapshot of all task states.
-    UBaseType_t status_array_size = uxTaskGetSystemState(task_status_array, REAL_NB_TASKS, &total_run_time);
-
-    // Retrieve the task with the highest stack usage.
-    for (uint32_t i = 0u; i < status_array_size; i++)
-    {
-        // Get task number
-        // Note : FreeRTOS numbers tasks starting from 1.
-        uint32_t task = task_status_array[i].xTaskNumber - 1u;
-
-        // Considere only TAPAS tasks (not FreeRTOS internal ones)
-        if (task < (uint32_t)NB_TASKS)
-        {
-            // Get task data
-            uint8_t current_stack_usage = ((g_tasks_conf[task].stack_size -
-                                           (task_status_array[i].usStackHighWaterMark * sizeof(StackType_t))) * 100u) /
-                                           g_tasks_conf[task].stack_size;
-
-            uint8_t current_time_usage = (task_status_array[i].ulRunTimeCounter * 100u) / total_run_time;
-
-            // Update task status in system usage
-            system_usage->system_report[task].task_mode = g_tasks_desc_table[task].mode;
-            system_usage->system_report[task].stack_usage = current_stack_usage;
-            system_usage->system_report[task].time_usage = current_time_usage;
-
-            // Update max usage data if needed
-            if (current_stack_usage > max_stack_usage_temp)
-            {
-                max_stack_usage_temp = current_stack_usage;
-                highest_stack_consumer_temp = (uint8_t)task;
-            }
-        }
-    }
-
-    // Update max usage data in the system usage
-    system_usage->highest_stack_consumer = highest_stack_consumer_temp;
-    system_usage->max_stack_usage = max_stack_usage_temp;
-
-    return return_value;
-}
-
