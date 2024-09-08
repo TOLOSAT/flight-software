9352521a0dac74512bdf1eaed8cdee7632cb1fef
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-09-08 10:39:11 +0200

    Fancier build

--- gen/build.mk
+++ gen/build.mk
@@ -28,7 +28,7 @@ endif
 ######## SOFTWARE BUILD CONFIGURATION ########
 ##############################################
 
-PRIVATE_COMPONENTS = core bsp
+PRIVATE_COMPONENTS = bsp core
 PUBLIC_COMPONENTS = hal fatfs
 
 PRIVATE_LIBS = $(foreach lib,$(PRIVATE_COMPONENTS),-l$(lib)-$(BUILD_TYPE))
@@ -40,18 +40,42 @@ PUBLIC_LIBS = $(foreach lib,$(PUBLIC_COMPONENTS),-l$(lib)-$(BUILD_TYPE))
 
 .PHONY += build
 
-build : $(TARGET)
+build : pre-build-info $(TARGET) post-build-info
+
+# Display general build info before linking
+pre-build-info :
+	@echo "=============================="
+	@echo "===    TAPAS BUILD INFO    ==="
+	@echo "=============================="
+	@echo "Software Version: $(VERSION)"
+	@echo "Project Name: $(PROJ_NAME)"
+	@echo "Compiler: $$( $(CC) --version | head -n 1 )"
+	@echo "Build Type: $(BUILD_TYPE)"
+	@echo "Board: $(BOARD)"
+	@echo "File System Mode: $(FS_MODE)"
+	@echo ""
 
 # Target Linking Stage
-$(TARGET) : $(PRIVATE_COMPONENTS) $(PUBLIC_COMPONENTS)
-	@echo "**************************************"
-	@echo "*******   TARGET Start Build   *******"
-	@echo "**************************************"
+$(TARGET) : pre-build $(PRIVATE_COMPONENTS) $(PUBLIC_COMPONENTS)
+	@echo "=============================="
+	@echo "===         LINKING        ==="
+	@echo "=============================="
 	@echo "  LD  $(@F)"
 	@mkdir -p $(@D)
+	@$(eval link_start_time=$(shell date +%s))
 	@$(CC) -L$(BUILD_LIBS_DIR) -Wl,--whole-archive $(PRIVATE_LIBS) -Wl,--no-whole-archive $(PUBLIC_LIBS) $(PROJECT_LDFLAGS) -T $(LD_SCRIPT) -o $@ > $(@:.elf=.size)
+	@$(eval link_end_time=$(shell date +%s))
 	@$(READELF) -a $@ > $(@:.elf=.readelf)
-	@echo "**************************************"
-	@echo "********   TARGET Build Done   *******"
-	@echo "**************************************"
-	@echo
+	@echo "Linking Done ($$(($(link_end_time)-$(link_start_time))) seconds elapsed)"
+	@echo ""
+
+# Display post-build information and statistics
+post-build-info :
+	@echo "=============================="
+	@echo "===    BUILD STATISTICS    ==="
+	@echo "=============================="
+	@echo "Executable: $(TARGET)"
+	@echo "Memory Usage :"
+	@cat $(TARGET:.elf=.size)
+	@echo "Build completed successfully."
+	@echo ""
--- gen/build_third_parties.mk
+++ gen/build_third_parties.mk
@@ -4,84 +4,104 @@
 #################### HAL #####################
 ##############################################
 
-# HAL Flags
+# HAL flags
 HAL_CFLAGS    = $(PROJECT_CFLAGS) -Wno-unused-variable -Wno-unused-parameter
 HAL_INCFLAGS  = -I$(HAL_INCDIR) -I$(HAL_INCDIR)/Legacy -I$(CONF_HALS_DIR)
 HAL_INCFLAGS += -I$(CMSIS_INCDIR) -I$(CMSIS_INCDIR_DEVICE)
 
-# HAL Files
+# HAL files
 include $(HAL_SRCS_LIST)
 HAL_OBJS  = $(subst $(HAL_SRCDIR)/,$(HAL_OBJDIR)/,$(HAL_SRCS:.c=-$(BUILD_TYPE).o))
 HAL_LIB   = $(BUILD_LIBS_DIR)/libhal-$(BUILD_TYPE).a
 
-# Include dependancies
+# Include dependencies
 -include $(HAL_OBJS:.o=.d)
 
-# HAL compilation
+# HAL recipes
+hal : hal-start $(HAL_LIB) hal-end
+
+# Build header
+hal-start :
+	@echo "============================="
+	@echo "===          HAL          ==="
+	@echo "============================="
+	@echo "Files to compile: $(words $(HAL_SRCS))"
+	@echo "Compilation Flags:"
+	@echo $(HAL_CFLAGS)
+	@echo "Include Paths:"
+	@echo $(HAL_INCFLAGS)
+	@echo "Version Flags:"
+	@echo $(VERSION_FLAGS)
+	@echo "Start building:"
+	@$(eval start_time=$(shell date +%s))
+
+# Building recipes
 $(HAL_OBJDIR)/%-$(BUILD_TYPE).o : $(HAL_SRCDIR)/%.c
 	@echo "  CC  $(@F)"
 	@mkdir -p $(@D)
 	@$(CC) $(HAL_CFLAGS) $(HAL_INCFLAGS) $(VERSION_FLAGS) $< -o $@ 
 
-# HAL Library
+# Library generation
 $(HAL_LIB) : $(HAL_OBJS)
 	@echo "  AR  $(@F)"
 	@mkdir -p $(@D)
 	@$(AR) rcs $@ $^
 
-# HAL Recipe
-hal-start :
-	@echo "**************************************"
-	@echo "*********   HAL Start Build   ********"
-	@echo "**************************************"
-
+# Build footer
 hal-end :
-	@echo "**************************************"
-	@echo "*********   HAL Build Done   *********"
-	@echo "**************************************"
-	@echo
-
-hal : hal-start $(HAL_LIB) hal-end
+	@$(eval end_time=$(shell date +%s))
+	@echo "Build done ($$(($(end_time)-$(start_time))) seconds elapsed)"
+	@echo ""
 
 ##############################################
 ################ FATS LIBRARY ################
 ##############################################
 
-# FATFS Flags
+# FATFS flags
 FATFS_CFLAGS    = $(PROJECT_CFLAGS) -Wno-unused-variable -Wno-unused-parameter -Wno-stringop-overflow -Wno-unused-function
 FATFS_INCFLAGS  = -I$(FATFS_INCDIR) -I$(CONF_FATFS_DIR)
 FATFS_INCFLAGS += -I$(CMSIS_INCDIR) -I$(CMSIS_INCDIR_DEVICE)
 
-# FATFS Files
+# FATFS files
 FATFS_SRCS  = $(wildcard $(FATFS_SRCDIR)/*.c)
 FATFS_OBJS  = $(subst $(FATFS_SRCDIR)/,$(FATFS_OBJDIR)/,$(FATFS_SRCS:.c=-$(BUILD_TYPE).o))
 FATFS_LIB   = $(BUILD_LIBS_DIR)/libfatfs-$(BUILD_TYPE).a
 
-# Include dependancies
+# Include dependencies
 -include $(FATFS_OBJS:.o=.d)
 
-# FATFS compilation
+# FATFS recipes
+fatfs : fatfs-start $(FATFS_LIB) fatfs-end
+
+# Build header
+fatfs-start :
+	@echo "============================="
+	@echo "===         FATFS         ==="
+	@echo "============================="
+	@echo "Files to compile: $(words $(FATFS_SRCS))"
+	@echo "Compilation Flags:"
+	@echo $(FATFS_CFLAGS)
+	@echo "Include Paths:"
+	@echo $(FATFS_INCFLAGS)
+	@echo "Version Flags:"
+	@echo $(VERSION_FLAGS)
+	@echo "Start building:"
+	@$(eval start_time=$(shell date +%s))
+
+# Building recipes
 $(FATFS_OBJDIR)/%-$(BUILD_TYPE).o : $(FATFS_SRCDIR)/%.c
 	@echo "  CC  $(@F)"
 	@mkdir -p $(@D)
 	@$(CC) $(FATFS_CFLAGS) $(FATFS_INCFLAGS) $(VERSION_FLAGS) $< -o $@ 
 
-# FATFS Library
+# Library generation
 $(FATFS_LIB) : $(FATFS_OBJS)
 	@echo "  AR  $(@F)"
 	@mkdir -p $(@D)
 	@$(AR) rcs $@ $^
 
-# FATFS Recipe
-fatfs-start :
-	@echo "**************************************"
-	@echo "********   FATFS Start Build   *******"
-	@echo "**************************************"
-
+# Build footer
 fatfs-end :
-	@echo "**************************************"
-	@echo "********   FATFS Build Done   ********"
-	@echo "**************************************"
-	@echo
-
-fatfs : fatfs-start $(FATFS_LIB) fatfs-end
+	@$(eval end_time=$(shell date +%s))
+	@echo "Build done ($$(($(end_time)-$(start_time))) seconds elapsed)"
+	@echo ""
