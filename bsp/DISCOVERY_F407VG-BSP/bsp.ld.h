ae7de5bf79964e2c88882585821094fc1e45f804
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-10-01 12:52:22 +0200

    Removed MPU because it will be required to code the MPU handling differently

--- bsp/DISCOVERY_F407VG-BSP/bsp.ld.h
+++ bsp/DISCOVERY_F407VG-BSP/bsp.ld.h
@@ -1,24 +1,24 @@
-/* Put section macro */
-#ifndef PUT_SECTION
-#define PUT_SECTION(section)    . = ALIGN(4); \
-                                _##section##_start_ = .; \
-                                *(.section) \
-                                . = ALIGN(4); \
-                                _##section##_end_ = .;
-#endif
+/* BSP contained sections */
+#define BSP_TEXT_SEGMENT    . = ALIGN(4); \
+                            _text_bsp_start_ = .; \
+                            *libbsp*.a:*(.text .text.*) \
+                            . = ALIGN(4); \
+                            _text_bsp_end_ = .;
 
-/* PUS contained sections */
-#define BSP_TEXT_SEGMENT        PUT_SECTION(text_bsp)
-#define BSP_DATA_SEGMENT        PUT_SECTION(data_bsp)
+#define BSP_DATA_SEGMENT    . = ALIGN(4); \
+                            _data_bsp_start_ = .; \
+                            *libbsp*.a:*(.data .data.* .bss .bss.*) \
+                            . = ALIGN(4); \
+                            _data_bsp_end_ = .;
 
-#define HAL_TEXT_SEGMENT        . = ALIGN(4); \
-                                _text_hal_start_ = .; \
-                                *libhal*.a:*(.text .text.*) \
-                                . = ALIGN(4); \
-                                _text_hal_end_ = .;
+#define HAL_TEXT_SEGMENT    . = ALIGN(4); \
+                            _text_hal_start_ = .; \
+                            *libhal*.a:*(.text .text.*) \
+                            . = ALIGN(4); \
+                            _text_hal_end_ = .;
 
-#define HAL_DATA_SEGMENT        . = ALIGN(4); \
-                                _data_hal_start_ = .; \
-                                *libhal*.a:*(.data .data.* .bss .bss.*) \
-                                . = ALIGN(4); \
-                                _data_hal_end_ = .;
+#define HAL_DATA_SEGMENT    . = ALIGN(4); \
+                            _data_hal_start_ = .; \
+                            *libhal*.a:*(.data .data.* .bss .bss.*) \
+                            . = ALIGN(4); \
+                            _data_hal_end_ = .;
