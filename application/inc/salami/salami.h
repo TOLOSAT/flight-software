1687b057b7df14e4e44981d1b9c3c397152049bd
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-10-09 14:11:16 +0200

    Correct few things in PUS11 to make it work and removed date in doxygen comments

--- application/inc/salami/salami.h
+++ application/inc/salami/salami.h
@@ -2,7 +2,6 @@
  * @file    salami.h
  * @author  Merlin Kooshmanian
  * @brief   Header file for SALAMI Task
- * @date    28/06/2023
  * 
  * @copyright Copyright (c) TOLOSAT 2024
  */
--- application/src/gravimetry/gravimetry.c
+++ application/src/gravimetry/gravimetry.c
@@ -2,7 +2,6 @@
  * @file    gravimetry.c
  * @author  Merlin Kooshmanian
  * @brief   Source file for GRAVIMETRY Task
- * @date    28/06/2023
  * 
  * @copyright Copyright (c) TOLOSAT 2024
  */
