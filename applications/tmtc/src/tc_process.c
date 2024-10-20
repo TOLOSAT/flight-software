7eb639e37fc49143f4d888313bc6fd2bfb8f524e
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-10-20 12:34:52 +0200

    Added a specific folder for each application

---
+++ applications/tmtc/src/tc_process.c
@@ -0,0 +1,57 @@
+/**
+ * @file    tc_process.c
+ * @author  Merlin Kooshmanian
+ * @brief   Source file for TC_PROCESS Task
+ *
+ * @copyright Copyright (c) TOLOSAT 2024
+ */
+
+/******************************* Include Files *******************************/
+
+#include "tc_process.h"
+#include "kernel.h"
+#include "pus.h"
+
+/***************************** Macros Definitions ****************************/
+
+#define NB_NORMAL_EXECUTION    4u  /**< Number of exution functions */
+
+/*************************** Functions Declarations **************************/
+
+/*************************** Variables Definitions ***************************/
+
+/*************************** Functions Definitions ***************************/
+
+/**
+ * @fn              TcProcessMain(void)
+ * @brief           Main of the TC_PROCESS Task
+ */
+void TcProcessMain(void)
+{
+    // Initialisation
+    static pusExecutionTable_t normal_exec_tab[NB_NORMAL_EXECUTION] =
+    {
+        {BUILD_ROUTING_KEY(OBC_APID, 6u, 1u)   , ExecuteS6SS1   , TM_NOT_REQUESTED },
+        {BUILD_ROUTING_KEY(OBC_APID, 6u, 3u)   , ExecuteS6SS3   , TM_REQUESTED     },
+        {BUILD_ROUTING_KEY(OBC_APID, 9u, 128u) , ExecuteS9SS128 , TM_NOT_REQUESTED },
+        {BUILD_ROUTING_KEY(OBC_APID, 17u, 1u)  , ExecuteS17SS1  , TM_REQUESTED     },
+    };
+    static pusExecutionContext_t normal_tc_context =
+    {
+        .execution_table = normal_exec_tab,
+        .execution_table_size = NB_NORMAL_EXECUTION,
+        .buffer_tc = TC_NORMAL,
+        .buffer_tm = TM_NORMAL,
+        .buffer_ack = TM_PUS1,
+    };
+    CheckError(InitTCExecutionContext(&normal_tc_context));
+
+    // Function Core
+    while (1)
+    {
+        // Execute incoming TC
+        CheckError(ExecuteTC(&normal_tc_context));
+
+        SleepPeriodic();
+    }
+}
