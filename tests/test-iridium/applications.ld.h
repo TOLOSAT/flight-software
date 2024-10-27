6fe3fc6a592ecd94ab613f079daa61f0ac2ff059
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-10-27 20:46:35 +0100

    Standardized the naming convention for ld symbols

--- tests/test-iridium/applications.ld.h
+++ tests/test-iridium/applications.ld.h
@@ -1,24 +1,24 @@
-/* Application contained sections */
+/* Application sections */
 #define APP_TEXT_SEGMENT    . = ALIGN(4); \
-                            _application_text_start_ = .; \
+                            __application_text_start__ = .; \
                             *libapplication*.a:*(.text .text.*) \
                             . = ALIGN(4); \
-                            _application_text_end_ = .;
+                            __application_text_end__ = .;
 
 #define APP_RODATA_SEGMENT  . = ALIGN(4); \
-                            _application_rodata_start_ = .; \
+                            __application_rodata_start__ = .; \
                             *libapplication*.a:*(.rodata .rodata.*) \
                             . = ALIGN(4); \
-                            _application_rodata_end_ = .;
+                            __application_rodata_end__ = .;
 
 #define APP_DATA_SEGMENT    . = ALIGN(4); \
-                            _application_data_start_ = .; \
+                            __application_data_start__ = .; \
                             *libapplication*.a:*(.data .data.*) \
                             . = ALIGN(4); \
-                            _application_data_end_ = .;
+                            __application_data_end__ = .;
 
 #define APP_BSS_SEGMENT     . = ALIGN(4); \
-                            _application_bss_start_ = .; \
+                            __application_bss_start__ = .; \
                             *libapplication*.a:*(.bss .bss.*) \
                             . = ALIGN(4); \
-                            _application_bss_end_ = .;
+                            __application_bss_end__ = .;
