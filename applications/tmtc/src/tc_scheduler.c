c355b0e0c112f474057b540fab83012996cfea83
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-02-06 09:20:41 +0100

    Added next tc released date information in the ReleaseDelayedTC() function outputs

--- applications/tmtc/src/tc_scheduler.c
+++ applications/tmtc/src/tc_scheduler.c
@@ -29,6 +29,7 @@
 void TcSchedulerMain(void)
 {
     // Initialisation
+    time_t next_tc_release_date = 0u;
     static pusExecutionTable_t sched_exec_tab[NB_PUS11_EXECUTION] =
     {
         { BUILD_ROUTING_KEY(OBC_APID, 11u, 1u) , ExecuteS11SS1 , TM_NOT_REQUESTED },
@@ -62,7 +63,7 @@ void TcSchedulerMain(void)
         CheckError(ExecuteTC(&sched_tc_context));
 
         // Process delayed TC
-        CheckError(ReleaseDelayedTC(&pus11_context));
+        CheckError(ReleaseDelayedTC(&pus11_context, &next_tc_release_date));
 
         SleepPeriodic();
     }
