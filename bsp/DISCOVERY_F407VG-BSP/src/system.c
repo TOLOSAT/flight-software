74bdd2f6974d3017cab8452b9f420338b8dbbcea
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-02-09 20:00:36 +0100

    Clang format respects what I was expecting

--- bsp/DISCOVERY_F407VG-BSP/src/system.c
+++ bsp/DISCOVERY_F407VG-BSP/src/system.c
@@ -228,13 +228,13 @@ void SystemCoreClockUpdate(void)
 
     switch (tmp)
     {
-        case 0x00: /* HSI used as system clock source */
+        case 0x00 : /* HSI used as system clock source */
             SystemCoreClock = HSI_VALUE;
             break;
-        case 0x04: /* HSE used as system clock source */
+        case 0x04 : /* HSE used as system clock source */
             SystemCoreClock = HSE_VALUE;
             break;
-        case 0x08: /* PLL used as system clock source */
+        case 0x08 : /* PLL used as system clock source */
 
             /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N
                SYSCLK = PLL_VCO / PLL_P
@@ -256,7 +256,7 @@ void SystemCoreClockUpdate(void)
             pllp            = (((RCC->PLLCFGR & RCC_PLLCFGR_PLLP) >> 16) + 1) * 2;
             SystemCoreClock = pllvco / pllp;
             break;
-        default:
+        default :
             SystemCoreClock = HSI_VALUE;
             break;
     }
