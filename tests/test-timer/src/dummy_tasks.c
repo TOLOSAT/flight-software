b196ace738a6e53a8498fa06ea28eb75cb206f11
Matteo Planchet <matteo.planchet@gmail.com>
2025-03-11 20:10:27 +0100

    wip: begins test + generic callback function

---
+++ tests/test-timer/src/dummy_tasks.c
@@ -0,0 +1,51 @@
+/**
+ * @file    dummy_tasks.c
+ * @author  Merlin Kooshmanian
+ * @brief   Source file with dummy tasks
+ *
+ * @copyright Copyright (c) TOLOSAT 2024
+ */
+
+/******************************* Include Files *******************************/
+
+#include "dummy_tasks.h"
+#include "kernel.h"
+#include "timers.h"
+
+/***************************** Macros Definitions ****************************/
+
+/*************************** Functions Declarations **************************/
+
+/*************************** Variables Definitions ***************************/
+
+/*************************** Functions Definitions ***************************/
+
+/**
+ * @fn      DummyMainTask(void)
+ * @brief   Function that runs the dummy main task.
+ */
+void DummyMainTask(void)
+{
+
+    // Initialisation
+    CreateTimers();
+    // TODO
+    // set timer
+    // check time
+    // launch timer
+    // ...
+    // timer ends
+    // check time
+    // check time is ok :thusup:
+
+    LOG("Init dummy task\n");
+
+    // Task Core
+    while (1)
+    {
+        // Send hello periodically
+        LOG("Hello\n");
+
+        SleepPeriodic();
+    }
+}
