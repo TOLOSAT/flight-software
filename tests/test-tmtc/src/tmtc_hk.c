d48e8796ed9dbd655df1ce849d7a0d08b9c21bd2
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-03-14 16:23:15 +0100

    ref: Update TMTC example

---
+++ tests/test-tmtc/src/tmtc_hk.c
@@ -0,0 +1,53 @@
+/**
+ * @file    tmtc_hk.c
+ * @author  Merlin Kooshmanian
+ * @brief   Source file for TMTC HK Task
+ *
+ * @copyright Copyright (c) TOLOSAT 2025
+ */
+
+/******************************* Include Files *******************************/
+
+#include "tmtc_hk.h"
+#include "kernel.h"
+#include "pus.h"
+
+/***************************** Macros Definitions ****************************/
+
+#define NB_PUS3_EXECUTION 2u /**< Number of pus3 exution functions */
+
+/*************************** Functions Declarations **************************/
+
+/*************************** Variables Definitions ***************************/
+
+/*************************** Functions Definitions ***************************/
+
+/**
+ * @fn              TmTcHkMain(void)
+ * @brief           Main of the TMTC HK Task
+ */
+void TmTcHkMain(void)
+{
+    // Initialisation
+    static pusExecutionTable_t hk_exec_tab[NB_PUS3_EXECUTION] = {
+        { BUILD_ROUTING_KEY(OBC_APID, 3u, 5u), ExecuteS3SS5, TM_NOT_REQUESTED },
+        { BUILD_ROUTING_KEY(OBC_APID, 3u, 6u), ExecuteS3SS6, TM_NOT_REQUESTED },
+    };
+    static pusExecutionContext_t hk_tc_context = {
+        .execution_table      = hk_exec_tab,
+        .execution_table_size = NB_PUS3_EXECUTION,
+        .buffer_tc            = TC_PUS3,
+        .buffer_tm            = NO_BUFFER,
+        .buffer_ack           = TM_PUS1,
+    };
+    CheckError(InitTCExecutionContext(&hk_tc_context));
+
+    // Task Core
+    while (1)
+    {
+        // Execute incoming TC
+        CheckError(ExecuteTC(&hk_tc_context));
+
+        SleepPeriodic();
+    }
+}
