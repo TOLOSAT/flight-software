c170a6eded8b055aa5a88daaf428499e9db6ade4
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-03-15 17:46:08 +0100

    Merge branch 'main' into feature-software-timers

--- tests/test-tmtc/inc/tmtc_hk.h
+++ tests/test-tmtc/inc/tmtc_hk.h
@@ -1,13 +1,13 @@
 /**
- * @file    salami.h
+ * @file    tmtc_hk.h
  * @author  Merlin Kooshmanian
- * @brief   Header file for SALAMI Task
+ * @brief   Header file for TMTC HK Task
  *
  * @copyright Copyright (c) TOLOSAT 2025
  */
 
-#ifndef SALAMI_H
-#define SALAMI_H
+#ifndef TMTC_HK_H
+#define TMTC_HK_H
 
 /******************************* Include Files *******************************/
 
@@ -21,6 +21,6 @@
 
 /*************************** Functions Declarations **************************/
 
-extern void SalamiMain(void);
+extern void TmTcHkMain(void);
 
-#endif /* SALAMI_H */
+#endif /* TMTC_HK_H */
