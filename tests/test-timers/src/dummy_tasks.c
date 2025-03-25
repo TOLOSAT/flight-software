1eb0d16730804145e1a3d9965451c9ec03bab277
Nell15 <nelltruong67@gmail.com>
2025-03-25 19:54:56 +0100

    wip: begins correction of test procedure
    Co-authored-by: Merlin Kooshmanian <mkooshmanian@gmail.com>

--- tests/test-timers/src/dummy_tasks.c
+++ tests/test-timers/src/dummy_tasks.c
@@ -30,30 +30,29 @@ void DummyMainTask(void)
     // Timer Core
     while (1)
     {
-        // Initialisation
-        CreateTimers();
-
         // set timer
-        SetTimer(TIMER_1, (tick_t)1000, TIMER_ONESHOT);
+        SetTimer(DUMMY_TIMER, (tick_t)1000, TIMER_ONESHOT);
 
         // check time (in ticks)
-        tick_t initial_time = xTaskGetTickCount();
+        tick_t initial_time = GetTick();
 
         // launch timer
-        StartTimer(TIMER_1);
+        StartTimer(DUMMY_TIMER);
         // timer ends
 
         // check time
-        tick_t timer_duration = xTaskGetTickCount() - initial_time;
+        tick_t timer_duration = GetTick() - initial_time;
 
         // check duration is ok :thusup:
         if (timer_duration == (tick_t)1000)
         {
-            LOG("It worked !\n");
+            LOG_DECIMAL("It worked ! (lasted: %d ticks)\n", timer_duration);
         }
         else
         {
-            LOG("Shit happened.\n");
+            LOG_DECIMAL("Shit happened. (lasted: %d ticks)\n", timer_duration);
         }
+
+        Sleep(1000);
     }
 }
