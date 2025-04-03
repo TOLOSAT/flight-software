c487def6eaa43e3a6f3a305db214905db1b2292d
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-04-03 19:01:52 +0200

    ref: Naming

--- tools/pre-build/timers-parser.py
+++ tools/pre-build/timers-parser.py
@@ -57,7 +57,7 @@ def generate_timers_conf(csv_file_name, output_directory):
 
 /******************************* Include Files *******************************/
 
-#include "core/timer.h"
+#include "core/timers.h"
 #include "conf/tasks_conf.h"
 #include "conf/timers_conf.h"
 
