b95765432b49cbf71dceedd5c3fbd58048eea604
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-04-04 22:00:57 +0200

    fix: Check inputs and return invalid param if not correct && clean

---
+++ tests/test-timers/inc/pause_resume.h
@@ -0,0 +1,27 @@
+/**
+ * @file    pause_resume.h
+ * @author  Matteo Planchet
+ * @author  Nell Truong
+ * @brief   Header file for pause resume timer task
+ *
+ * @copyright Copyright (c) TOLOSAT 2025
+ */
+
+#ifndef PAUSE_RESUME_TASK_H
+#define PAUSE_RESUME_TASK_H
+
+/******************************* Include Files *******************************/
+
+#include <stdint.h>
+
+/***************************** Macros Definitions ****************************/
+
+/***************************** Types Definitions *****************************/
+
+/*************************** Variables Declarations **************************/
+
+/*************************** Functions Declarations **************************/
+
+extern void PauseResumeTask(void);
+
+#endif /* PAUSE_RESUME_TASK_H */
