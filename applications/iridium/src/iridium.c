e51ea12c2f34e4df433911139c337e3f3af0dd06
Yrolis <yannawbi66@gmail.com>
2025-04-10 20:08:17 +0200

    wip: pus193

--- applications/iridium/src/iridium.c
+++ applications/iridium/src/iridium.c
@@ -11,10 +11,13 @@
 #include "iridium.h"
 #include "kernel.h"
 #include "iridium_driver.h"
+#include "pus193.h"
 
 /***************************** Macros Definitions ****************************/
 
 /*************************** Functions Declarations **************************/
+
+#define NB_PUS193_EXECUTION 4u /**< Number of execution functions */
 
 /*************************** Variables Definitions ***************************/
 
@@ -27,10 +30,27 @@
 void IridiumMain(void)
 {
     // Initialisation
+    static pusExecutionTable_t pus193_exec_tab[NB_PUS193_EXECUTION] = {
+        { BUILD_ROUTING_KEY(OBC_APID, 193u, 1u), ExecuteS193SS1, TM_NOT_REQUESTED },
+        { BUILD_ROUTING_KEY(OBC_APID, 193u, 2u), ExecuteS193SS2, TM_NOT_REQUESTED },
+        { BUILD_ROUTING_KEY(OBC_APID, 193u, 3u), ExecuteS193SS3, TM_REQUESTED     },
+        { BUILD_ROUTING_KEY(OBC_APID, 193u, 5u), ExecuteS193SS5, TM_NOT_REQUESTED },
+    };
+    static pusExecutionContext_t pus193_tc_context = {
+        .execution_table      = pus193_exec_tab,
+        .execution_table_size = NB_PUS193_EXECUTION,
+        .buffer_tc            = TC_PUS193,
+        .buffer_tm            = TM_PUS193,
+        .buffer_ack           = TM_PUS193,
+    };
+    CheckError(InitTCExecutionContext(&pus193_tc_context));
 
     // Task Core
     while (1)
     {
+        // Execute incoming TC
+        CheckError(ExecuteTC(&pus193_tc_context));
+
         SleepPeriodic();
     }
 }
