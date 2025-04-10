6825affeb50e4fe4192987a1606898c649801a26
Yrolis <yannawbi66@gmail.com>
2025-04-10 20:08:17 +0200

    wip: pus193

--- applications/tmtc/src/tm_sender.c
+++ applications/tmtc/src/tm_sender.c
@@ -14,7 +14,7 @@
 
 /***************************** Macros Definitions ****************************/
 
-#define NB_SEND_ENTRY 2u /**< Maximum number of input buffers */
+#define NB_SEND_ENTRY 4u /**< Maximum number of input buffers */
 
 /*************************** Functions Declarations **************************/
 
@@ -33,6 +33,8 @@ void TmSenderMain(void)
     static pusSendTable_t tm_send_table[NB_SEND_ENTRY] = {
         { .buffer = TM_PUS1 },
         { .buffer = TM_NORMAL },
+        { .buffer = TM_PUS161 },
+        { .buffer = TM_PUS193 },
     };
     static pusSendContext_t send_tm_context = {
         .send_table      = tm_send_table,
