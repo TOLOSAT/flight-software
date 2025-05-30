181d7392f9ee225c5f41759044e00d3c516e3d7d
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-05-30 13:38:20 +0200

    feat: flash-erase / set-boot-ram / set-boot-flash commands

--- gen/debug.mk
+++ gen/debug.mk
@@ -15,70 +15,81 @@ include gen/path.mk
 ##############################################
 
 ifeq ($(CHIP_FAMILLY), STM32H7)
-OCD_DBG = interface/stlink.cfg
-OCD_CHIP = target/stm32h7x.cfg
+OCD_DBG = stlink
+OCD_CHIP = stm32h7x
 else ifeq ($(CHIP_FAMILLY), STM32F4)
-OCD_DBG = interface/stlink.cfg
-OCD_CHIP = target/stm32f4x.cfg
+OCD_DBG = stlink
+OCD_CHIP = stm32f4x
 else ifeq ($(CHIP_FAMILLY), CMSDK)
 QEMU_MACHINE = mps2-an500
 else
 $(error This boards is not supported for debugging)
 endif
 
-# Upload Commands
+# Upload command
 ifeq ($(CONFIG_LOAD_MEMORY_FLASH), y)
-UPLOAD_CMDS  = -c "reset init"
-UPLOAD_CMDS += -c "program $(TARGET)"
-UPLOAD_CMDS += -c "reset"
-UPLOAD_CMDS += -c "shutdown"
+OCD_LOAD = program
+OCD_HW_BRKP_CMD =
 else ifeq ($(CONFIG_LOAD_MEMORY_RAM), y)
-UPLOAD_CMDS  = -c "reset init"
-UPLOAD_CMDS += -c "load_image $(TARGET)"
-UPLOAD_CMDS += -c "reset"
-UPLOAD_CMDS += -c "shutdown"
-else
-$(error Load memory can only be FLASH or RAM)
-endif
-
-# Debug Commands
-ifeq ($(CONFIG_LOAD_MEMORY_FLASH), y)
-DBG_CMDS  = -c "reset init"
-DBG_CMDS += -c "program $(TARGET)"
-DBG_CMDS += -c "reset halt"
-else ifeq ($(CONFIG_LOAD_MEMORY_RAM), y)
-DBG_CMDS  = -c "reset init"
-DBG_CMDS += -c "gdb_breakpoint_override hard"
-DBG_CMDS += -c "load_image $(TARGET)"
-DBG_CMDS += -c "reset halt"
+OCD_LOAD = load_image
+OCD_HW_BRKP_CMD = -c "gdb_breakpoint_override hard"
 else
 $(error Load memory can only be FLASH or RAM)
 endif
 
 ##############################################
-############### DEBUG COMMANDS ###############
+############ DEBUG/UPLOAD COMMANDS ###########
 ##############################################
 
-.PHONY : debug gdb upload
+.PHONY : debug gdb upload flash-erase set-boot-ram set-boot-flash
 
 ifeq ($(BOARD), QEMU)
+####################
+####### QEMU #######
+####################
+
+# Debug Command
 debug :
 	@$(EMU) -machine $(QEMU_MACHINE) -cpu $(MACH) -m 16M -kernel $(TARGET) -nographic -serial mon:stdio -serial tcp:localhost:4444,server,nowait -s -S
 
+# Start GDB
 gdb:
 	@$(GDB) --eval-command="target remote:1234" $(TARGET)
 
+# Upload Command
 upload :
 	@$(EMU) -machine $(QEMU_MACHINE) -cpu $(MACH) -m 16M -kernel $(TARGET) -nographic -serial mon:stdio -serial tcp:localhost:4444,server,nowait
-else
-debug :
-	@$(OCD) -f $(OCD_DBG) -f $(OCD_CHIP) -c init $(DBG_CMDS)
 
+else
+####################
+### REAL TARGETS ###
+####################
+
+# Debug Command
+debug :
+	@echo $(OCD) -f interface/$(OCD_DBG).cfg -f target/$(OCD_CHIP).cfg -c init -c "reset init" $(OCD_HW_BRKP_CMD) -c "$(OCD_LOAD) $(TARGET)" -c "reset halt"
+
+# Start GDB
 gdb:
 	@$(GDB) -ex "set pagination off" -ex "target extended-remote localhost:3333" $(TARGET)
 
+# Upload Command
 upload :
-	@$(OCD) -f $(OCD_DBG) -f $(OCD_CHIP) -c init $(UPLOAD_CMDS)
+	@$(OCD) -f interface/$(OCD_DBG).cfg -f target/$(OCD_CHIP).cfg -c init  -c "reset init" -c "$(OCD_LOAD) $(TARGET)" -c "reset" -c "shutdown"
+
+# Flash Erase Command
+flash-erase:
+	@$(OCD) -f interface/$(OCD_DBG).cfg -f target/$(OCD_CHIP).cfg -c init -c 'reset halt' -c '$(OCD_CHIP) mass_erase 0' -c "reset" -c "shutdown"
+
+# Boot Option Commands
+ifeq ($(CHIP_FAMILLY), STM32H7)
+set-boot-ram:
+	@$(OCD) -f interface/$(OCD_DBG).cfg -f target/$(OCD_CHIP).cfg -c init -c "reset init" -c "$(OCD_CHIP) option_write 0 0x44 0x00000000" -c "reset" -c "shutdown"
+
+set-boot-flash:
+	@$(OCD) -f interface/$(OCD_DBG).cfg -f target/$(OCD_CHIP).cfg -c init -c "reset init" -c "$(OCD_CHIP) option_write 0 0x44 0x00000800" -c "reset" -c "shutdown"
+endif
+
 endif
 
 endif # BUILD_DEBUG_MK #
