bf29d85176b1cdf449e1a192077a76565f263f18
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-06-22 17:26:40 +0200

    feat: Usage test (with dummy load)

---
+++ tests/test-usage/src/dummy_tasks.c
@@ -0,0 +1,40 @@
+/**
+ * @file    dummy_tasks.c
+ * @author  Merlin Kooshmanian
+ * @brief   Source file with dummy tasks
+ *
+ * @copyright Copyright (c) TOLOSAT 2025
+ */
+
+/******************************* Include Files *******************************/
+
+#include "dummy_tasks.h"
+#include "kernel.h"
+#include "bsp.h"
+
+/***************************** Macros Definitions ****************************/
+
+/*************************** Functions Declarations **************************/
+
+/*************************** Variables Definitions ***************************/
+
+/*************************** Functions Definitions ***************************/
+
+/**
+ * @fn      DummyMainTask(void)
+ * @brief   Function that runs the dummy main task.
+ */
+void DummyMainTask(void)
+{
+    // Initialisation
+    LOG("Init dummy task\n");
+
+    // Task Core
+    while (1)
+    {
+        // Consume CPU (active wait)
+        HAL_Delay(500);
+
+        SleepPeriodic();
+    }
+}
