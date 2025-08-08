e854a840c467db0d26646138ccd24a36864e10b4
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-08-08 13:09:51 +0200

    ref: Moving macros to BSP && fix: Updated ioc

--- bsp/KLARA-BSP/inc/bsp.h
+++ bsp/KLARA-BSP/inc/bsp.h
@@ -89,6 +89,28 @@
 #define ONEWIRE_PIN                 GPIO_PIN_2
 #define ONEWIRE_PORT                GPIOA
 
+/* NAND CONSTANTS */
+#define NAND_PAGE_SIZE              4096U
+#define NAND_SPARE_AREA_SIZE        256U
+#define NAND_BLOCK_SIZE_IN_PAGES    64U
+#define NAND_PLANE_SIZE_IN_BLOCKS   2048U
+#define NAND_BLOCK_COUNT            4096
+#define NAND_PLANE_COUNT            2U
+#define NAND_TCLR_SETUP_TIME        2U
+#define NAND_TAR_SETUP_TIME         2U
+#define NAND_TIMING_SETUP_TIME      0U
+#define NAND_TIMING_WAIT_TIME       1U
+#define NAND_TIMING_HOLD_TIME       1U
+#define NAND_TIMING_HIZ_TIME        4U
+#define NAND_ECC_COMPUTATION        FMC_NAND_ECC_DISABLE
+#define NAND_ECC_PAGE_SIZE          FMC_NAND_ECC_PAGE_SIZE_4096BYTE
+#define NAND_MEMORY_BUS_WIDTH       FMC_NAND_MEM_BUS_WIDTH_8
+#define NAND_WAIT_FEATURE           FMC_NAND_WAIT_FEATURE_ENABLE
+#define NAND_BANK                   FMC_NAND_BANK3
+#define NAND_EXTRA_COMMAND          DISABLE
+#define NAND_WP_PIN                 GPIO_PIN_10
+#define NAND_WP_PORT                GPIOD
+
 /* WATCHDOG CONSTANTS */
 #define WATCHDOG_REF                IWDG1
 
