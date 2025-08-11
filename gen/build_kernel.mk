5a690c43c94e150f2d91cfe81af5b399c58b12cc
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-08-11 12:30:51 +0200

    feat: split peripherals from system conf

--- gen/build_kernel.mk
+++ gen/build_kernel.mk
@@ -35,8 +35,10 @@ MEMDRV += $(KERNEL_DRV_SRCDIR)/memory/memdrv_qspi.c
 endif
 
 # Kernel files
-KERNEL_SRCS = $(wildcard $(KERNEL_SRCDIR)/*.c $(KERNEL_SRCDIR)/*/*.c $(KERNEL_DRV_SRCDIR)/*.c $(KERNEL_DRV_SRCDIR)/peripherals/*.c $(KERNEL_DRV_SRCDIR)/others/*.c $(DISKDRV) $(MEMDRV)) $(CONF_SRCS)
-KERNEL_OBJS = $(patsubst $(KERNEL_SRCDIR)/%.c,$(KERNEL_OBJDIR)/%-$(BUILD_TYPE).o,$(patsubst $(PRE_BUILD_DIR)/conf/%.c,$(KERNEL_OBJDIR)/conf/%-$(BUILD_TYPE).o,$(KERNEL_SRCS)))
+KERNEL_SRCS = $(wildcard $(KERNEL_SRCDIR)/*.c $(KERNEL_SRCDIR)/*/*.c $(KERNEL_DRV_SRCDIR)/*.c $(KERNEL_DRV_SRCDIR)/peripherals/*.c $(KERNEL_DRV_SRCDIR)/others/*.c $(DISKDRV) $(MEMDRV)) $(SYS_CONF_SRCS) $(BSP_CONF_SRCS)
+KERNEL_OBJS = $(patsubst $(KERNEL_SRCDIR)/%.c,$(KERNEL_OBJDIR)/%-$(BUILD_TYPE).o, \
+			  $(patsubst $(PRE_BUILD_DIR)/conf/%.c,$(KERNEL_OBJDIR)/conf/%-$(BUILD_TYPE).o, \
+			  $(KERNEL_SRCS)))
 KERNEL_LIB  = $(LIBS_DIR)/libkernel-$(BUILD_TYPE).a
 
 # System defines (those are use for system info const struct)
