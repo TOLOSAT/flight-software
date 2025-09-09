a64416e32395235774b3a0b1efdde9c2329ccbc8
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-09-09 21:45:34 +0200

    feat: NAND memory driver

--- bsp/KLARA-BSP/inc/bsp.h
+++ bsp/KLARA-BSP/inc/bsp.h
@@ -15,36 +15,13 @@
 /***************************** Macros Definitions ****************************/
 
 /* INTERNAL TIMERS */
-#define HAL_TIMER_REF             TIM4
-#define HAL_TIMER_IRQ_NO          TIM4_IRQn
-#define MONITORING_TIMER_REF      TIM3
-#define MONITORING_TIMER_IRQ_NO   TIM3_IRQn
+#define HAL_TIMER_REF           TIM4
+#define HAL_TIMER_IRQ_NO        TIM4_IRQn
+#define MONITORING_TIMER_REF    TIM3
+#define MONITORING_TIMER_IRQ_NO TIM3_IRQn
 
 /* WATCHDOG CONSTANTS */
-#define WATCHDOG_REF              IWDG1
-
-/* NAND CONSTANTS */
-#define NAND_PAGE_SIZE            4096U
-#define NAND_SPARE_AREA_SIZE      256U
-#define NAND_BLOCK_SIZE_IN_PAGES  64U
-#define NAND_PLANE_SIZE_IN_BLOCKS 2048U
-#define NAND_PLANE_COUNT          2U
-#define NAND_BLOCK_COUNT          (NAND_PLANES * NAND_BLOCKS_PER_PLANE)
-#define NAND_TCLR_SETUP_TIME      2U
-#define NAND_TAR_SETUP_TIME       2U
-#define NAND_TIMING_SETUP_TIME    0U
-#define NAND_TIMING_WAIT_TIME     1U
-#define NAND_TIMING_HOLD_TIME     1U
-#define NAND_TIMING_HIZ_TIME      4U
-#define NAND_ECC_COMPUTATION      FMC_NAND_ECC_DISABLE
-#define NAND_ECC_PAGE_SIZE        FMC_NAND_ECC_PAGE_SIZE_4096BYTE
-#define NAND_BUS_WIDTH            FMC_NAND_MEM_BUS_WIDTH_8
-#define NAND_WAIT_FEATURE         FMC_NAND_WAIT_FEATURE_ENABLE
-#define NAND_BANK                 FMC_NAND_BANK3
-#define NAND_EXTRA_COMMAND        DISABLE
-#define NAND_WP_PIN               GPIO_PIN_10
-#define NAND_WP_PORT              GPIOD
-#define NAND_WP_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+#define WATCHDOG_REF            IWDG1
 
 /***************************** Types Definitions *****************************/
 
