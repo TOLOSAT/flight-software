c2c32a9deed46fa2fcfdb7408da93428bc5ea760
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-10-04 19:51:55 +0200

    feat: enabled a verif for each module

--- gen/verification.mk
+++ gen/verification.mk
@@ -12,52 +12,40 @@ include gen/paths.mk
 include $(APPLICATIONS_DIR)/Makefile
 
 ##############################################
-################ CHECKER FILES ###############
+################ STATIC CHECK ################
 ##############################################
 
+.PHONY : verif
+
+# Main recipe
+verif: kernel-verif $(foreach lib,$(APPLICATION_DEPENDANCIES),$(lib)-verif) applications-verif
+
+##############################################
+########## APPLICATION STATIC CHECK ##########
+##############################################
+
+# Application verif
+.PHONY : applications-verif
+
+CHECKER_ERROR_MSG = "\033[1;31mCode checked: errors have been found. Please correct them before continuing.\033[0m"
+
+# Checker files and includes
 CONF_MISRA = $(GEN_DIR)/MISRA/misra.json
+CHECKER_LOGS = $(APPLICATIONS_OBJDIR)/code-checking.log
 
-CHECKER_SRCS =	$(APPLICATIONS_SRCS) \
-				$(KERNEL_SRCS) \
-				$(wildcard $(PUS_DIR)/src/*.c $(PUS_DIR)/src/*/*.c) \
-				$(wildcard $(IRIDIUM_DIR)/src/*.c $(IRIDIUM_DIR)/src/*/*.c)
-
-CHECKER_SRCS := $(filter-out $(PRE_BUILD_DIR)/%, \
-                $(filter-out $(KERNEL_SRCDIR)/bsp/%, $(CHECKER_SRCS)))
-
-CHECKER_INCS =	$(APPLICATIONS_INCS) \
-				-I$(KERNEL_HEADERS) \
-				-I$(KERNEL_INCDIR) \
-				-I$(PRE_BUILD_DIR) \
-				-I$(PUS_DIR)/inc \
-				-I$(IRIDIUM_DIR)/inc \
-				-I$(FREERTOS_CONFDIR) \
-				-I$(FATFS_CONFDIR)
-
-##############################################
-############### CHECKER CONFIGS ##############
-##############################################
-
+# Checker commands
 CHECKER_CMDS  = --enable=all # Enables all warnings
 CHECKER_CMDS += --suppress=missingInclude # Disables missing includes warnings
 CHECKER_CMDS += --suppress=unusedFunction # Disables unused function warnings
 CHECKER_CMDS += --inline-suppr # Allows to do suppress inside the code (inline)
 CHECKER_CMDS += --addon=$(CONF_MISRA) # Check MISRA C compliancee if misra settings are added
-CHECKER_CMDS += --output-file=build/code-checking.log # Print the result in a log file
+CHECKER_CMDS += --output-file=$(CHECKER_LOGS) # Print the result in a log file
 CHECKER_CMDS += --error-exitcode=1 # Returns 1 if cppcheck has encountered an error
-CHECKER_CMDS += --suppress=misra-c2012-11.5 # Suppression of this rule because its often use to pass parameters for callbacks inside the kernel
 
-##############################################
-############### CHECKER COMMAND ##############
-##############################################
-
-CHECKER_ERROR_MSG = "\033[1;31mCode checked: errors have been found. Please correct them before continuing.\033[0m"
-
-.PHONY : verif
-
-verif : autoconf conf-files
-	@mkdir -p $(BUILD_DIR)
-	@$(CHECKER) $(CHECKER_CMDS) $(CHECKER_INCS) $(CHECKER_SRCS) || (cat build/code-checking.log; echo $(CHECKER_ERROR_MSG) ; exit 1)
+# Checker recipes
+applications-verif : conf-files
+	@mkdir -p $(APPLICATIONS_OBJDIR)
+	@$(CHECKER) $(CHECKER_CMDS) $(filter-out $(SYS_CONF_SRCS), $(APPLICATIONS_SRCS)) $(APPLICATIONS_INCFLAGS) $(CHECKER_SRCS) || (cat $(CHECKER_LOGS); echo $(CHECKER_ERROR_MSG) ; exit 1)
 
 ##############################################
 ############ DOCUMENTATION COMMAND ###########
