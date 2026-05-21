8a24096254b5e99627612ebba61f3bd4251dac86
Matteo Planchet <matteo.planchet@gmail.com>
2026-05-21 18:42:41 +0200

    feat(qebab): add beautiful branding

--- tools/qebab.py
+++ tools/qebab.py
@@ -6,9 +6,56 @@ import socket
 host = "127.0.0.1"
 port = 4444
 
-print("Connecting to QEMU debug console on")
-# TODO: figure out why the first response is truncated
+print(
+    """
+   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+   ⠀⠀⠀⠀⠀⠀⢀⣠⣤⣶⣶⣶⣿⣿⣿⣿⣿⣿⣶⣶⣶⣤⣄⠀⠀⠀⠀⠀⠀⠀
+   ⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠇⠀⠀⠀⠀⠀⠀
+   ⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣦⣤⣤⣤⣤⡄⠀⠀⠀⠀⠀⠀
+   ⠀⠀⠀⠀⠀⠀⠀⣀⣉⣭⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀
+   ⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠀⠀⠀⠀⠀⠀⠀
+   ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⣴⠀⠀⠀⠀
+   ⠀⠀⢀⡄⠀⠀⠀⠀⠙⠛⠿⠿⣿⣿⣿⣿⣿⣿⣟⣛⣛⡋⠀⠀⣾⣿⣀⠀⠀⠀
+   ⠀⠀⣼⣇⣀⠀⠀⠀⠀⢶⣶⣶⣶⣾⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠉⢻⣿⠇⠀⠀
+   ⠀⠀⠻⢿⣿⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⣴⣾⠟⠁⠀⠀⠀
+   ⠀⠀⠀⢀⣯⢀⡀⠀⠀⠀⠀⣤⣭⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠈⢻⣦⠀⠀⠀⠀
+   ⠀⠀⠀⠈⠛⠻⢿⡄⠀⠀⠀⠘⢿⣿⣿⣿⣿⠿⠃⠀⠀⠀⠀⢀⡼⠋⠀⠀⠀⠀
+   ⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⢠⡄⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀
+   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                 888              888
+ e88 888  ,e e,  888 88e   ,"Y88b 888 88e
+d888 888 d88 88b 888 888b "8" 888 888 888b
+Y888 888 888   , 888 888P ,ee 888 888 888P
+ "88 888  "YeeP" 888 88"  "88 888 888 88"
+     888
+     888
+      """
+)
+
+print("Connecting to QEMU debug console...")
+
 s = socket.create_connection((host, port))
+s.settimeout(0.2)
+
 while True:
-    s.send(bytes.fromhex(input("Enter command (hex): ")))
-    print(s.recv(4096).hex())
+    cmd = input("Enter command (hex): ")
+
+    s.sendall(bytes.fromhex(cmd))
+
+    data = b""
+
+    while True:
+        try:
+            chunk = s.recv(4096)
+
+            if not chunk:
+                break
+
+            data += chunk
+
+        except socket.timeout:
+            # no more bytes incoming
+            break
+
+    print(data.hex())
