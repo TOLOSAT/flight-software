387d586d133583a5c77105f2582b69b52e4ba1da
Merlin Kooshmanian <mkooshmanian@gmail.com>
2026-09-13 14:33:44 +0200

    chore: finished pus11 refactoring && fix: bug leaving an infinite wait in tc_scheduler when delay == 0

--- applications/tmtc/src/tc_scheduler.c
+++ applications/tmtc/src/tc_scheduler.c
@@ -66,6 +66,9 @@ void TcSchedulerMain(void)
     // Task Core
     while (1)
     {
+        // Indicates whether the task must wait before processing the schedule again
+        bool wait_for_signal = true;
+
         // Execute incoming TC
         CheckError(ExecuteTC(&sched_tc_context), SEVERITY_MEDIUM);
 
@@ -83,17 +86,19 @@ void TcSchedulerMain(void)
             {
                 tick_t delay = CUC_TO_TICK(next_tc_release_date - current_time, kernel_clock_freq_hz);
                 CheckError(SetTimer(PUS11_TIMER, delay, TIMER_ONESHOT), SEVERITY_MEDIUM);
+                CheckError(StartTimer(PUS11_TIMER), SEVERITY_MEDIUM);
             }
             else
             {
-                CheckError(SetTimer(PUS11_TIMER, 0u, TIMER_ONESHOT), SEVERITY_MEDIUM);
+                // Skip wait for signal (a delayed TC needs to be released)
+                wait_for_signal = false;
             }
-
-            // Then start the timer
-            CheckError(StartTimer(PUS11_TIMER), SEVERITY_MEDIUM);
         }
 
         // Wait for timer end
-        CheckError(WaitSignal(SIGNAL_TIMER_ENDED | SIGNAL_TC), SEVERITY_MEDIUM);
+        if (wait_for_signal)
+        {
+            CheckError(WaitSignal(SIGNAL_TIMER_ENDED | SIGNAL_TC), SEVERITY_MEDIUM);
+        }
     }
 }
